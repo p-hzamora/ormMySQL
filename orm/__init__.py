81fc2b9a834e841ebbba2402a21ad6db4ca25d5e
p-hzamora <p.hzamora@icloud.com>
2024-04-22 15:27:28 +0200

    feat: added 'nameof' function

--- orm/__init__.py
+++ orm/__init__.py
@@ -1,4 +1,4 @@
 from .interfaces import IRepositoryBase  # noqa: F401
 from .repository_my_sql import MySQLRepository, errorcode, Error  # noqa: F401
 from .model_base import ModelBase  # noqa: F401
-from .orm_objects import Table, Column  # noqa: F401
+from .orm_objects import Table, Column, nameof  # noqa: F401
--- orm/orm_objects/__init__.py
+++ orm/orm_objects/__init__.py
@@ -1,2 +1,3 @@
 from .column import Column  # noqa: F401
 from .table import Table  # noqa: F401
+from .name_of import nameof  # noqa: F401
