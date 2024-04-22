28302d9f5c98f9ed28e5c0c0372dc88cbaed280e
p-hzamora <p.hzamora@icloud.com>
2024-04-22 14:25:13 +0200

    style: applied Ruff

--- orm/orm_objects/dissambler.py
+++ orm/orm_objects/dissambler.py
@@ -3,7 +3,7 @@ from types import FrameType
 from typing import Iterator
 
 
-def get_assigned_name(frame:FrameType):
+def get_assigned_name(frame: FrameType):
     """Takes a frame and returns a description of the name(s) to which the
     currently executing CALL_FUNCTION instruction's value will be assigned.
 
@@ -26,12 +26,12 @@ def get_assigned_name(frame:FrameType):
     return instr_dispatch(instr, iterator)
 
 
-def instr_dispatch(instr:dis.Instruction, iterator:Iterator[dis.Instruction]):
+def instr_dispatch(instr: dis.Instruction, iterator: Iterator[dis.Instruction]):
     valid_opname = (
         "STORE_FAST",
         "STORE_GLOBAL",
         "STORE_NAME",
-        "STORE_DEREF"  ,
+        "STORE_DEREF",
     )
     if instr.opname in valid_opname:  # (co_cellvars++co_freevars)
         return instr.argval
@@ -58,17 +58,25 @@ def instr_dispatch(instr:dis.Instruction, iterator:Iterator[dis.Instruction]):
     # Thus we limit ourselfs to loading a simply variable (of any kind)
     # and a arbitary number of LOAD_ATTR calls before the final STORE_ATTR.
     # We will represents simply a string like `my_var.loaded.loaded.assigned`
-    if valid_opname in {"LOAD_CONST", "LOAD_DEREF", "LOAD_FAST", "LOAD_GLOBAL", "LOAD_NAME"}:
+    if valid_opname in {
+        "LOAD_CONST",
+        "LOAD_DEREF",
+        "LOAD_FAST",
+        "LOAD_GLOBAL",
+        "LOAD_NAME",
+    }:
         return instr.argval + "." + ".".join(instr_dispatch_for_load(instr, iterator))
     raise NotImplementedError(
         "assignment could not be parsed: " "instruction {} not understood".format(instr)
     )
 
-def take(iterator:Iterator[dis.Instruction], count):
+
+def take(iterator: Iterator[dis.Instruction], count):
     for _ in range(count):
         yield take1(iterator)
 
-def take1(iterator:Iterator[dis.Instruction])->dis.Instruction:
+
+def take1(iterator: Iterator[dis.Instruction]) -> dis.Instruction:
     try:
         return next(iterator)
     except StopIteration:
