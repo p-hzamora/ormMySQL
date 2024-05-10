b566fc6f6d0d814ca77642f80979bdec811ea5b5
p-hzamora <p.hzamora@icloud.com>
2024-05-10 08:49:16 +0200

    feat: added models/ to testing purpose

---
+++ test/models/address.py
@@ -0,0 +1,120 @@
+from typing import NamedTuple, Optional
+from datetime import datetime
+
+from orm import (
+    Column,
+    Table,
+    nameof,
+    ModelBase,
+    IRepositoryBase,
+)
+
+
+
+class Point(NamedTuple):
+    x: float
+    y: float
+
+
+class Address(Table):
+    __table_name__ = "address"
+
+    def __init__(
+        self,
+        address_id: int,
+        address: Optional[str],
+        address2: Optional[str],
+        district: str,
+        city_id: int,
+        postal_code: str,
+        phone: str,
+        location: Point,
+        last_update: datetime,
+    ) -> None:
+        self._address_id: Column[int] = Column(nameof(address_id), address_id, is_primary_key=True)
+        self._address: Column[str] = Column(nameof(address), address)
+        self._address2: Column[str] = Column(nameof(address2), address2)
+        self._district: Column[str] = Column(nameof(district), district)
+        self._city_id: Column[datetime] = Column(nameof(city_id), city_id)
+        self._postal_code: Column[datetime] = Column(nameof(postal_code), postal_code)
+        self._phone: Column[datetime] = Column(nameof(phone), phone)
+        self._location: Column[datetime] = Column(nameof(location), location)
+        self._last_update: Column[datetime] = Column(nameof(last_update), last_update)
+
+
+    @property
+    def address_id(self) -> int:
+        return self._address_id.column_value
+
+    @address_id.setter
+    def address_id(self, value: int) -> None:
+        self._address_id.column_value = value
+
+    @property
+    def address(self) -> Optional[str]:
+        return self._address.column_value
+
+    @address.setter
+    def address(self, value: Optional[str]) -> None:
+        self._address.column_value = value
+
+    @property
+    def address2(self) -> Optional[str]:
+        return self._address2.column_value
+
+    @address2.setter
+    def address2(self, value: Optional[str]) -> None:
+        self._address2.column_value = value
+
+    @property
+    def district(self) -> str:
+        return self._district.column_value
+
+    @district.setter
+    def district(self, value: str) -> None:
+        self._district.column_value = value
+
+    @property
+    def city_id(self) -> int:
+        return self._city_id.column_value
+
+    @city_id.setter
+    def city_id(self, value: int) -> None:
+        self._city_id.column_value = value
+
+    @property
+    def postal_code(self) -> str:
+        return self._postal_code.column_value
+
+    @postal_code.setter
+    def postal_code(self, value: str):
+        self._postal_code.column_value = value
+
+    @property
+    def phone(self) -> str:
+        return self._phone.column_value
+
+    @phone.setter
+    def phone(self, value: str):
+        self._phone.column_value = value
+
+    @property
+    def location(self) -> Point:
+        return self._location.column_value
+
+    @location.setter
+    def location(self, value: Point):
+        self._location.column_value = value
+
+    @property
+    def last_update(self) -> datetime:
+        return self._last_update.column_value
+
+    @last_update.setter
+    def last_update(self, value: datetime):
+        self._last_update.column_value = value
+
+
+class AddressModel(ModelBase[Address]):
+    def __init__(self, repository: IRepositoryBase):
+        super().__init__(Address, repository=repository)
---
+++ test/models/city.py
@@ -0,0 +1,66 @@
+from orm import (
+    Column,
+    Table,
+    nameof,
+    ModelBase,
+    IRepositoryBase,
+    ForeignKey,
+)
+
+from datetime import datetime
+
+from .country import Country
+
+class City(Table):
+    __table_name__ = "city"
+
+    def __init__(self, city_id: int, city: str, country_id: int, last_update: datetime) -> None:
+        self._city_id: Column[int] = Column(nameof(city_id), city_id, is_primary_key=True)
+        self._city: Column[str] = Column(nameof(city), city)
+        self._country_id: Column[str] = Column(nameof(country_id), country_id)
+        self._last_update: Column[datetime] = Column(nameof(last_update), last_update)
+
+        self.country: ForeignKey = ForeignKey(
+            orig_table=City,
+            orig_column=nameof(City.country_id),
+            referenced_table=Country,
+            referenced_column=nameof(Country.country_id),
+        )
+
+
+    @property
+    def city_id(self) -> int:
+        return self._city_id.column_value
+
+    @city_id.setter
+    def city_id(self, value: int) -> None:
+        self._city_id.column_value = value
+
+    @property
+    def city(self) -> str:
+        return self._city.column_value
+
+    @city.setter
+    def city(self, value: str) -> None:
+        self._city.column_value = value
+
+    @property
+    def country_id(self) -> int:
+        return self._country_id.column_value
+
+    @country_id.setter
+    def country_id(self, value: int) -> None:
+        self._country_id.column_value = value
+
+    @property
+    def last_update(self) -> str:
+        return self._last_update.column_value
+
+    @last_update.setter
+    def last_update(self, value: str) -> None:
+        self._last_update.column_value = value
+
+
+class CityModel(ModelBase[City]):
+    def __init__(self, repository: IRepositoryBase):
+        super().__init__(City, repository=repository)
