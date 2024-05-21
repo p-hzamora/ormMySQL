ab53ee03d9eef09dd8383ab23c042a84ad6fd845
phzamora <p.hzamora@icloud.com>
2024-05-21 18:20:48 +0200

    update: 'QuerySelector' class

--- orm/orm_objects/queries/query.py
+++ orm/orm_objects/queries/query.py
@@ -4,10 +4,13 @@ from abc import abstractmethod
 
 from ...interfaces.IQuery import IQuery
 from ..table import Table
-from .condition import Condition
+from .where_condition import WhereCondition
+from orm.dissambler import Dissambler
+
 
 def get_var_from_lambda[T](foo: Callable[[T], None]) -> str:
     return {x.opname: x.argval for x in dis.Bytecode(foo)}["LOAD_ATTR"]
+
 
 class QuerySelector[T](IQuery):
     @overload
@@ -19,14 +22,14 @@ class QuerySelector[T](IQuery):
     @overload
     def __init__(
         self,
-        where: Condition,
+        where: WhereCondition,
     ) -> None: ...
 
     @overload
     def __init__(
         self,
         orig_table: Table,
-        where: Condition,
+        where: WhereCondition,
     ) -> None: ...
 
     @overload
@@ -58,7 +61,7 @@ class QuerySelector[T](IQuery):
         self._select_list: Optional[Callable[[T], None] | Iterable[Callable[[T], None]]] = select_list
         self._select_str: str = self._convert_select_list(select_list)
         self._orig_table: Table = orig_table
-        self._where: Condition = Condition[T, T2](where) if where else None
+        self._where: WhereCondition = WhereCondition[T, T2](where) if where else None
 
     def _convert_select_list(self, select_list: Callable[[T], None] | Iterable[Callable[[T], None]]) -> str:
         if select_list is None:
@@ -79,8 +82,8 @@ class QuerySelector[T](IQuery):
 
     @override
     def load(self) -> str:
+        _dis = Dissambler(self._select_list)
         condition = ""
         if self._where:
             condition = " " + self._where.to_query()
         return f"{self.query}{condition}{self.SEMICOLON}"
-
