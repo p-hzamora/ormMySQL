41a6ddebd8a757a85b9b1668b8068e4f7158f0c3
p-hzamora <p.hzamora@icloud.com>
2024-05-21 13:56:22 +0200

    chore: added __database_name__ in Table class

--- orm/orm_objects/table.py
+++ orm/orm_objects/table.py
@@ -1,9 +1,13 @@
 from abc import ABC
 import json
+# from .foreign_key import ForeignKey
 
 
 class Table(ABC):
     __table_name__: str = ...
+    __database_name__: str = ...
+
+    # foreign_key: ForeignKey = ForeignKey
 
     def __repr__(self) -> str:
         params = ", ".join([f"{x}={getattr(self,x)}" for x, y in self.__class__.__dict__.items() if isinstance(y, property)])
