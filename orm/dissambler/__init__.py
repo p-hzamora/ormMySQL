9f83dabbc7b7f124ae73ba985c55b6c10dbd0dd8
phzamora <p.hzamora@icloud.com>
2024-05-21 18:21:40 +0200

    chore: updated __init__.py

--- orm/dissambler/__init__.py
+++ orm/dissambler/__init__.py
@@ -1,2 +1,3 @@
 from .dissambler import Dissambler  # noqa: F401
+from .nested_element import NestedElement  # noqa: F401
 from .name_of import nameof  # noqa: F401
--- orm/orm_objects/queries/__init__.py
+++ orm/orm_objects/queries/__init__.py
@@ -1,4 +1,4 @@
-from .condition import Condition  # noqa: F401
+from .where_condition import WhereCondition  # noqa: F401
 from .joins import JoinSelector, JoinType  # noqa: F401
 from .query import QuerySelector  # noqa: F401
 from .select import SelectQuery  # noqa: F401
