303e16037b8ce2e69d909cd56148bf9bb96cb738
p-hzamora <p.hzamora@icloud.com>
2024-05-21 13:47:15 +0200

    udpate: added ConditionType

---
+++ orm/condition_types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class ConditionType(Enum):
+    EQUAL = "=="
+    LESS_THAN = "<"
+    GREATER_THAN = ">"
+    LESS_THAN_OR_EQUAL = "<="
+    GREATER_THAN_OR_EQUAL = ">="
+    NOT_EQUAL = "!="
+    REGEXP = "REGEXP"
