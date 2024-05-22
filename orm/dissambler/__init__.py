e8ea95dbf76b784eec67b88fc215f25447a42072
phzamora <p.hzamora@icloud.com>
2024-05-23 01:19:35 +0200

    chore: updated __init__ with "TreeInstruction"

--- orm/dissambler/__init__.py
+++ orm/dissambler/__init__.py
@@ -1,3 +1,4 @@
 from .dissambler import Dissambler  # noqa: F401
 from .nested_element import NestedElement  # noqa: F401
+from .tree_instruction import TreeInstruction  # noqa: F401
 from .name_of import nameof  # noqa: F401
