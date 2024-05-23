7d3442a9eccdff1511c1b443be2eee907c54c1c7
p-hzamora <p.hzamora@icloud.com>
2024-05-23 09:27:07 +0200

    update: ForeignKey class

--- orm/orm_objects/foreign_key.py
+++ orm/orm_objects/foreign_key.py
@@ -13,33 +13,24 @@ class RelationShip[T: Table](NamedTuple):
 class ForeignKey[Tbl1: Table, Tbl2: Table]:
     MAPPED: dict[str, list[dict[str, RelationShip]]] = defaultdict(dict)
 
-    def __new__(
-        cls,
+    def __init__(
+        self,
         orig_table: Table,
         referenced_table: Table,
         relationship: Callable[[Tbl1, Tbl2], bool],
     ) -> None:
-        cls._orig_table: Tbl1 = orig_table
-        cls._referenced_table: Tbl2 = referenced_table
-        cls._relationship: Callable[[Tbl1, Tbl2], bool] = relationship
+        self._orig_table: Tbl1 = orig_table
+        self._referenced_table: Tbl2 = referenced_table
+        self._dissambler_functions: Dissambler[Tbl1, Tbl2] = Dissambler[Tbl1, Tbl2](relationship)
 
-        cls._dissambler_functions: Dissambler = Dissambler(relationship)
-
-        rs: RelationShip = RelationShip[Tbl2](cls._dissambler_functions.cond_2, referenced_table)
-
-        cls.MAPPED[cls._orig_table.__table_name__][cls._referenced_table.__table_name__] = rs
-
-        if cls._dissambler_functions.cond_1 not in cls.MAPPED[cls._orig_table.__table_name__][cls._referenced_table.__table_name__]:
-            cls.MAPPED[cls._orig_table.__table_name__][cls._dissambler_functions.cond_1] = RelationShip[Tbl2](cls._dissambler_functions.cond_2, referenced_table)
-
-        return object.__new__(cls)
+        rs: RelationShip = RelationShip[Tbl2](self._dissambler_functions.cond_2, referenced_table)
+        self._add_fk(self._orig_table.__table_name__, self._dissambler_functions.cond_1.name, rs)
 
     def __repr__(self) -> str:
         return f"<{ForeignKey.__name__}>: {self._orig_table.__name__}.{self._dissambler_functions.cond_1} {self._dissambler_functions.compare_op} {self._referenced_table.__name__}.{self._dissambler_functions.cond_2}>"
 
-    # @classmethod
-    # def update_fk_dicc(cls) -> None:
-    #     ForeignKey.MAPPED[cls._orig_table.__table_name__][cls._orig_column: RelationShip(
-    #                 cls._referenced_column,
-    #                 cls._referenced_table,
-    #     ]
+    @classmethod
+    def _add_fk(cls, orig_table: str, fk_orig_table: str, relationship: RelationShip) -> None:
+        if fk_orig_table in cls.MAPPED[orig_table]:
+            raise KeyError(f"'{fk_orig_table}' already in '{orig_table}' table as foregin key")
+        cls.MAPPED[orig_table][fk_orig_table] = relationship
