d5ef22b67eb904350d1a2f548011230d0a1d6099
phzamora <p.hzamora@icloud.com>
2024-05-23 02:42:13 +0200

    wip: query.py

--- orm/orm_objects/queries/query.py
+++ orm/orm_objects/queries/query.py
@@ -5,11 +5,7 @@ from abc import abstractmethod
 from ...interfaces.IQuery import IQuery
 from ..table import Table
 from .where_condition import WhereCondition
-from orm.dissambler import Dissambler
-
-
-def get_var_from_lambda[T](foo: Callable[[T], None]) -> str:
-    return {x.opname: x.argval for x in dis.Bytecode(foo)}["LOAD_ATTR"]
+from orm.dissambler import TreeInstruction
 
 
 class QuerySelector[T](IQuery):
@@ -58,23 +54,17 @@ class QuerySelector[T](IQuery):
         select_list: Optional[Callable[[T], None] | Iterable[Callable[[T], None]]] = None,
         where: Optional[Callable[[T, T2], bool] | Iterable[Callable[[T, T2], bool]]] = None,
     ) -> None:
-        self._select_list: Optional[Callable[[T], None] | Iterable[Callable[[T], None]]] = select_list
-        self._select_str: str = self._convert_select_list(select_list)
+        _dis = TreeInstruction(dis.Bytecode(select_list), "list").to_list()
+        self._select_list: Optional[Callable[[T], None] | Iterable[Callable[[T], None]]] = [x.nested_element.name for x in _dis]
+        self._select_str: str = self._convert_select_list(self._select_list)
         self._orig_table: Table = orig_table
         self._where: WhereCondition = WhereCondition[T, T2](where) if where else None
 
     def _convert_select_list(self, select_list: Callable[[T], None] | Iterable[Callable[[T], None]]) -> str:
         if select_list is None:
             return "*"
-
-        if callable(select_list):
-            return get_var_from_lambda(select_list)
-
-        if isinstance(select_list, Iterable) and not isinstance(select_list, str):
-            return ", ".join([get_var_from_lambda(x) for x in select_list])
-
         else:
-            raise ValueError(f"'{type(select_list)}' is not valid value")
+            return ", ".join(select_list)
 
     @property
     @abstractmethod
@@ -82,7 +72,6 @@ class QuerySelector[T](IQuery):
 
     @override
     def load(self) -> str:
-        _dis = Dissambler(self._select_list)
         condition = ""
         if self._where:
             condition = " " + self._where.to_query()
