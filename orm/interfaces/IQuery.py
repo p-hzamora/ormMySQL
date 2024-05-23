dc7753dea7346e82edc876499e0d338c0f34931c
p-hzamora <p.hzamora@icloud.com>
2024-05-23 12:51:09 +0200

    fix: cleaned IQuery interface

--- orm/interfaces/IQuery.py
+++ orm/interfaces/IQuery.py
@@ -2,11 +2,6 @@ from abc import abstractmethod, ABC
 
 
 class IQuery(ABC):
-    SEMICOLON = ";"
-
-    @abstractmethod
-    def load(self) -> str: ...
-
     @property
     @abstractmethod
     def query(self) -> str: ...
