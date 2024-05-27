bb7c521ad5b292950e6a462b76341cacea5aaae7
p-hzamora <p.hzamora@icloud.com>
2024-05-27 15:41:44 +0200

    update: Table adding TableMeta metaclass

--- orm/orm_objects/table.py
+++ orm/orm_objects/table.py
@@ -1,12 +1,34 @@
-from abc import ABC
 import json
-# from .foreign_key import ForeignKey
+from typing import Any, dataclass_transform
+from .column import Column
 
 
-class Table(ABC):
+class TableMeta(type):
+    def __new__(cls, name: str, bases: tuple, dct: dict[str, Any]):
+        annotations = dct.get("__annotations__", {})
+        fields = list(annotations.keys())
+
+        if "__init__" not in dct:
+
+            def __init__(self, *args, **kwargs):
+                for field, arg in zip(fields, args):
+                    attr: Column = getattr(self, field)
+                    attr.column_name = arg
+
+                for field, value in kwargs.items():
+                    attr: Column = getattr(self, field)
+                    attr.column_name = value
+
+            dct["__init__"] = __init__
+
+        cls_object = super().__new__(cls, name, bases, dct)
+        cls_object.__annotations__ = annotations
+        return cls_object
+
+
+@dataclass_transform(eq_default=False)
+class Table(metaclass=TableMeta):
     __table_name__: str = ...
-
-    # foreign_key: ForeignKey = ForeignKey
 
     def __repr__(self) -> str:
         params = ", ".join([f"{x}={getattr(self,x)}" for x, y in self.__class__.__dict__.items() if isinstance(y, property)])
