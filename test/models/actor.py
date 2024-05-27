16a7797194cda386ee704449e0c2b972b9eb62e5
p-hzamora <p.hzamora@icloud.com>
2024-05-27 15:42:04 +0200

    update: models

--- test/models/actor.py
+++ test/models/actor.py
@@ -1,7 +1,6 @@
 from orm import (
     Column,
     Table,
-    nameof,
     ModelBase,
     IRepositoryBase,
 )
@@ -12,43 +11,10 @@ from datetime import datetime
 class Actor(Table):
     __table_name__ = "actor"
 
-    def __init__(self, actor_id: int, first_name: str, last_name: str, last_update: datetime) -> None:
-        self._actor_id: Column[int] = Column(nameof(actor_id), actor_id, is_primary_key=True)
-        self._first_name: Column[str] = Column(nameof(first_name), first_name)
-        self._last_name: Column[str] = Column(nameof(last_name), last_name)
-        self._last_update: Column[datetime] = Column(nameof(last_update), last_update)
-
-    @property
-    def actor(self) -> int:
-        return self._actor_id.column_value
-
-    @actor.setter
-    def actor(self, value: int) -> None:
-        self._actor_id.column_value = value
-
-    @property
-    def first_name(self) -> str:
-        return self._first_name.column_value
-
-    @first_name.setter
-    def first_name(self, value: str) -> None:
-        self._first_name.column_value = value
-
-    @property
-    def last_name(self) -> str:
-        return self._last_name.column_value
-
-    @last_name.setter
-    def last_name(self, value: str) -> None:
-        self._last_name.column_value = value
-
-    @property
-    def last_update(self) -> str:
-        return self._last_update.column_value
-
-    @last_update.setter
-    def last_update(self, value: str) -> None:
-        self._last_update.column_value = value
+    actor_id: int = Column[int](is_primary_key=True)
+    first_name: str = Column[str]()
+    last_name: str = Column[str]()
+    last_update: datetime = Column[datetime]()
 
 
 class ActorModel(ModelBase[Actor]):
--- test/models/city.py
+++ test/models/city.py
@@ -1,7 +1,7 @@
+from typing import Self
 from orm import (
     Column,
     Table,
-    nameof,
     ModelBase,
     IRepositoryBase,
     ForeignKey,
@@ -15,55 +15,12 @@ from .country import Country
 class City(Table):
     __table_name__ = "city"
 
-    def __init__(
-        self,
-        city: str,
-        country_id: int,
-        city_id: int = None,
-        last_update: datetime = None,
-    ) -> None:
-        self._city_id: Column[int] = Column(nameof(city_id), city_id, is_primary_key=True)
-        self._city: Column[str] = Column(nameof(city), city)
-        self._country_id: Column[str] = Column(nameof(country_id), country_id)
-        self._last_update: Column[datetime] = Column(nameof(last_update), last_update)
+    city_id: int = Column[int](is_primary_key=True)
+    city: str = Column[str]()
+    country_id: str = Column[str]()
+    last_update: datetime = Column[datetime]()
 
-        self.country: Country = ForeignKey[City,Country](
-            orig_table=City,
-            referenced_table=Country,
-            relationship= lambda ci,co: ci.country_id == co.country_id
-        )
-
-    @property
-    def city_id(self) -> int:
-        return self._city_id.column_value
-
-    @city_id.setter
-    def city_id(self, value: int) -> None:
-        self._city_id.column_value = value
-
-    @property
-    def city(self) -> str:
-        return self._city.column_value
-
-    @city.setter
-    def city(self, value: str) -> None:
-        self._city.column_value = value
-
-    @property
-    def country_id(self) -> int:
-        return self._country_id.column_value
-
-    @country_id.setter
-    def country_id(self, value: int) -> None:
-        self._country_id.column_value = value
-
-    @property
-    def last_update(self) -> str:
-        return self._last_update.column_value
-
-    @last_update.setter
-    def last_update(self, value: str) -> None:
-        self._last_update.column_value = value
+    country = ForeignKey[Self, Country](__table_name__, Country, lambda ci, co: ci.country_id == co.country_id)
 
 
 class CityModel(ModelBase[City]):
