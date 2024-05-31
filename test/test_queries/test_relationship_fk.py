d174d5d350d0a2e65e0cd8cbc7b356aab89fb31e
phzamora <p.hzamora@icloud.com>
2024-05-31 18:57:18 +0200

    test: updated test_relationship_fk.py

--- test/test_queries/test_relationship_fk.py
+++ test/test_queries/test_relationship_fk.py
@@ -6,7 +6,8 @@ from datetime import datetime
 
 sys.path = [str(Path(__file__).parent.parent.parent), *sys.path]
 
-from test.models import Address# noqa: E402
+from orm.orm_objects.foreign_key import ForeignKey  # noqa: E402
+from test.models import Address,City,Country  # noqa: E402
 from orm.orm_objects.queries import SelectQuery  # noqa: E402
 
 
@@ -14,17 +15,10 @@ class TestFK(unittest.TestCase):
     def test_fk(self):
         a = Address(1, "panizo", None, "tetuan", 28900, 26039, "617128992", "Madrid", datetime.now())
 
-        aa = Address()
 
-        self.assertTrue(a.address_id, 1)
-        self.assertTrue(a.address, "panizo")
-        self.assertTrue(a.address2, None)
-        self.assertTrue(a.district, "tetuan")
-        self.assertTrue(a.city_id, 28900)
-        self.assertTrue(a.postal_code, 26039)
-        self.assertTrue(a.phone, "617128992")
-        self.assertTrue(a.location, "Madrid")
-        self.assertTrue(a.last_update, datetime.now())
+        mapped = ForeignKey.MAPPED
+        self.assertEqual(mapped[Address.__table_name__]["city_id"].col,"city_id")
+
 
         SelectQuery[Address](Address, lambda x: (x.address, x.city))
 
