fedee76e3a6843a4a9e2f4dab979b1c2d647a3a5
p-hzamora <p.hzamora@icloud.com>
2024-05-31 08:31:37 +0200

    test: updated test_relationship_fk.py

--- test/test_queries/test_relationship_fk.py
+++ test/test_queries/test_relationship_fk.py
@@ -6,9 +6,8 @@ from datetime import datetime
 
 sys.path = [str(Path(__file__).parent.parent.parent), *sys.path]
 
-from test.models import Address, Country, City  # noqa: E402
-from orm.orm_objects.foreign_key import ForeignKey  # noqa: E402
-from orm.orm_objects.queries import SelectQuery
+from test.models import Address# noqa: E402
+from orm.orm_objects.queries import SelectQuery  # noqa: E402
 
 
 class TestFK(unittest.TestCase):
@@ -17,18 +16,18 @@ class TestFK(unittest.TestCase):
 
         aa = Address()
 
-        
-        self.assertTrue(a.address_id,1)
-        self.assertTrue(a.address,"panizo")
-        self.assertTrue(a.address2,None)
-        self.assertTrue(a.district,"tetuan")
-        self.assertTrue(a.city_id,28900)
-        self.assertTrue(a.postal_code,26039)
-        self.assertTrue(a.phone,"617128992")
-        self.assertTrue(a.location,"Madrid")
-        self.assertTrue(a.last_update,datetime.now())
-        
-        SelectQuery[Address](Address,lambda x: (x.address,x.city))
+        self.assertTrue(a.address_id, 1)
+        self.assertTrue(a.address, "panizo")
+        self.assertTrue(a.address2, None)
+        self.assertTrue(a.district, "tetuan")
+        self.assertTrue(a.city_id, 28900)
+        self.assertTrue(a.postal_code, 26039)
+        self.assertTrue(a.phone, "617128992")
+        self.assertTrue(a.location, "Madrid")
+        self.assertTrue(a.last_update, datetime.now())
+
+        SelectQuery[Address](Address, lambda x: (x.address, x.city))
+
 
 if __name__ == "__main__":
     unittest.main()
