9c61fb2cc6b2cb2660895e5b65577887a547e7af
phzamora <p.hzamora@icloud.com>
2024-05-31 18:56:36 +0200

    fix: test_select.py

--- test/test_queries/test_select.py
+++ test/test_queries/test_select.py
@@ -12,27 +12,22 @@ class TestQuery(unittest.TestCase):
     def test_select_all_col_with_no_select_list_attr(self):
         q = SelectQuery[City](City)
         self.assertEqual(q.query, "SELECT * FROM city")
-        self.assertEqual(q.load(), "SELECT * FROM city;")
 
     def test_select_all_col_with_select_list_attr(self):
         q = SelectQuery[City](City,lambda: "*")
         self.assertEqual(q.query, "SELECT * FROM city")
-        self.assertEqual(q.load(), "SELECT * FROM city;")
 
     def test_select_one_col(self):
         q = SelectQuery[City](City, lambda c: c.city)
         self.assertEqual(q.query, "SELECT city FROM city")
-        self.assertEqual(q.load(), "SELECT city FROM city;")
 
     def test_select_two_col(self):
         q = SelectQuery[City](City, lambda c: (c.city, c.city_id))
         self.assertEqual(q.query, "SELECT city, city_id FROM city")
-        self.assertEqual(q.load(), "SELECT city, city_id FROM city;")
 
     def test_select_three_col(self):
         q = SelectQuery[City](City, lambda c: (c.city, c.last_update, c.country_id))
         self.assertEqual(q.query, "SELECT city, last_update, country_id FROM city")
-        self.assertEqual(q.load(), "SELECT city, last_update, country_id FROM city;")
 
     def test_select_cols_from_foreign_keys(self):
         q = SelectQuery[Address](Address, lambda a: (a.city.country.country))
