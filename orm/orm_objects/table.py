08b4d136a246790be5f4faa3b2ea8e80eb1369cb
p-hzamora <p.hzamora@icloud.com>
2024-05-31 08:54:03 +0200

    update: table.py

--- orm/orm_objects/table.py
+++ orm/orm_objects/table.py
@@ -1,34 +1,17 @@
 import json
 from typing import Any, dataclass_transform
-from .column import Column
+from .table_constructor.table_constructor import __init_constructor__
 
-
+@dataclass_transform()
 class TableMeta(type):
     def __new__(cls, name: str, bases: tuple, dct: dict[str, Any]):
-        annotations = dct.get("__annotations__", {})
-        fields = list(annotations.keys())
-
-        if "__init__" not in dct:
-
-            def __init__(self, *args, **kwargs):
-                for field, arg in zip(fields, args):
-                    attr: Column = getattr(self, field)
-                    attr.column_name = arg
-
-                for field, value in kwargs.items():
-                    attr: Column = getattr(self, field)
-                    attr.column_name = value
-
-            dct["__init__"] = __init__
-
         cls_object = super().__new__(cls, name, bases, dct)
-        cls_object.__annotations__ = annotations
-        return cls_object
+        return __init_constructor__(cls_object)
 
 
 @dataclass_transform(eq_default=False)
 class Table(metaclass=TableMeta):
-    __table_name__: str = ...
+    __table_name__ = ...
 
     def __repr__(self) -> str:
         params = ", ".join([f"{x}={getattr(self,x)}" for x, y in self.__class__.__dict__.items() if isinstance(y, property)])
