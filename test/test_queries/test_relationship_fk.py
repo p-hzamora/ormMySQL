ad9c1deb3380050a58f5dee1b1d639b026e003e7
phzamora <p.hzamora@icloud.com>
2024-06-01 14:40:14 +0200

    test: wip test_relationship_fk

--- test/test_queries/test_relationship_fk.py
+++ test/test_queries/test_relationship_fk.py
@@ -7,7 +7,7 @@ from datetime import datetime
 sys.path = [str(Path(__file__).parent.parent.parent), *sys.path]
 
 from orm.orm_objects.foreign_key import ForeignKey  # noqa: E402
-from test.models import Address,City,Country  # noqa: E402
+from test.models import Address, City, Country  # noqa: E402
 from orm.orm_objects.queries import SelectQuery  # noqa: E402
 
 
@@ -15,10 +15,8 @@ class TestFK(unittest.TestCase):
     def test_fk(self):
         a = Address(1, "panizo", None, "tetuan", 28900, 26039, "617128992", "Madrid", datetime.now())
 
-
         mapped = ForeignKey.MAPPED
-        self.assertEqual(mapped[Address.__table_name__]["city_id"].col,"city_id")
-
+        self.assertEqual(mapped[Address.__table_name__]["city_id"].col, "city_id")
 
         SelectQuery[Address](Address, lambda x: (x.address, x.city))
 
