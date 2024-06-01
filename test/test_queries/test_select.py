dd0ab0d9aa5815385b3a2613044073e40d1b44cf
phzamora <p.hzamora@icloud.com>
2024-06-01 14:39:59 +0200

    test: commented wip test in test_select.py

--- test/test_queries/test_select.py
+++ test/test_queries/test_select.py
@@ -29,9 +29,11 @@ class TestQuery(unittest.TestCase):
         q = SelectQuery[City](City, lambda c: (c.city, c.last_update, c.country_id))
         self.assertEqual(q.query, "SELECT city, last_update, country_id FROM city")
 
-    def test_select_cols_from_foreign_keys(self):
-        q = SelectQuery[Address](Address, lambda a: (a.city.country.country))
-        self.assertEqual(q.query, "SELECT city.city_id, address.last_update, address.country_id FROM address")
+    # def test_select_cols_from_foreign_keys(self):
+    #     # this response must not be the real one, 
+    #     address = Address(1, "panizo", None, "tetuan", 28900, 26039, "617128992", "Madrid")
+    #     q = SelectQuery[Address](address, lambda a: (a.city.country.country))
+    #     self.assertEqual(q.query, "SELECT city.city_id, address.last_update, address.country_id FROM address")
 
 
 if "__main__" == __name__:
