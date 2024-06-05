6d9ec41cf05c6184ae0ede52e8b199d11bafcaa9
phzamora <p.hzamora@icloud.com>
2024-06-05 09:39:48 +0200

    update: foreign_key

--- orm/orm_objects/foreign_key.py
+++ orm/orm_objects/foreign_key.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
-from typing import Callable, NamedTuple
+from typing import Callable, NamedTuple,Type
 
-from orm.dissambler import Dissambler
 from .table import Table
 
 
@@ -11,28 +10,19 @@ class RelationShip[T: Table](NamedTuple):
 
 
 class ForeignKey[Tbl1, Tbl2]:
-    MAPPED: dict[str, dict[str, RelationShip]] = defaultdict(dict)
+    MAPPED: dict[str, dict[str, Callable[[Tbl1, Tbl2], bool]]] = defaultdict(dict)
 
     def __new__(
         cls,
         orig_table: str,
-        referenced_table,
+        referenced_table: Type[Tbl2],
         relationship: Callable[[Tbl1, Tbl2], bool],
     ) -> Tbl2:
-        dissambler_functions: Dissambler[Tbl1, Tbl2] = Dissambler[Tbl1, Tbl2](relationship)
-
-        cls.add_foreign_key(orig_table, referenced_table, dissambler_functions)
+        cls.add_foreign_key(orig_table, referenced_table, relationship)
 
         return referenced_table
 
     @classmethod
-    def add_foreign_key(cls, orig_table: str, referenced_table: str, relationship: Dissambler[Tbl1, Tbl2]) -> None:
-        fk_orig_table = relationship.cond_1.name
-        referenced_column = relationship.cond_2.name
-
-        if fk_orig_table in cls.MAPPED[orig_table]:
-            return None
-            raise KeyError(f"'{fk_orig_table}' already in '{orig_table}' table as foregin key")
-
-        cls.MAPPED[orig_table][fk_orig_table] = RelationShip(referenced_column, referenced_table)
+    def add_foreign_key(cls, orig_table: str, referenced_table: Table, relationship: Callable[[Tbl1, Tbl2], bool]) -> None:
+        cls.MAPPED[orig_table][referenced_table.__table_name__] = relationship
         return None
