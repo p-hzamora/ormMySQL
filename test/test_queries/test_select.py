63e5c64b62dd3165d8411798c91c52af1410beef
phzamora <p.hzamora@icloud.com>
2024-06-06 00:39:06 +0200

    wip: error in test_select

--- test/test_queries/test_select.py
+++ test/test_queries/test_select.py
@@ -15,7 +15,6 @@ from test.models import (  # noqa: E402
     Country,
     CountryModel,
 )
-from orm import Table
 
 
 
@@ -30,25 +29,22 @@ class TestQuery(unittest.TestCase):
 
     def test_select_one_col(self):
         q = SelectQuery[City](City, lambda c: c.city)
-        self.assertEqual(q.query, "SELECT city FROM city")
+        self.assertEqual(q.query, "SELECT city.city FROM city")
 
     def test_select_two_col(self):
         q = SelectQuery[City](City, lambda c: (c.city, c.city_id))
-        self.assertEqual(q.query, "SELECT city, city_id FROM city")
+        self.assertEqual(q.query, "SELECT city.city, city.city_id FROM city")
 
     def test_select_three_col(self):
         q = SelectQuery[City](City, lambda c: (c.city, c.last_update, c.country_id))
-        self.assertEqual(q.query, "SELECT city, last_update, country_id FROM city")
+        self.assertEqual(q.query, "SELECT city.city, city.last_update, city.country_id FROM city")
 
     def test_select_cols_from_foreign_keys(self):
         # this response must not be the real one,
-        a = SelectQuery(Address, City, Country, lambda a, ci, co: (a.address, ci.city_id, co.last_update))
-
-        pass
 
         address = Address(1, "panizo", None, "tetuan", 28900, 26039, "617128992", "Madrid")
-        q = SelectQuery[Address](address, lambda a: (a.city.country.country))
-        self.assertEqual(q.query, "SELECT city.city_id, address.last_update, address.country_id FROM address")
+        q = SelectQuery[Address](address, lambda a: (a.city_id, a.last_update, a.city, a.city.country.country_id))
+        self.assertEqual(q.query, "SELECT address.city_id, address.last_update, city.*, country.country_id FROM address")
 
 
 if "__main__" == __name__:
