0a85be0daa7dc3752aaf39d5a38b0a500f8ad719
p-hzamora <p.hzamora@icloud.com>
2024-06-06 15:19:56 +0200

    update: select.py

--- orm/orm_objects/queries/select.py
+++ orm/orm_objects/queries/select.py
@@ -1,68 +1,82 @@
 from typing import Callable, Optional, Iterable, Type
 import dis
 
+import inspect
 from orm.dissambler import NestedElement
 from orm.orm_objects import Table
+from orm.orm_objects.table.table_constructor import TableMeta
 
 from .query import IQuery
 from orm.dissambler import TreeInstruction, TupleInstruction
 
 
-class SelectQuery[T: Table](IQuery):
+class SelectQuery[T: Table, *Ts](IQuery):
     def __init__(
         self,
-        table: T,
-        select_list: Optional[Callable[[T], None]] = lambda: None,
+        *tables: tuple[T, *Ts],
+        select_list: Optional[Callable[[T, *Ts], None]] = lambda: None,
     ) -> None:
-        self._table: T = table
-        self._select_list: Iterable[str] = self._make_column_list(select_list)
-        self._different_columns: Iterable[Table] = []
+        self._first_table: T = tables[0]
+        self._tables: tuple[T, *Ts] = tables
 
-    def _make_column_list(self, select_list: Optional[Callable[[T], None]]) -> Iterable[str]:
+        self._transform_lambda_variables: dict[str, Table] = {}
+        self._select_list: Iterable[tuple[str,str]] = self._make_column_list(select_list)
+
+    def _make_column_list(self, select_list: Optional[Callable[[T], None]]) -> Iterable[tuple[str,str]]:
+        def _get_parents(tbl_obj: Table, tuple_inst: TupleInstruction) -> None:
+            """
+            Recursive function tu replace variable names by Select Query
+            """
+            last_el = tuple_inst.nested_element.name
+            parents = tuple_inst.nested_element.parents
+
+            if issubclass(tbl_obj.__class__, Table | TableMeta) and len(parents) == 1:
+                # if parents length is 1 says that the element is the table itself
+                return res.append((tbl_obj.__table_name__, "*"))
+
+            first_el = tuple_inst.nested_element.parents[1]
+            try:
+                new_obj = getattr(tbl_obj.__class__, first_el)
+            except Exception:
+                new_obj = getattr(tbl_obj, first_el, None)
+
+            if last_el not in tbl_obj.__dict__ or not isinstance(new_obj, property):
+                if isinstance(new_obj, property):
+                    return res.append((tbl_obj.__table_name__, last_el))
+
+                new_ti = TupleInstruction(first_el, NestedElement[str](parents[1:]))
+                return _get_parents(new_obj, new_ti)
+            return res.append((tbl_obj.__table_name__, last_el))
+
         instruction_list: list[TupleInstruction] = TreeInstruction(dis.Bytecode(select_list), list).to_list()
-
         res = []
-        obj: Type[T] = self._table
-        for tuple_instruction in instruction_list:
-            last_nested_element: str = tuple_instruction.nested_element.name
-            if hasattr(obj, tuple_instruction.nested_element.name):
-                value: Table | property = getattr(obj, last_nested_element)
 
-                if isinstance(value, property):  # if is property we gonna get column of the initial table
-                    res.append(f"{obj.__table_name__}.{last_nested_element}")
+        lambda_vars = tuple(inspect.signature(select_list).parameters)
 
-                elif issubclass(value, Table):
-                    for children in tuple_instruction.nested_element.parents:
-                        if hasattr(value, children):
-                            ...
-                    res.append(f"{value.__table_name__}.*")
-                else:
-                    raise Exception
-            else:
-                external_table: str = self._get_parent_table_name(self._table, tuple_instruction.nested_element.parents[1:])
-                res.append(f"{external_table}.{tuple_instruction.nested_element.name}")
+        self._update_transform_lambda_variables(lambda_vars)
 
+        for ti in instruction_list:
+            obj = self._transform_lambda_variables[ti.var]
+
+            var = obj.__table_name__
+            new_nested = ti.nested_element.parents
+            new_nested[0] = var
+            ti = TupleInstruction(var, NestedElement(new_nested))
+            _get_parents(obj, ti)
         return res
 
-    @classmethod
-    def _get_parent_table_name(cls, table: Type[Table], nested_string: list[str]) -> str:
-        for name in nested_string:
-            if hasattr(table, name):
-                new_table: Table | property = getattr(table, name)
-                # To avoid confusion, ensure that column names and table names are clearly distinct.
-                if not isinstance(new_table, property):
-                    new_nested_string = nested_string[nested_string.index(name) + 1 :]
-                    return cls._get_parent_table_name(new_table, new_nested_string)
-            return table.__table_name__
+    def _update_transform_lambda_variables(self, lambda_vars: tuple[str, ...]):
+        for i in range(len(lambda_vars)):
+            self._transform_lambda_variables[lambda_vars[i]] = self._tables[i]
 
     def _convert_select_list(self) -> str:
         if not self._select_list:
             return "*"
         else:
-            return ", ".join(self._select_list)
+            return ", ".join(".".join(x) for x in self._select_list)
 
     @property
     def query(self) -> str:
         select_str = self._convert_select_list()
-        query = f"SELECT {select_str} FROM {self._table.__table_name__}"
+        query = f"SELECT {select_str} FROM {self._first_table.__table_name__}"
         return query
