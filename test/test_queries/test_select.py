b35886fe0eae46c141d72028eb85c15d19c305cb
p-hzamora <p.hzamora@icloud.com>
2024-06-06 15:20:34 +0200

    test: updated test_select.py

--- test/test_queries/test_select.py
+++ test/test_queries/test_select.py
@@ -7,15 +7,9 @@ sys.path = [str(Path(__file__).parent.parent.parent), *sys.path]
 from orm.orm_objects.queries.select import SelectQuery  # noqa: E402
 from test.models import (  # noqa: E402
     City,
-    CityModel,
     Address,
-    AddressModel,
-    Actor,
-    ActorModel,
     Country,
-    CountryModel,
 )
-
 
 
 class TestQuery(unittest.TestCase):
@@ -24,27 +18,62 @@ class TestQuery(unittest.TestCase):
         self.assertEqual(q.query, "SELECT * FROM city")
 
     def test_select_all_col_with_select_list_attr(self):
-        q = SelectQuery[City](City, lambda: "*")
+        q = SelectQuery[City](City, select_list=lambda: "*")
         self.assertEqual(q.query, "SELECT * FROM city")
 
     def test_select_one_col(self):
-        q = SelectQuery[City](City, lambda c: c.city)
+        q = SelectQuery[City](City, select_list=lambda c: c.city)
         self.assertEqual(q.query, "SELECT city.city FROM city")
 
     def test_select_two_col(self):
-        q = SelectQuery[City](City, lambda c: (c.city, c.city_id))
+        q = SelectQuery[City](City, select_list=lambda c: (c.city, c.city_id))
         self.assertEqual(q.query, "SELECT city.city, city.city_id FROM city")
 
     def test_select_three_col(self):
-        q = SelectQuery[City](City, lambda c: (c.city, c.last_update, c.country_id))
+        q = SelectQuery[City](City, select_list=lambda c: (c.city, c.last_update, c.country_id))
         self.assertEqual(q.query, "SELECT city.city, city.last_update, city.country_id FROM city")
 
     def test_select_cols_from_foreign_keys(self):
         # this response must not be the real one,
+        q = SelectQuery[Address](
+            Address,
+            select_list=lambda a: (
+                a,
+                a.city,
+                a.city.country,
+                a.city.country_id,
+                a.city_id,
+                a.last_update,
+                a.city.country.country,
+            ),
+        )
+        self.assertEqual(q.query, "SELECT address.*, city.*, country.*, city.country_id, address.city_id, address.last_update, country.country FROM address")
 
-        address = Address(1, "panizo", None, "tetuan", 28900, 26039, "617128992", "Madrid")
-        q = SelectQuery[Address](address, lambda a: (a.city_id, a.last_update, a.city, a.city.country.country_id))
-        self.assertEqual(q.query, "SELECT address.city_id, address.last_update, city.*, country.country_id FROM address")
+    def test_select_all_columns_from_all_tables(self):
+        # this response must not be the real one,
+        q = SelectQuery[Address, City, Country](
+            Address,
+            City,
+            Country,
+            select_list=lambda a, ci, co: (
+                a,
+                ci,
+                co,
+            ),
+        )
+        self.assertEqual(q.query, "SELECT address.*, city.*, country.* FROM address")
+
+    def test_select_all_columns_from_tables_without_use_all_vars(self):
+        # this response must not be the real one,
+        q = SelectQuery[Address, City, Country](
+            Address,
+            City,
+            select_list=lambda a, ci: (
+                a,
+                ci.country,
+            ),
+        )
+        self.assertEqual(q.query, "SELECT address.*, country.* FROM address")
 
 
 if "__main__" == __name__:
