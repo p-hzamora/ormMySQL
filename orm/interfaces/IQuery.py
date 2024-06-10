c0b93f65b3facd2494f17eb6bdf7d588d932e12d
p-hzamora <p.hzamora@icloud.com>
2024-06-10 12:56:39 +0200

    chore: added "__repr__" method in IQuery

--- orm/interfaces/IQuery.py
+++ orm/interfaces/IQuery.py
@@ -5,3 +5,6 @@ class IQuery(ABC):
     @property
     @abstractmethod
     def query(self) -> str: ...
+
+    def __repr__(self) -> str:
+        return f"{IQuery.__name__}: {self.__class__.__name__}"
