3f00b568dd5762fad38c4bd13097688a0cb366f2
p-hzamora <p.hzamora@icloud.com>
2024-06-10 10:11:46 +0200

    wip: WhereCondition class

--- orm/orm_objects/queries/where_condition.py
+++ orm/orm_objects/queries/where_condition.py
@@ -1,4 +1,4 @@
-from typing import Callable, override
+from typing import Any, Callable, override
 
 from orm.dissambler.tree_instruction import TreeInstruction
 from orm.interfaces.IQuery import IQuery
@@ -14,7 +14,7 @@ class WhereCondition[*Inst](IQuery):
 
     def __init__(
         self,
-        instances: tuple[*Inst],
+        *instances: tuple[*Inst],
         lambda_function: Callable[[*Inst], bool],
     ) -> None:
         self._instances: tuple[*Inst] = instances
@@ -25,20 +25,29 @@ class WhereCondition[*Inst](IQuery):
     @override
     @property
     def query(self) -> str:
+        return self._select_type_condition()
+
+
+    def _select_type_condition(self)->Callable[[],Any]:
         n = len(self._tree.compare_op)
+        dicc_selector:dict[int,Callable[[],str]] = {
+            1:self.__comparable,
+            2: self.__between_condition
+        }
+        return dicc_selector[n]()
+        
 
-        if n == 2:
-            return self.__between_condition()
-        else:
-            query: str = self.WHERE
-            for val in self._tree.to_list():
-                nested_element = val.nested_element
-                query += nested_element
-            return query
-
-
+    def __comparable(self)->str:
+        c1,c2 = [x.nested_element.name for x in self._tree.to_list()]
+        return f"{self.WHERE} {c1} {self._tree.compare_op[0]} {c2}"
+    
     def __between_condition(self) -> str:
-        return
+        query: str = self.WHERE
+        c1,c2,c3 = [x.nested_element.name for x in self._tree.to_list()]
+        for val in self._tree.to_list():
+            nested_element = val.nested_element
+            query += nested_element
+        return query
 
     @classmethod
     def join_condition(cls, *args: "WhereCondition", restrictive=False) -> str:
