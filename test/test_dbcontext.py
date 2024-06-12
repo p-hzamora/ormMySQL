d08071360a5479639ec9d7ba0a47708d0139520e
phzamora <p.hzamora@icloud.com>
2024-06-12 21:27:53 +0200

    wip: test_dbcontext

--- test/test_dbcontext.py
+++ test/test_dbcontext.py
@@ -3,9 +3,9 @@ import dbcontext as db
 
 from orm import MySQLRepository, IRepositoryBase
 
-from orm.condition_types import ConditionType
 from test.models import AddressModel
 from test.models.store import StoreModel, Store
+from test.models.staff import StaffModel, Staff
 
 from dotenv import load_dotenv
 
@@ -21,16 +21,15 @@ HOST = "localhost"  # os.getenv("DB_HOST")
 
 database: IRepositoryBase = MySQLRepository(user=USERNAME, password=PASSWORD, database="sakila", host=HOST).connect()
 
-address = db.Address(address_id=10)
+# address = db.Address(address_id=10)
 
-a_model = AddressModel(database)
+# a_model = AddressModel(database)
 
 
-filter_by_address_id = a_model.where(lambda x: x.address_id == 1).select()
+# filter_by_address_id = a_model.where(lambda x: x.address_id == 1).select()
 
-store = Store(None,3,603,datetime.now())
-a = StoreModel(database).insert(store)
+# store = Store(None,3,603,datetime.now())
+# a = StoreModel(database).insert(store)
 
-a_model.where(lambda x: x.address_id == 3).delete()
-
+staff = StaffModel(database).where(lambda x: x.staff_id == 1).select()
 pass
