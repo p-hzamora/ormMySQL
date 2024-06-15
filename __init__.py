b2a01282ca267ccd5af2b58bcec9fad696897b70
phzamora <p.hzamora@icloud.com>
2024-06-15 19:19:41 +0200

    chore: added orm into sys.path

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent))
+
 from .orm.interfaces import IRepositoryBase  # noqa: F401
 from .orm.repository_my_sql import MySQLRepository, errorcode, Error  # noqa: F401
 from .orm.model_base import ModelBase  # noqa: F401
