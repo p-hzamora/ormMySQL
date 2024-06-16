05444381820bb133653c5d38e7bacc7a302d16c7
phzamora <p.hzamora@icloud.com>
2024-06-16 14:21:03 +0200

    fix: avoid  adding path into sys.path list

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent))
-
 from .orm.interfaces import IRepositoryBase  # noqa: F401
 from .orm.repository_my_sql import MySQLRepository, errorcode, Error  # noqa: F401
 from .orm.model_base import ModelBase  # noqa: F401
