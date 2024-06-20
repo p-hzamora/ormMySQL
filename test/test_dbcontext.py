a192140d20106f0240c24ec625b0061c1f89510f
p-hzamora <p.hzamora@icloud.com>
2024-06-20 12:20:20 +0200

    wip: test_dbcontext.py

--- test/test_dbcontext.py
+++ test/test_dbcontext.py
@@ -1,35 +1,49 @@
-from datetime import datetime
-import dbcontext as db
+import os
 
 from orm import MySQLRepository, IRepositoryBase
-
-from test.models import AddressModel
-from test.models.store import StoreModel, Store
-from test.models.staff import StaffModel, Staff
+from orm.condition_types import ConditionType
+from test.models.address import AddressModel
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-USERNAME = "root"  # os.getenv("DB_USERNAME")
-PASSWORD = "1234"  # os.getenv("DB_PASSWORD")
-HOST = "localhost"  # os.getenv("DB_HOST")
+USERNAME = os.getenv("DB_USERNAME")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOST = os.getenv("DB_HOST")
 
-
-# db_common = MySQLRepository(user=USERNAME, password=PASSWORD, database="common", host=HOST).connect()
 
 database: IRepositoryBase = MySQLRepository(user=USERNAME, password=PASSWORD, database="sakila", host=HOST).connect()
 
-# address = db.Address(address_id=10)
 
-# a_model = AddressModel(database)
+a_model = AddressModel(database)
+address = (
+    a_model.where(lambda a: 177 >= a.address_id >= 28)
+    .where(lambda x: (x.city.country.country, ConditionType.REGEXP, r"^[FHA]"))
+    .where(lambda x: (x.city.city, ConditionType.REGEXP, r"[^W]"))
+    .order(lambda a: a.address_id, order_type="DESC")
+    .select(
+        lambda a: (
+            a.address_id,
+            a.city.country.country,
+            a.city.city,
+            a.address,
+        ),
+        flavour=tuple,
+    )
+)
 
-
-# filter_by_address_id = a_model.where(lambda x: x.address_id == 1).select()
-
-# store = Store(None,3,603,datetime.now())
-# a = StoreModel(database).insert(store)
-
-staff = StaffModel(database).where(lambda x: x.staff_id == 1).select()
+query = """
+SELECT address.address_id as `kkk`, country.country as `country_country`, city.city as `city_city`, address.address as `address_address` FROM address INNER JOIN city ON address.city_id = city.city_id INNER JOIN country ON city.country_id = country.country_id
+WHERE country.country REGEXP '^[FHA]' AND (city.city REGEXP '[^W]') AND (177 >= address.address_id) AND (address.address_id <= 28)
+ORDER BY address.address_id DESC
+"""
+address = a_model._repository.read_sql(query, flavour=dict)
 pass
+
+
+address = a_model.where(lambda x: x.address_id == 1).select()
+
+address.address2 = "primera vivienda"
+a_model.upsert(address)
