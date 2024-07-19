b0d41f6c5cdd7b18b08c1844bbeb6681a5e1798c
phzamora <p.hzamora@icloud.com>
2024-07-19 17:03:35 +0200

    updated: added 'create_query' in ForeignKey class

--- orm/orm_objects/foreign_key.py
+++ orm/orm_objects/foreign_key.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
-from typing import Callable, NamedTuple,Type
+from typing import Callable, NamedTuple, Type
 
 from .table import Table
+from ..dissambler import Dissambler
 
 
 class RelationShip[T: Table](NamedTuple):
@@ -26,3 +27,14 @@ class ForeignKey[Tbl1, Tbl2]:
     def add_foreign_key(cls, orig_table: str, referenced_table: Table, relationship: Callable[[Tbl1, Tbl2], bool]) -> None:
         cls.MAPPED[orig_table][referenced_table.__table_name__] = relationship
         return None
+
+    @classmethod
+    def create_query(cls, orig_table: Table) -> list[str]:
+        clauses: list[str] = []
+        for referenced_table, _lambda in ForeignKey.MAPPED[orig_table.__table_name__].items():
+            dissambler: Dissambler = Dissambler(_lambda)
+            orig_col: str = dissambler.cond_1.name
+            referenced_col: str = dissambler.cond_2.name
+
+            clauses.append(f"FOREIGN KEY ({orig_col}) REFERENCES {referenced_table}({referenced_col})")
+        return clauses
