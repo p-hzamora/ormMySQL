6832957af804f31bb44228cbdc1ab252afb01982
phzamora <p.hzamora@icloud.com>
2024-07-20 00:02:23 +0200

    test: updated file with multiples classes as models

---
+++ test/models/models_in_the_same_file/all_models_in_one_file.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+
+from orm import (
+    Column,
+    Table,
+    ModelBase,
+    IRepositoryBase,
+    ForeignKey,
+)
+
+
+class Country(Table):
+    __table_name__ = "country"
+
+    country_id: int = Column[int](is_primary_key=True)
+    country: str
+    last_update: datetime
+
+
+class CountryModel(ModelBase[Country]):
+    def __init__(self, repository: IRepositoryBase):
+        super().__init__(Country, repository=repository)
+
+
+class City(Table):
+    __table_name__ = "city"
+
+    city_id: int = Column[int](is_primary_key=True)
+    city: str
+    country_id: int
+    last_update: datetime
+
+    country = ForeignKey["City", Country](__table_name__, Country, lambda ci, co: ci.country_id == co.country_id)
+
+
+class CityModel(ModelBase[City]):
+    def __init__(self, repository: IRepositoryBase):
+        super().__init__(City, repository=repository)
+
+
+class Address(Table):
+    __table_name__ = "address"
+
+    address_id: int = Column[int](is_primary_key=True)
+    address: str
+    address2: str
+    district: str
+    city_id: int
+    postal_code: datetime
+    phone: str
+    location: datetime
+    last_update: datetime = Column[datetime](is_auto_generated=True)
+
+    city = ForeignKey["Address", City](__table_name__, City, lambda a, c: a.city_id == c.city_id)
+
+
+class AddressModel(ModelBase[Address]):
+    def __init__(self, repository: IRepositoryBase):
+        super().__init__(Address, repository=repository)
