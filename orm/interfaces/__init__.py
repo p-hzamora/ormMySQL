60cc5725121c89b1db83ec64d83ee0bf4f46d6cc
p-hzamora <p.hzamora@icloud.com>
2024-07-30 21:12:20 +0200

    chore: added clases into __init__.py

--- orm/interfaces/__init__.py
+++ orm/interfaces/__init__.py
@@ -2,3 +2,4 @@ from .IRepositoryBase import IRepositoryBase  # noqa: F401
 from .IQuery import IQuery  # noqa: F401
 from .ISelect import ISelect  # noqa: F401
 from .IStatements import IStatements  # noqa: F401
+from .IDelete import IDelete  # noqa: F401
