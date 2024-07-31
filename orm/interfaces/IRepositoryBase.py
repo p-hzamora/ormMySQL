98f0d18adc52f78c905d21633282ca03398fec55
phzamora <p.hzamora@icloud.com>
2024-07-31 13:51:00 +0200

    fix: adjusted IRepositoryBase interface

--- orm/interfaces/IRepositoryBase.py
+++ orm/interfaces/IRepositoryBase.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from typing import Literal, Optional, Type, overload
+from typing import Optional, Type
 
 
 class IRepositoryBase[T](ABC):
@@ -11,22 +11,13 @@ class IRepositoryBase[T](ABC):
     # def close_connection(self) -> None: ...
 
     @abstractmethod
-    def create_database(self, db_name: str, if_exists: type_exists = "fail") -> bool: ...
-
-    @abstractmethod
-    def drop_database(self, db_name: str) -> bool: ...
-
-    @abstractmethod
-    def drop_table(self, name: str) -> bool: ...
-
-    @overload
-    def read_sql(self, query: str) -> tuple[tuple]: ...
-
-    @overload
-    def executemany_with_values(self, query: str, values) -> None: ...
-    
-    @overload
-    def execute_with_values(self, query: str, values) -> None: ...
-    
-    @overload
     def read_sql[TFlavour](self, query: str, flavour: Optional[Type[TFlavour]], **kwargs) -> tuple[TFlavour]: ...
+
+    @abstractmethod
+    def executemany_with_values(self, query: str, values) -> None: ...
+
+    @abstractmethod
+    def execute_with_values(self, query: str, values) -> None: ...
+
+    @property
+    def connection(self) -> T: ...
