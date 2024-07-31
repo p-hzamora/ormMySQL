4b8c261f1c62edb6e9cba6087c786033eafb030a
phzamora <p.hzamora@icloud.com>
2024-07-31 13:48:46 +0200

    feat: added 'DropDatabase' class

---
+++ orm/databases/my_sql/clauses/drop_database.py
@@ -0,0 +1,18 @@
+from orm.interfaces import IQuery, IRepositoryBase
+from mysql.connector import Error, MySQLConnection
+
+from ..repository import MySQLRepository
+
+
+class DropDatabase(IQuery):
+    DROP: str = "DROP DATABASE IF EXISTS"
+    def __init__(self, repository: IRepositoryBase[MySQLConnection]) -> None:
+        self._repository: IRepositoryBase[MySQLConnection] = repository
+
+    @MySQLRepository._is_connected
+    def execute(self, db_name: str) -> None:
+        try:
+            with self._repository.connection.cursor() as cursor:
+                cursor.execute(f"{self.DROP} {db_name}")
+        except Error as err:
+            raise err
