95d19edebb2405a3df8db481ebdf69cbbec5c968
p-hzamora <p.hzamora@icloud.com>
2024-08-01 11:58:16 +0200

    fix: return Type of abstract class

--- orm/databases/my_sql/statements.py
+++ orm/databases/my_sql/statements.py
@@ -3,7 +3,10 @@ from typing import override, Type
 
 from orm.abstract_model import AbstractSQLStatements
 from orm.utils import Table
-from orm.common.interfaces import IQuery, IRepositoryBase, INonQueryCommand
+from orm.common.interfaces import IQuery, IRepositoryBase
+from orm.components.create_database import CreateDatabaseBase
+from orm.components.drop_database import DropDatabaseBase
+
 from orm.components.select import ISelect
 
 from .clauses import CreateDatabase
@@ -79,10 +82,10 @@ class MySQLStatements[T: Table](AbstractSQLStatements[T, MySQLRepository]):
 
     @property
     @override
-    def DropDatabase(self) -> Type[INonQueryCommand]:
+    def DropDatabase(self) -> Type[DropDatabaseBase]:
         return DropDatabase
 
     @property
     @override
-    def CreateDatabase(self) -> Type[INonQueryCommand]:
+    def CreateDatabase(self) -> Type[CreateDatabaseBase]:
         return CreateDatabase
