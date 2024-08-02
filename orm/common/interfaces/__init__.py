7449ac7661fae7e2f393ef2bf4065ab2db245105
phzamora <p.hzamora@icloud.com>
2024-08-02 14:03:41 +0200

    fix!: It's mandatory to break circular import due to 'INonQueryCommand' depends on IQuery

--- orm/common/interfaces/__init__.py
+++ orm/common/interfaces/__init__.py
@@ -1,4 +1,4 @@
-from .INonQueryCommand import INonQueryCommand  # noqa: F401
 from .IQueryCommand import IQuery  # noqa: F401
+from .INonQueryCommand import INonQueryCommand  # noqa: F401
 from .IRepositoryBase import IRepositoryBase  # noqa: F401
 from .IStatements import IStatements,IStatements_two_generic  # noqa: F401
