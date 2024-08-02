d7ab07d49daead8046bc062cb4af3dcbbefbc000
phzamora <p.hzamora@icloud.com>
2024-08-02 23:48:43 +0200

    update: deleted unnecessary Model

--- test/models/store.py
+++ test/models/store.py
@@ -27,27 +27,3 @@ class Store(Table):
 class StoreModel(ModelBase[Store]):
     def __init__(self, repository: IRepositoryBase):
         super().__init__(Store, repository=repository)
-
-
-class Staff(Table):
-    __table_name__ = "staff"
-
-    staff_id: int = Column[int](is_primary_key=True, is_auto_increment=True)
-    first_name: str
-    last_name: str
-    address_id: str
-    picture: bytes
-    email: str
-    store_id: int
-    active: int
-    username: str
-    password: str
-    last_update: datetime
-
-    Address = ForeignKey[Self, Address](__table_name__, Address, lambda s, a: s.staff_id == a.address_id)
-    Store = ForeignKey[Self, Store](__table_name__, Store, lambda staff, store: staff.staff_id == store.store_id)
-
-
-class StaffModel(ModelBase[Staff]):
-    def __init__(self, repository: IRepositoryBase):
-        super().__init__(Staff, repository=repository)
