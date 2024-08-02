ffcdab74aef3463cd4af9ce361cfe588ea7352ee
phzamora <p.hzamora@icloud.com>
2024-08-02 23:30:09 +0200

    update: ModelBase class

--- orm/common/abstract_classes/model_base.py
+++ orm/common/abstract_classes/model_base.py
@@ -2,7 +2,7 @@
 from typing import Type
 
 
-from ..interfaces import IRepositoryBase, IStatements
+from ..interfaces import IRepositoryBase, IStatements_two_generic
 from ...databases.my_sql import MySQLStatements, MySQLRepository
 from orm.abstract_model import AbstractSQLStatements
 
@@ -12,25 +12,25 @@ from ...utils import Table
 # endregion
 
 
-
 class ModelBase[T: Table]:
     """
-    Clase base de las clases Model.
+    Class to select the correct AbstractSQLStatements class depends on the repository.
 
     Contiene los metodos necesarios para hacer consultas a una tabla
     """
-    statements_dicc: dict[Type[IRepositoryBase], Type[AbstractSQLStatements[T,IRepositoryBase]]] = {
+
+    statements_dicc: dict[Type[IRepositoryBase], Type[AbstractSQLStatements[T, IRepositoryBase]]] = {
         MySQLRepository: MySQLStatements,
     }
 
     # region Constructor
 
-    def __new__(cls, model: T, repository: IRepositoryBase) -> IStatements[T]:
-        cls:AbstractSQLStatements[T] = cls.statements_dicc.get(type(repository),None)
+    def __new__[TRepo](cls, model: T, repository: IRepositoryBase[TRepo]) -> IStatements_two_generic[T, TRepo]:
+        cls: AbstractSQLStatements[T, TRepo] = cls.statements_dicc.get(type(repository), None)
 
         if not cls:
             raise Exception(f"Repository selected does not exits '{repository}'")
-        
+
         self = object().__new__(cls)
         cls.__init__(self, model, repository)
         return self
