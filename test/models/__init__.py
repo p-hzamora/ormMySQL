394910e2ce492fd38f9488d0316f6a45153592b8
phzamora <p.hzamora@icloud.com>
2024-08-03 02:23:43 +0200

    chore: added Models into __init__.py

--- test/models/__init__.py
+++ test/models/__init__.py
@@ -2,3 +2,5 @@ from .actor import Actor, ActorModel  # noqa: F401
 from .address import Address, AddressModel  # noqa: F401
 from .city import City, CityModel  # noqa: F401
 from .country import Country, CountryModel  # noqa: F401
+from .staff import Staff, StaffModel  # noqa: F401
+from .store import Store, StoreModel  # noqa: F401
