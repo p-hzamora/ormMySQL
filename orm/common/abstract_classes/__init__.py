8aebb785fba87e235ef55f9ea6e4f8a4b133fd4f
p-hzamora <p.hzamora@icloud.com>
2024-08-05 10:33:52 +0200

    chore: updated __init__.py in 'abstract_classes' folder

--- orm/common/abstract_classes/__init__.py
+++ orm/common/abstract_classes/__init__.py
@@ -1,1 +1,4 @@
+from .abstract_model import AbstractSQLStatements  # noqa: F401
+from .base_model import BaseModel  # noqa: F401
 from .non_query_base import NonQueryBase  # noqa: F401
+from .query_base import QueryBase  # noqa: F401
