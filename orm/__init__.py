7ac4171a26059e113fe91a1cf3c98abcd8859131
p-hzamora <p.hzamora@icloud.com>
2024-08-05 09:09:59 +0200

    chore: changed folder name

--- orm/__init__.py
+++ orm/__init__.py
@@ -1,4 +1,4 @@
-from .common.abstract_classes.model_base import BaseModel  # noqa: F401
+from .common.abstract_classes.base_model import BaseModel  # noqa: F401
 from .utils import Table, Column, ForeignKey  # noqa: F401
 from .utils.dissambler import Dissambler, nameof  # noqa: F401
 from .common.interfaces import IRepositoryBase  # noqa: F401
