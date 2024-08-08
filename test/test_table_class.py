13bb2ddfe9f5d13c64649722bb72d6b94edacb04
p-hzamora <p.hzamora@icloud.com>
2024-08-08 11:08:03 +0200

    feat: added new test 'test_table_class'

---
+++ test/test_table_class.py
@@ -0,0 +1,48 @@
+import unittest
+import sys
+from pathlib import Path
+from typing import Optional
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from orm import Column
+from orm.utils.table_constructor import Table, TableMeta
+
+
+class Person(Table):
+    __table_name__ = "person"
+
+    pk_id: Optional[int] = Column[int](is_primary_key=True, is_auto_increment=True)
+    name: str
+    age: int
+    email: str
+    phone: str
+    address: int
+
+
+P1 = Person(None, "Pablo", 25, "pablo@icloud.com", "6XXXXXXXXX", "C/ Madrid N_1, 3B")
+P2 = Person(None, "Pablo", 25, "pablo@icloud.com", "6XXXXXXXXX", "C/ Madrid N_1, 3B")
+
+
+class TestTableConstructor(unittest.TestCase):
+    def test_initialize_person(self):
+        self.assertIsInstance(P1, Table)
+
+    def test_if_two_objects_are_equals(self):
+        self.assertEqual(P1, P2)
+
+    def test_if_two_objects_are_not_the_same(self):
+        self.assertIsNot(P1, P2)
+
+    def test_Person_is_subclass_of_Table(self):
+        self.assertTrue(issubclass(Person, Table))
+
+    def test_Person_is_type_of_TableMeta(self):
+        self.assertTrue(type(Table), TableMeta)
+
+    def test_TableMeta_type_is_type(self):
+        self.assertTrue(type(TableMeta), type)
+
+
+if __name__ == "__main__":
+    unittest.main()
