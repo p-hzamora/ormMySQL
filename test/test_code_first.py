6bc242f1c3f31f8d2cfc66fc0ace3ccb630128aa
phzamora <p.hzamora@icloud.com>
2024-08-13 13:47:06 +0200

    update: test_code_first.py

--- test/test_code_first.py
+++ test/test_code_first.py
@@ -11,6 +11,8 @@ sys.path = [str(Path(__file__).parent.parent), *sys.path]
 from orm.databases.my_sql import MySQLRepository  # noqa: E402
 from test.config import config_dict  # noqa: E402
 
+from test.models import Country, CountryModel  # noqa: E402
+
 TDDBB_name = "__test_ddbb__"
 TTABLE_name = "__test_table__"
 
@@ -32,7 +34,11 @@ class Test_my_sql(unittest.TestCase):
     def test_create_table_code_first_passing_file(self):
         self.ddbb.create_tables_code_first("test/models/models_in_the_same_file/all_models_in_one_file.py")
         pass
-
+    
+    def test_create_table(self):
+        if CountryModel(self.ddbb).table_exists():
+            self.ddbb.drop_table(Country.__table_name__)
+        CountryModel(self.ddbb).create_table()
 
 if __name__ == "__main__":
     unittest.main()
