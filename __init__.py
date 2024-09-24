541e131908887b7cbb5fdda153f4d27c4348c8b4
p-hzamora <p.hzamora@icloud.com>
2024-09-24 06:32:36 +0200

    chore: updated  orm/__init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from .src.common.interfaces import IRepositoryBase  # noqa: F401
-from .src.databases.my_sql.repository import MySQLRepository  # noqa: F401
-from .src import BaseModel  # noqa: F401
-from .src.utils import Table, Column, ForeignKey  # noqa: F401
-from .src.common.enums.condition_types import ConditionType  # noqa: F401
-from .src.common.enums import JoinType  # noqa: F401
+from .src.ormlambda.common.interfaces import IRepositoryBase  # noqa: F401
+from .src.ormlambda.databases.my_sql.repository import MySQLRepository  # noqa: F401
+from .src.ormlambda import BaseModel  # noqa: F401
+from .src.ormlambda.utils import Table, Column, ForeignKey  # noqa: F401
+from .src.ormlambda.common.enums.condition_types import ConditionType  # noqa: F401
+from .src.ormlambda.common.enums import JoinType  # noqa: F401
