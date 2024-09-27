dbcc16046d28f768e9b3a7b9a5efc29fed5b760b
p-hzamora <p.hzamora@icloud.com>
2024-09-27 15:20:48 +0200

    test: used 'config_dict' and deleted address query due to unhashable data-type

--- src/test/test_abstract_model.py
+++ src/test/test_abstract_model.py
@@ -1,33 +1,29 @@
 import unittest
 import sys
 from pathlib import Path
+from config import config_dict
 
 sys.path = [str(Path(__file__).parent.parent.parent), *sys.path]
 
 from src.test.models import (  # noqa: E402
     City,
-    Address,
     Country,
 )
-
 
 from src.ormlambda.databases.my_sql.repository import MySQLRepository  # noqa: E402
 from src.ormlambda.databases.my_sql.statements import MySQLStatements  # noqa: E402
 
-db = MySQLRepository(user="root", password="1234", database="sakila")
+db = MySQLRepository(**config_dict)
+db.set_config({"database": "sakila"})
 
 
 class TestAbstractSQLStatements(unittest.TestCase):
     def test_constructor(self):
-        address = MySQLStatements[Address](Address, db)
         city = MySQLStatements[City](City, db)
         country = MySQLStatements[Country](Country, db)
 
-        result_a = address.select(flavour=set)
         rusult_ci = city.select(flavour=set)
         result_co = country.select(flavour=set)
-        self.assertIsInstance(result_a, tuple)
-        self.assertIsInstance(result_a[0], set)
 
         self.assertIsInstance(rusult_ci, tuple)
         self.assertIsInstance(rusult_ci[0], set)
