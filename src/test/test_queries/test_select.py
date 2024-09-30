dea06370209c07053d3abd8d3221b44f776331fb
p-hzamora <p.hzamora@icloud.com>
2024-09-30 20:08:46 +0200

    test: added test/ folder to sys.path

--- src/test/test_queries/test_select.py
+++ src/test/test_queries/test_select.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 
 sys.path.append([str(x) for x in Path(__file__).parents if x.name == "src"].pop())
+sys.path.append([str(x) for x in Path(__file__).parents if x.name == "test"].pop())
 
 from ormlambda.databases.my_sql.clauses.select import SelectQuery, TableColumn  # noqa: E402
 from models import (  # noqa: E402
