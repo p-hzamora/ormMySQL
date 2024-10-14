9ea7684fa1af7af6c063572f8ac8e1bedc4d9dd4
p-hzamora <p.hzamora@icloud.com>
2024-10-14 19:17:27 +0200

    fix: added 'IAggregate' interface

--- src/ormlambda/common/interfaces/IAggregate.py
+++ src/ormlambda/common/interfaces/IAggregate.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+import typing as tp
+
+if tp.TYPE_CHECKING:
+    from ormlambda import Table
 from .IQueryCommand import IQuery
+from .IDecompositionQuery import IDecompositionQuery
 
 
-class IAggregate(IQuery): ...
+class IAggregate[T: tp.Type[Table]](IDecompositionQuery[T],IQuery): ...
