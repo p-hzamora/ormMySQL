6c56bb69805dad1b4a6b35156ee1c37a730d114b
p-hzamora <p.hzamora@icloud.com>
2024-10-14 20:12:07 +0200

    update: 'GroupBy' class

---
+++ src/ormlambda/databases/my_sql/functions/group_by.py
@@ -0,0 +1,38 @@
+import typing as tp
+from ormlambda.common.enums.join_type import JoinType
+from ormlambda.common.abstract_classes.decomposition_query import ClauseInfo, DecompositionQueryBase
+from ormlambda.common.interfaces.IAggregate import IAggregate
+from ormlambda import Table
+
+
+class GroupBy[T: tp.Type[Table], TProp](DecompositionQueryBase[T], IAggregate[T]):
+    CLAUSE: str = "GROUP BY"
+
+    def __init__(
+        self,
+        table: T,
+        column: tp.Callable[[T], TProp],
+        *,
+        alias: bool = True,
+        alias_name: str | None = None,
+        by: JoinType = JoinType.INNER_JOIN,
+    ) -> None:
+        super().__init__(
+            table,
+            lambda_query=column,
+            alias=alias,
+            alias_name=alias_name,
+            by=by,
+        )
+
+        self._column: TProp = column
+
+
+    def alias_children_resolver[Tclause: tp.Type[Table]](self, clause_info: ClauseInfo[Tclause]):
+        return None
+
+    @property
+    def query(self) -> str:
+        col: str = ", ".join([x.query for x in self.all_clauses])
+
+        return f"{self.CLAUSE} {col}"
