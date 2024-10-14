e7f4f9ff609b31c523ec80c3c587d5ecf66e6d76
p-hzamora <p.hzamora@icloud.com>
2024-10-14 20:05:58 +0200

    update: added 'IDecompositionQuery' into __init__

--- src/ormlambda/common/interfaces/__init__.py
+++ src/ormlambda/common/interfaces/__init__.py
@@ -2,4 +2,5 @@ from .IQueryCommand import IQuery  # noqa: F401
 from .INonQueryCommand import INonQueryCommand  # noqa: F401
 from .IRepositoryBase import IRepositoryBase  # noqa: F401
 from .IStatements import IStatements, IStatements_two_generic  # noqa: F401
+from .IDecompositionQuery import IDecompositionQuery  # noqa: F401
 from .IAggregate import IAggregate  # noqa: F401
