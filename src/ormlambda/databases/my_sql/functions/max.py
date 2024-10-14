a68790d428158a2822ad23a0397834aabdac5f19
p-hzamora <p.hzamora@icloud.com>
2024-10-14 20:12:43 +0200

    feat: added Max class

--- src/ormlambda/databases/my_sql/functions/max.py
+++ src/ormlambda/databases/my_sql/functions/max.py
@@ -1,28 +1,39 @@
 from ormlambda.common.interfaces import IAggregate
 import typing as tp
 
-from ormlambda.databases.my_sql.clauses.decomposition_query import DecompositionQuery
+from ormlambda.common.abstract_classes.decomposition_query import DecompositionQueryBase, ClauseInfo
 
 if tp.TYPE_CHECKING:
     from ormlambda import Table
 
 
-class Max(IAggregate):
+class Max[T: tp.Type[Table]](DecompositionQueryBase[T], IAggregate[T]):
     NAME: str = "MAX"
 
     @tp.overload
-    def __init__[T: tp.Type[Table]](self, table: T, column: str, *, alias: str = "maximun") -> None: ...
-    @tp.overload
-    def __init__[T: tp.Type[Table]](self, table: T, column: tp.Callable[[T], tp.Any], *, alias: str = "maximun") -> None: ...
+    def __init__[T: tp.Type[Table]](self, table: T, column: tp.Callable[[T], tp.Any], *, alias: bool = True, alias_name: str = "max") -> None: ...
 
-    def __init__[T: tp.Type[Table]](self, table: T, column: str | tp.Callable[[T], tp.Any] = "*", *, alias: str = "maximun") -> None:
-        self._table: T = table
-        self._column: DecompositionQuery | str | tp.Callable[[T], tp.Any] = column
-        self._alias: str = alias
+    def __init__(
+        self,
+        table: T,
+        column: str | tp.Callable[[T], tuple],
+        *,
+        alias: bool = True,
+        alias_name: str = "max",
+    ) -> None:
+        super().__init__(
+            table,
+            lambda_query=column,
+            alias=alias,
+            alias_name=alias_name,
+        )
+
+    def alias_children_resolver[Tclause: tp.Type[Table]](self, clause_info: ClauseInfo[Tclause]):
+        if isinstance(clause_info._row_column, IAggregate):
+            return clause_info._row_column.alias
+        return None
 
     @property
     def query(self) -> str:
-        decom = DecompositionQuery(self._table, self._column, alias=False)
-        col = decom.query
-
-        return f"{self.NAME}({col}) as `{self._alias}`"
+        col = ", ".join([x.query for x in self.all_clauses])
+        return f"{self.NAME}({col})"
