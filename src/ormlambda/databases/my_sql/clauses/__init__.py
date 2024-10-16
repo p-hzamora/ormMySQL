53829a18634294994ed3c2c454b7c6f15b4030e2
p-hzamora <p.hzamora@icloud.com>
2024-10-16 13:32:38 +0200

    chore: updated __init__ with 'as' clause

--- src/ormlambda/databases/my_sql/clauses/__init__.py
+++ src/ormlambda/databases/my_sql/clauses/__init__.py
@@ -1,15 +1,16 @@
-from .create_database import CreateDatabase, TypeExists  # noqa: F401
-from .delete import DeleteQuery  # noqa: F401
-from .drop_database import DropDatabase  # noqa: F401
-from .drop_table import DropTable  # noqa: F401
-from .insert import InsertQuery  # noqa: F401
-from .joins import JoinSelector, JoinType  # noqa: F401
-from .limit import LimitQuery  # noqa: F401
-from .offset import OffsetQuery  # noqa: F401
-from .order import OrderQuery  # noqa: F401
-from .select import SelectQuery  # noqa: F401
-from .update import UpdateQuery  # noqa: F401
-from .upsert import UpsertQuery  # noqa: F401
-from .where_condition import WhereCondition  # noqa: F401
-from .count import CountQuery  # noqa: F401
-from ..functions.group_by import GroupBy  # noqa: F401
+from .create_database import CreateDatabase as CreateDatabase
+from .create_database import TypeExists as TypeExists
+from .delete import DeleteQuery as DeleteQuery
+from .drop_database import DropDatabase as DropDatabase
+from .drop_table import DropTable as DropTable
+from .insert import InsertQuery as InsertQuery
+from .joins import JoinSelector as JoinSelector
+from .joins import JoinType as JoinType
+from .limit import LimitQuery as LimitQuery
+from .offset import OffsetQuery as OffsetQuery
+from .order import OrderQuery as OrderQuery
+from .update import UpdateQuery as UpdateQuery
+from .upsert import UpsertQuery as UpsertQuery
+from .where_condition import WhereCondition as WhereCondition
+from .count import Count as Count
+from ..functions.group_by import GroupBy as GroupBy
