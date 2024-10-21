62a15f9324a8a7345d7789633ac9b10fe25725d5
p-hzamora <p.hzamora@icloud.com>
2024-10-21 14:32:11 +0200

    chore: added alias into __init__

--- src/ormlambda/__init__.py
+++ src/ormlambda/__init__.py
@@ -1,11 +1,20 @@
-# enums
-from .common.enums import (  # noqa: F401
-    JoinType,
-    ConditionType,
+# region enums
+from .common.enums import (
+    JoinType as JoinType,
+    ConditionType as ConditionType,
 )
+from ormlambda.common.interfaces.IStatements import OrderType as OrderType
+# endregion
 
-from .common.abstract_classes import AbstractSQLStatements  # noqa: F401
-from .common.interfaces import IRepositoryBase  # noqa: F401
-from .utils import Table, Column, ForeignKey  # noqa: F401
-from .utils.lambda_disassembler import Disassembler, nameof  # noqa: F401
-from .model_base import BaseModel  # noqa: F401  # COMMENT: to avoid relative import we need to import BaseModel after import Table,Column, ForeignKey, IRepositoryBase and Disassembler
+from .common.abstract_classes import AbstractSQLStatements as AbstractSQLStatements
+from .common.interfaces import IRepositoryBase as IRepositoryBase
+from .utils import (
+    Table as Table,
+    Column as Column,
+    ForeignKey as ForeignKey,
+)
+from .utils.lambda_disassembler import (
+    Disassembler as Disassembler,
+    nameof as nameof,
+)
+from .model_base import BaseModel as BaseModel  # COMMENT: to avoid relative import we need to import BaseModel after import Table,Column, ForeignKey, IRepositoryBase and Disassembler
