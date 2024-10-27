5c50cef9f9617e9402993c10615ac11dd9138c36
p-hzamora <p.hzamora@icloud.com>
2024-10-27 21:41:48 +0100

    wip: added mysql wrapped method into Select class

--- src/ormlambda/databases/my_sql/clauses/select.py
+++ src/ormlambda/databases/my_sql/clauses/select.py
@@ -3,6 +3,7 @@ from typing import override, Type, Callable, TYPE_CHECKING
 from ormlambda.common.abstract_classes.decomposition_query import DecompositionQueryBase
 from ormlambda.common.enums.join_type import JoinType
 from ormlambda.common.interfaces.IAggregate import IAggregate
+import shapely as shp
 
 if TYPE_CHECKING:
     from ormlambda import Table
@@ -32,12 +33,16 @@ class Select[T: Type[Table]](DecompositionQueryBase[T]):
     # def alias_children_resolver[Tclause: Type[Table]](self, clause_info: ClauseInfo[Tclause]):
     #     return f"{clause.table.__table_name__}_{name}"
 
+    # TODOL: see who to deal when we will have to add more mysql methods
     @override
     @property
     def query(self) -> str:
         cols: list[str] = []
         for x in self.all_clauses:
-            cols.append(x.query)
+            if x.dtype is shp.Point:
+                cols.append(x.concat_with_alias(f"ST_AsText({self._table.__table_name__}.{x.column})"))
+            else:
+                cols.append(x.query)
 
             if isinstance(x._row_column, IAggregate) and x._row_column.has_foreign_keys:
                 self._fk_relationship.update(x._row_column.fk_relationship)
