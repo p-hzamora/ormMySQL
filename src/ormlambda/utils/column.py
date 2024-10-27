eff632434994334ee6134918351bbbf3df643202
p-hzamora <p.hzamora@icloud.com>
2024-10-27 21:33:49 +0100

    wip: updated 'Column' class

--- src/ormlambda/utils/column.py
+++ src/ormlambda/utils/column.py
@@ -1,7 +1,14 @@
-from typing import Type
+from __future__ import annotations
+from typing import Type, Optional, Callable, TYPE_CHECKING, Any
+import shapely as sph
+
+if TYPE_CHECKING:
+    from .table_constructor import Field
 
 
 class Column[T]:
+    CHAR: str = "%s"
+
     __slots__ = (
         "dtype",
         "column_name",
@@ -31,22 +38,54 @@ class Column[T]:
         self.is_auto_increment: bool = is_auto_increment
         self.is_unique: bool = is_unique
 
-    def __repr__(self) -> str:
-        return f"<Column: {self.column_name}>"
+    @property
+    def column_value_to_query(self) -> T:
+        """
+        This property must ensure that any variable requiring casting by different database methods is properly wrapped.
+        """
+        if self.dtype is sph.Point:
+            return sph.to_wkt(self.column_value, -1)
+        return self.column_value
 
-    def __to_string__(self, name: str, var_name: T, type_: str):
-        dicc: dict = {
-            "dtype": type_,
-            "column_name": f"'{name}'",
-            "column_value": var_name,  # must be the same variable name as the instance variable name in Table's __init__ class
+    @property
+    def placeholder(self) -> str:
+        return self.placeholder_resolutor(self.dtype)
+
+    @property
+    def placeholder_resolutor(self) -> Callable[[Type, T], str]:
+        return self.__fetch_wrapped_method
+
+    # FIXME [ ]: this method is allocating the Column class with MySQL database
+    @classmethod
+    def __fetch_wrapped_method(cls, type_: Type) -> Optional[str]:
+        """
+        This method must ensure that any variable requiring casting by different database methods is properly wrapped.
+        """
+        caster: dict[Type[Any], Callable[[str], str]] = {
+            sph.Point: lambda x: f"ST_GeomFromText({x})",
         }
-        exec_str: str = f"{Column.__name__}[{type_}]("
-        for x in self.__slots__:
-            self_value = getattr(self, x)
+        return caster.get(type_, lambda x: x)(cls.CHAR)
 
-            exec_str += f"  {x}={dicc.get(x,self_value)},\n"
-        exec_str += ")"
-        return exec_str
+    def __repr__(self) -> str:
+        return f"<Column: {self.dtype}>"
+
+    def __to_string__(self, field: Field):
+        column_class_string: str = f"{Column.__name__}[{field.type_name}]("
+
+        dicc: dict[str, Callable[[Field], str]] = {
+            "dtype": lambda field: field.type_name,
+            "column_name": lambda field: f"'{field.name}'",
+            "column_value": lambda field: field.name,  # must be the same variable name as the instance variable name in Table's __init__ class
+        }
+        for self_var in self.__init__.__annotations__:
+            if not hasattr(self, self_var):
+                continue
+
+            self_value = dicc.get(self_var, lambda field: getattr(self, self_var))(field)
+            column_class_string += f" {self_var}={self_value}, "
+
+        column_class_string += ")"
+        return column_class_string
 
     def __hash__(self) -> int:
         return hash(
