c742ea506166aabcbf90bd20b2b7b860f10a8855
p-hzamora <p.hzamora@icloud.com>
2024-10-27 19:10:56 +0100

    test: updated __init__.py

---
+++ src/test/models/table_with_different_types.py
@@ -0,0 +1,27 @@
+from shapely import Point
+from ormlambda import (
+    Column,
+    Table,
+    BaseModel,
+)
+
+from datetime import datetime
+
+from ormlambda import IRepositoryBase
+
+
+class TableType(Table):
+    __table_name__ = "table_type"
+
+
+    pk: int = Column[int](is_primary_key=True, is_auto_increment=True)
+    strings:str
+    integers: int
+    floats: int
+    points: Point
+    datetimes:datetime
+
+
+class TableTypeModel(BaseModel[TableType]):
+    def __new__[TRepo](cls, repository: IRepositoryBase[TRepo]):
+        return super().__new__(cls, TableType, repository)
