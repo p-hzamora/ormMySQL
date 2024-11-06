3d320ea98a34112461e2d885a5ad4c4a7c9aa838
phzamora <p.hzamora@icloud.com>
2024-11-06 09:50:14 +0100

    refactor: __init__'s files

--- src/ormlambda/common/abstract_classes/__init__.py
+++ src/ormlambda/common/abstract_classes/__init__.py
@@ -1,3 +1,4 @@
-from .non_query_base import NonQueryBase  # noqa: F401
-from .abstract_model import AbstractSQLStatements  # noqa: F401
-from .query_base import QueryBase  # noqa: F401
+from .non_query_base import NonQueryBase as NonQueryBase
+from .abstract_model import AbstractSQLStatements as AbstractSQLStatements
+from .query_base import QueryBase as QueryBase
+from .decomposition_query import DecompositionQueryBase as  DecompositionQueryBase
