0dae9d8594dc6d090c5440fa81e08a2a150e506c
phzamora <p.hzamora@icloud.com>
2024-11-11 14:38:10 +0100

    chore: updated IJoinSelector properties

--- src/ormlambda/common/interfaces/IJoinSelector.py
+++ src/ormlambda/common/interfaces/IJoinSelector.py
@@ -3,7 +3,19 @@ import abc
 
 
 from .IQueryCommand import IQuery
-class IJoinSelector[TLeft, TRight](IQuery):
 
+
+class IJoinSelector[TLeft, TRight](IQuery):
     @abc.abstractmethod
     def sort_join_selectors(cls, joins: set[IJoinSelector]) -> tuple[IJoinSelector]: ...
+
+    @property
+    def alias(self) -> str: ...
+    @property
+    def left_table(self) -> str: ...
+    @property
+    def right_table(self) -> str: ...
+    @property
+    def left_col(self) -> str: ...
+    @property
+    def right_col(self) -> str: ...
