a5f7cd73dc39d19a1f5ed441b7859658c7ef57a5
phzamora <p.hzamora@icloud.com>
2024-11-14 07:02:14 +0100

    test: updated test_new_context_select

--- src/test/test_new_context_select.py
+++ src/test/test_new_context_select.py
@@ -2,17 +2,26 @@ import unittest
 import typing as tp
 import sys
 from pathlib import Path
+from mysql.connector import MySQLConnection
 
 
 sys.path.append([str(x) for x in Path(__file__).parents if x.name == "src"].pop())
 
-from ormlambda.databases.my_sql.clauses.select import Select
+from ormlambda.common.interfaces import IRepositoryBase, IStatements_two_generic
+from ormlambda.databases.my_sql import MySQLRepository
 from config import config_dict  # noqa: E402
 from ormlambda.databases.my_sql import MySQLRepository  # noqa: E402
 from ormlambda import IRepositoryBase, Table, Column, BaseModel, JoinType, ForeignKey  # noqa: E402
 
 
+
 DDBBNAME = "__test_ddbb__"
+
+
+class CSimple(Table):
+    __table_name__ = "C"
+    pk_c: int = Column(is_primary_key=True, is_auto_increment=True)
+    name: str
 
 
 class BSimple(Table):
@@ -21,11 +30,7 @@ class BSimple(Table):
     name: str
     fk_c: int
 
-
-class CSimple(Table):
-    __table_name__ = "C"
-    pk_b: int = Column(is_primary_key=True, is_auto_increment=True)
-    name: str
+    CSimple = ForeignKey[tp.Self, CSimple](__table_name__, CSimple, lambda self, c: self.fk_c == c.pk_c)
 
 
 class AWithMultipleReferencesToB(Table):
@@ -41,20 +46,52 @@ class AWithMultipleReferencesToB(Table):
     B_fk_b3 = ForeignKey[tp.Self, BSimple](__table_name__, BSimple, lambda self, b: self.fk_b3 == b.pk_b)
 
 
+class AModel(BaseModel[AWithMultipleReferencesToB]):
+    def __new__[TRepo](cls, repository: IRepositoryBase[TRepo]):
+        return super().__new__(cls, AWithMultipleReferencesToB, repository)
+
+
+class CSimpleModel(BaseModel[CSimple]):
+    def __new__[TRepo](cls, repository: IRepositoryBase[TRepo]):
+        return super().__new__(cls, CSimple, repository)
+
+
+class BSimpleModel(BaseModel[BSimple]):
+    def __new__[TRepo](cls, repository: IRepositoryBase[TRepo]):
+        return super().__new__(cls, BSimple, repository)
+
+
 class TestJoinQueries(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.ddbb: IRepositoryBase[MySQLConnection] = MySQLRepository(**config_dict)
+        cls.ddbb.database = DDBBNAME
+        cls.ddbb.create_database(DDBBNAME, "replace")
+
+        cls.model = AModel(cls.ddbb)
+        CSimpleModel(cls.ddbb).create_table()
+        BSimpleModel(cls.ddbb).create_table()
+        cls.model.create_table()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.ddbb.drop_database(DDBBNAME)
+
     def test_new_select_context(self):
-        select_a = Select[AWithMultipleReferencesToB](
-            (AWithMultipleReferencesToB,),
+        select_a = self.model.select(
             lambda a: (
                 a.data_a,
                 a.B_fk_b1.name,
                 a.B_fk_b2.name,
                 a.B_fk_b3.name,
+                a.B_fk_b1.CSimple.name,
+                a.B_fk_b2.CSimple.name,
             ),
         )
         real_query: str = "SELECT A.data_a, b1.name as `b1.name`, b2.name as `b2.name`, b3.name as `b3.name` FROM A INNER JOIN B AS `b1` ON A.fk_b1 = b1.pk_b INNER JOIN B AS `b2` ON A.fk_b2 = b2.pk_b INNER JOIN B AS `b3` ON A.fk_b3 = b3.pk_b"
-        select_a.query
-        self.assertEqual(select_a.query, real_query)
+        self.model.query
+        select_a
+        self.assertEqual(self.model.query, real_query)
 
 
 if __name__ == "__main__":
