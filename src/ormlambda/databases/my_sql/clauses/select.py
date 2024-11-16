f18e79a97bb7caf3eacada6ab10a2ecc7499eeac
phzamora <p.hzamora@icloud.com>
2024-11-16 14:12:27 +0100

    wip: refactorin Select class

--- src/ormlambda/databases/my_sql/clauses/select.py
+++ src/ormlambda/databases/my_sql/clauses/select.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 from typing import override, Type, Callable, TYPE_CHECKING, Optional
 
 from ormlambda.common.enums.join_type import JoinType
-from ormlambda.common.interfaces.IAggregate import IAggregate
 import shapely as shp
+
+from ormlambda.databases.my_sql.clauses import ST_AsText
+from ormlambda.common.abstract_classes.clause_info import IAggregate
+from ormlambda.common.abstract_classes.clause_info import ClauseInfo
 
 
 if TYPE_CHECKING:
@@ -39,21 +42,28 @@ class Select[T: Type[Table], *Ts](MySQLDecompositionQuery[T, *Ts]):
     @override
     @property
     def query(self) -> str:
-        cols: list[str] = []
+        cols: list[ClauseInfo[T]] = []
         for x in self.all_clauses:
             if x.dtype is shp.Point:
-                cols.append(x.concat_with_alias(f"ST_AsText({self.table.table_alias()}.{x.column})"))
+                cols.append(ST_AsText(x))
             else:
-                cols.append(x.query)
+                cols.append(x)
 
-            
-            if isinstance(x._row_column, IAggregate) and x._row_column.has_foreign_keys:
-                self._joins.update(x._row_column.fk_relationship)
+            # # FIXME [ ]: refactor ClauseInfo class
+            # if isinstance(x, IAggregate) and x._row_column.has_foreign_keys:
+            #     self._joins.update(x._row_column.fk_relationship)
 
-        col: str = ", ".join(cols)
-        query: str = f"{self.CLAUSE} {col} FROM {self.table.__table_name__} AS `{self.table.table_alias()}`"
+        from_clause = "FROM " + ClauseInfo[T](self.table, None, alias_table=self.table.table_alias()).query
+        all_cols: str = ClauseInfo[T].join_clauses(cols, ",")
+
+        select_clauses = [
+            self.CLAUSE,
+            all_cols,
+            from_clause,
+        ]
+        select_clause = " ".join(select_clauses)
 
         if self.has_foreign_keys:
-            query += " " + self.stringify_foreign_key(" ")
+            select_clause += " " + self.stringify_foreign_key(" ")
 
-        return query
+        return select_clause
