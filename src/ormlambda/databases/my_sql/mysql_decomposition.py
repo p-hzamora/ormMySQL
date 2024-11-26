8effbcd2cc3490440bcca7777e24e9d33ad557f6
phzamora <p.hzamora@icloud.com>
2024-11-26 08:52:30 +0100

    update: 'MySQLDecompositionQuery' class

--- src/ormlambda/databases/my_sql/mysql_decomposition.py
+++ src/ormlambda/databases/my_sql/mysql_decomposition.py
@@ -7,6 +7,7 @@ from ormlambda.common.enums import JoinType
 from .clauses import JoinSelector
 
 if tp.TYPE_CHECKING:
+    from src.ormlambda.utils.foreign_key import ReferencedTable
     from ormlambda import Table
     from ormlambda.common.interfaces.IJoinSelector import IJoinSelector
 
@@ -37,10 +38,25 @@ class MySQLDecompositionQuery[T: tp.Type[Table], *Ts](DecompositionQueryBase[T, 
         sorted_joins = JoinSelector.sort_join_selectors(self._joins)
         return f"{sep}".join([join.query for join in sorted_joins])
 
-    def _add_fk_relationship[T1: tp.Type[Table], T2: tp.Type[Table]](self, t1: T1, t2: T2) -> None:
-        for relation in ForeignKey.MAPPED[t1.__table_name__].referenced_tables[t2.__table_name__]:
-            new_join = JoinSelector[T1, T2](t1, t2, self._by, where=relation.relationship)
-            self._joins.add(new_join)
+    def _add_fk_relationship[T1: tp.Type[Table], T2: tp.Type[Table]](
+        self,
+        t1: tp.Optional[T1] = None,
+        t2: tp.Optional[T2] = None,
+        *,
+        referenced_table: tp.Optional[ReferencedTable[T1, T2]] = None,
+    ) -> None:
+        if referenced_table:
+            join_selector = JoinSelector[T1, T2](
+                left_table=referenced_table.orig_table,
+                right_table=referenced_table.referenced_table,
+                by=self._by,
+                where=referenced_table.relationship,
+            )
+            self._joins.add(join_selector)
+        else:
+            for relation in ForeignKey.MAPPED[t1.__table_name__].referenced_tables[t2.__table_name__]:
+                new_join = JoinSelector[T1, T2](t1, t2, self._by, where=relation.relationship)
+                self._joins.add(new_join)
 
         tables = list(self._tables)
         if t2 not in tables:
