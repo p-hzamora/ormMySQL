0141ccb6dadbcbc490a97df04843abc5dcac5882
phzamora <p.hzamora@icloud.com>
2024-12-13 11:26:29 +0100

    update: types.py

--- src/ormlambda/types.py
+++ src/ormlambda/types.py
@@ -2,6 +2,7 @@ import typing as tp
 
 if tp.TYPE_CHECKING:
     from ormlambda import Table, Column
+    from ormlambda.common.abstract_classes.comparer import Comparer
 
 
 type AsteriskType = str
@@ -10,3 +11,6 @@ type ColumnType[TProp] = TProp | Column[TProp] | AsteriskType
 type AliasType[T] = tp.Optional[str | tp.Callable[[T], str]]
 
 type ComparerType = tp.Literal["=", "!=", "<", "<=", ">", ">=", "in"]
+
+
+type TupleJoinType[T] = tuple[str, T, Comparer[T]]
