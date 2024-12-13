ee40e6612825575cfcdf3e366b4455fe00001e7a
phzamora <p.hzamora@icloud.com>
2024-12-13 11:37:15 +0100

    wip: updating 'JoinSelector' class

--- src/ormlambda/databases/my_sql/clauses/joins.py
+++ src/ormlambda/databases/my_sql/clauses/joins.py
@@ -1,24 +1,25 @@
 from __future__ import annotations
 from collections import defaultdict
-from typing import override, Callable, overload, Optional, TYPE_CHECKING, Type
+from typing import override, Optional, TYPE_CHECKING, Type
 
 
 from ormlambda.utils.module_tree.dfs_traversal import DFSTraversal
 from ormlambda.common.interfaces.IJoinSelector import IJoinSelector
 from ormlambda.common.interfaces.IQueryCommand import IQuery
-from ormlambda import Disassembler
 from ormlambda import JoinType
 from ormlambda.common.abstract_classes.clause_info import ClauseInfo
+from ormlambda.common.abstract_classes.comparer import Comparer
 
 # TODOL [x]: Try to import Table module without circular import Error
 if TYPE_CHECKING:
     from ormlambda import Table
 
 
-class JoinSelector[TLeft, TRight](IJoinSelector[TLeft, TRight]):
+class JoinSelector[TLeft: Table, TRight: Table](IJoinSelector[TLeft, TRight]):
     __slots__: tuple = (
+        "_comparer",
         "_orig_table",
-        "_table_right",
+        "_right_table",
         "_by",
         "_left_col",
         "_right_col",
@@ -28,58 +29,27 @@ class JoinSelector[TLeft, TRight](IJoinSelector[TLeft, TRight]):
 
     @override
     def __repr__(self) -> str:
-        table_col_left: str = f"{self._left_table.table_alias()}.{self._left_col}"
-        table_col_right: str = f"{self._right_table.table_alias()}.{self._right_col}"
+        table_col_left: str = f"{self.left_table.table_alias()}.{self._left_col}"
+        table_col_right: str = f"{self.right_table.table_alias()}.{self._right_col}"
 
         return f"{IQuery.__name__}: {self.__class__.__name__} ({table_col_left} == {table_col_right})"
 
-    @overload
     def __init__(
         self,
-        left_table: TLeft,
-        right_table: TRight,
-        col_left: str,
-        col_right: str,
+        where: Comparer,
         by: JoinType,
-    ) -> None: ...
-
-    @overload
-    def __init__(
-        self,
-        left_table: TLeft,
-        right_table: TRight,
-        by: JoinType,
-        where: Callable[[TLeft, TRight], bool],
-    ) -> None: ...
-
-    def __init__(
-        self,
-        left_table: Table,
-        right_table: Table,
-        by: JoinType,
-        left_col: Optional[str] = None,
-        right_col: Optional[str] = None,
-        where: Optional[Callable[[TLeft, TRight], bool]] = None,
+        alias: Optional[str] = None,
     ) -> None:
-        self._left_table: Table = left_table
-        self._right_table: Table = right_table
-        self._by: JoinType = by
-
-        if all(x is None for x in (left_col, right_col, where)):
-            raise ValueError("You must specify at least 'where' clausule or ('_left_col',_right_col')")
-
-        if where is None:
-            self._left_col: str = left_col
-            self._right_col: str = right_col
-            self._compareop: str = "="
-        else:
-            _dis: Disassembler[TLeft, TRight] = Disassembler[TLeft, TRight](where)
-            self._left_col: str = _dis.cond_1.name
-            self._right_col: str = _dis.cond_2.name
-            self._compareop: str = _dis.compare_op
+        self._comparer = where
+        self._orig_table = where._left_condition.table
+        self._right_table = where._right_condition.table
+        self._by = by
+        self._left_col = where._left_condition._column.column_name
+        self._right_col = where._right_condition._column.column_name
+        self._compareop = where._compare
 
         # COMMENT: When multiple columns reference the same table, we need to create an alias to maintain clear references.
-        self._alias: Optional[str] = f"{self._right_table.table_alias()}_{self._left_col}"
+        self._alias: Optional[str] = alias
 
     def __eq__(self, __value: "JoinSelector") -> bool:
         return isinstance(__value, JoinSelector) and self.__hash__() == __value.__hash__()
@@ -87,8 +57,8 @@ class JoinSelector[TLeft, TRight](IJoinSelector[TLeft, TRight]):
     def __hash__(self) -> int:
         return hash(
             (
-                self._left_table,
-                self._right_table,
+                self.left_table,
+                self.right_table,
                 self._by,
                 self._left_col,
                 self._right_col,
@@ -103,25 +73,24 @@ class JoinSelector[TLeft, TRight](IJoinSelector[TLeft, TRight]):
     @property
     @override
     def query(self) -> str:
-        ltable = self._left_table
-        rtable = self._right_table
+        ltable = self.left_table
+        rtable = self.right_table
 
-        clause_table_name_join = ClauseInfo[TLeft](ltable, alias_table=self.alias)
+        clause_table_name_join = ClauseInfo[TLeft](rtable, alias_table=self.alias)
         # return f"{self._by.value} {rtable} {alias}ON {left_col} {self._compareop} {right_col}"
         list_ = [
             self._by.value,  # inner join
             clause_table_name_join.query,
             "ON",
-            ClauseInfo[TLeft](ltable, column=self.left_col, alias_table=self.left_table.table_alias(), alias_clause=lambda x: "{table}_{column}").query,
+            ClauseInfo[TLeft](ltable, column=self.left_col, alias_table="{table}").query,
             self._compareop,  # =
-            ClauseInfo[TLeft](rtable, column=self.right_col, alias_table=clause_table_name_join.alias_table, alias_clause=lambda x: "{table}_{column}").query,
+            ClauseInfo[TLeft](rtable, column=self.right_col, alias_table="{table}").query,
         ]
         return " ".join([x for x in list_ if x is not None])
 
-
     @property
     def left_table(self) -> TLeft:
-        return self._left_table
+        return self._orig_table
 
     @property
     def right_table(self) -> TRight:
@@ -144,11 +113,11 @@ class JoinSelector[TLeft, TRight](IJoinSelector[TLeft, TRight]):
         join_object_map: dict[str, list[JoinSelector]] = defaultdict(list)
 
         for obj in joins:
-            join_object_map[obj._left_table].append(obj)
+            join_object_map[obj.left_table].append(obj)
 
         graph: dict[Type[Table], list[Type[Table]]] = defaultdict(list)
         for join in joins:
-            graph[join._left_table].append(join._right_table)
+            graph[join.left_table].append(join.right_table)
 
         sorted_graph = DFSTraversal.sort(graph)[::-1]
 
@@ -166,4 +135,4 @@ class JoinSelector[TLeft, TRight](IJoinSelector[TLeft, TRight]):
 
     @property
     def alias(self) -> str:
-        return self._right_table.__table_name__ + "_" + self._left_col
+        return self._alias
