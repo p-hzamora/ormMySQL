946f12916fdc0eb5b0d4b9a57d18d3637f882f7a
phzamora <p.hzamora@icloud.com>
2024-12-13 11:36:50 +0100

    wip: updating Select class

--- src/ormlambda/databases/my_sql/clauses/select.py
+++ src/ormlambda/databases/my_sql/clauses/select.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 from typing import override, Type, Callable, TYPE_CHECKING, Optional
 
 from ormlambda.common.enums.join_type import JoinType
-import shapely as shp
-
-from ormlambda.databases.my_sql.clauses import ST_AsText
-from ormlambda.common.abstract_classes.clause_info import ClauseInfo
+from ormlambda.common.abstract_classes.clause_info import ClauseInfo,ClauseInfoContext
 
 
 if TYPE_CHECKING:
@@ -23,37 +20,28 @@ class Select[T: Type[Table], *Ts](MySQLDecompositionQuery[T, *Ts]):
         tables: tuple[T, *Ts],
         lambda_query: Callable[[T], tuple] = lambda x: x,
         *,
-        alias: bool = False,
-        alias_name: str | None = None,
         joins: Optional[list[JoinSelector]] = None,
         by: JoinType = JoinType.INNER_JOIN,
+        context:Optional[ClauseInfoContext] = None
     ) -> None:
         super().__init__(
             tables,
             lambda_query,
-            alias=alias,
-            alias_name=alias_name,
             by=by,
             joins=joins,
+            context=context            
         )
 
     # TODOL: see who to deal when we will have to add more mysql methods
     @override
     @property
     def query(self) -> str:
-        cols: list[ClauseInfo[T]] = []
-        for x in self.all_clauses:
-            if x.dtype is shp.Point:
-                cols.append(ST_AsText(x))
-            else:
-                cols.append(x)
-
             # # FIXME [ ]: refactor ClauseInfo class
             # if isinstance(x, IAggregate) and x._row_column.has_foreign_keys:
             #     self._joins.update(x._row_column.fk_relationship)
 
-        from_clause = "FROM " + ClauseInfo[T](self.table, None, alias_table=self.table.table_alias()).query
-        all_cols: str = ClauseInfo[T].join_clauses(cols, ",")
+        from_clause = "FROM " + ClauseInfo[T](self.table, None, context=self._context).query
+        all_cols: str = ClauseInfo.join_clauses(self._all_clauses, ",")
 
         select_clauses = [
             self.CLAUSE,
