46ce238f9666f77370e5ae4ac107d26ba3a295ee
phzamora <p.hzamora@icloud.com>
2024-12-13 11:28:54 +0100

    update: 'IAggregate' interface

--- src/ormlambda/common/interfaces/IAggregate.py
+++ src/ormlambda/common/interfaces/IAggregate.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
-import typing as tp
+import abc
 
-if tp.TYPE_CHECKING:
-    from ormlambda import Table
 from .IQueryCommand import IQuery
-from .IDecompositionQuery import IDecompositionQuery
 
 
-class IAggregate[T: tp.Type[Table]](IDecompositionQuery[T], IQuery): ...
+class IAggregate(IQuery):
+    @classmethod
+    @abc.abstractmethod
+    def FUNCTION_NAME(cls) -> str: ...
+
