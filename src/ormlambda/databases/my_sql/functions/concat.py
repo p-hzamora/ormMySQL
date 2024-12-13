17b9f485320d61add35a6adc263f40e1ce103aeb
phzamora <p.hzamora@icloud.com>
2024-12-13 11:36:13 +0100

    wip: cleaned 'Concat' method

--- src/ormlambda/databases/my_sql/functions/concat.py
+++ src/ormlambda/databases/my_sql/functions/concat.py
@@ -1,36 +1,38 @@
 from ormlambda.common.interfaces.IAggregate import IAggregate
-from ormlambda.common.abstract_classes.decomposition_query import DecompositionQueryBase, ClauseInfo
+from ormlambda.common.abstract_classes.decomposition_query import ClauseInfo
 
 
 import typing as tp
-from ormlambda.common.enums.join_type import JoinType
+from ormlambda.types import ColumnType
+from ormlambda import Column
 
 if tp.TYPE_CHECKING:
     from ormlambda import Table
 
 
-class Concat[T: tp.Type[Table]](DecompositionQueryBase[T], IAggregate[T]):
-    CLAUSE = "CONCAT"
+class Concat[T: tp.Type[Table]](IAggregate):
+    FUNCTION_NAME: str = "CONCAT"
 
     def __init__[*Ts](
         self,
-        table: T,
-        lambda_query: str | tp.Callable[[T], tuple[*Ts]],
-        *,
-        alias: bool = True,
         alias_name: str = "CONCAT",
-        by: JoinType = JoinType.INNER_JOIN,
+        *values: ColumnType[Ts],
     ) -> None:
-        super().__init__(
-            table,
-            lambda_query,
-            alias=alias,
-            alias_name=alias_name,
-            by=by,
-        )
+        self._alias_name: str = alias_name
+        self._all_clauses: list[ClauseInfo] = []
+
+        for value in values:
+            if isinstance(value, Column):
+                clause_info = ClauseInfo(value.table, value)
+            else:
+                clause_info = ClauseInfo(None, value)
+            self._all_clauses.append(clause_info)
+
+    @property
+    def alias_clause(self) -> tp.Optional[str]:
+        return self._alias_name
 
     @property
     def query(self) -> str:
-        col: str = ", ".join([x.query for x in self.all_clauses])
-
-        return f"{self.CLAUSE}({col})"
+        string = f"{self.FUNCTION_NAME}({ClauseInfo.join_clauses(self._all_clauses)})"
+        return ClauseInfo(IAggregate, string, alias_clause=self._alias_name).query
