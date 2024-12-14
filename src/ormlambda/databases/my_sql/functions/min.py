4c13fb99edac5af31fb702f853dae31e8ef77c08
phzamora <p.hzamora@icloud.com>
2024-12-14 13:55:32 +0100

    update: 'ST_AsText', 'Max', 'Min' and 'Sum' classes

--- src/ormlambda/databases/my_sql/functions/min.py
+++ src/ormlambda/databases/my_sql/functions/min.py
@@ -1,22 +1,32 @@
+from __future__ import annotations
 import typing as tp
 
 from ormlambda.common.abstract_classes.clause_info_context import ClauseInfoContext
-from ormlambda.types import ColumnType
-from ormlambda.common.abstract_classes.clause_info import AggregateFunctionBase
+from ormlambda.types import ColumnType, AliasType
+from ormlambda.common.abstract_classes.clause_info import AggregateFunctionBase, ClauseInfo
 
 if tp.TYPE_CHECKING:
     from ormlambda import Table
 
-
-class Min[T: tp.Type[Table]](AggregateFunctionBase):
+class Min(AggregateFunctionBase):
     @staticmethod
     def FUNCTION_NAME() -> str:
-        return "MIN"
+        return "Min"
 
-    def __init__[TProp](
+    def __init__[T:Table,TProp](
         self,
         column: tuple[ColumnType[TProp]] | ColumnType[TProp],
-        alias_name: str = "min",
+        alias_table: AliasType[ColumnType[TProp]] = None,
+        alias_clause: AliasType[ColumnType[TProp]] = 'min',
         context: tp.Optional[ClauseInfoContext] = None,
     ):
-        super().__init__(column, alias_name, context)
+        if isinstance(column,tp.Iterable):
+            column = ClauseInfo.join_clauses([ClauseInfo[T](x.table,x,alias_table=alias_table,context=context) for x in column])
+        else:
+            column = ClauseInfo[T](column.table,column,alias_table=alias_table,context=context)
+            
+        super().__init__(
+            column=column,
+            alias_clause=alias_clause,
+            context=context,
+        )
