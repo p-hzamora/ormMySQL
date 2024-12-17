dff05b34da92da8c2274feb8d012af81b9edb5f7
phzamora <p.hzamora@icloud.com>
2024-12-17 20:40:57 +0100

    fix: Concat class

--- src/ormlambda/databases/my_sql/functions/concat.py
+++ src/ormlambda/databases/my_sql/functions/concat.py
@@ -4,6 +4,7 @@ from ormlambda.common.abstract_classes.clause_info_context import ClauseInfoCont
 
 import typing as tp
 from ormlambda.types import ColumnType, AliasType
+from ormlambda.common.abstract_classes.clause_info import ClauseInfo
 
 
 class Concat[*Ts](AggregateFunctionBase):
@@ -22,3 +23,15 @@ class Concat[*Ts](AggregateFunctionBase):
             alias_clause=alias_clause,
             context=context,
         )
+
+    @tp.override
+    @property
+    def query(self) -> str:
+        columns: list[str] = []
+
+        context = ClauseInfoContext(table_context=self._context._table_context, clause_context=None)
+        for clause in self._convert_into_clauseInfo(self.unresolved_column, context):
+            new_clause = clause
+            new_clause.alias_clause = None
+            columns.append(new_clause)
+        return f"{self.FUNCTION_NAME()}({ClauseInfo.join_clauses(columns)})"
