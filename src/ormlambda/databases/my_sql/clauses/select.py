bdc027f3fa90a4d66fc84932659d54d54bc1b051
phzamora <p.hzamora@icloud.com>
2024-12-17 06:29:43 +0100

    wip: Select class

--- src/ormlambda/databases/my_sql/clauses/select.py
+++ src/ormlambda/databases/my_sql/clauses/select.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 from typing import override, Type, Callable, TYPE_CHECKING, Optional
 
 from ormlambda.common.enums.join_type import JoinType
-from ormlambda.common.abstract_classes.clause_info import ClauseInfo,ClauseInfoContext
-
+from ormlambda.common.abstract_classes.clause_info import ClauseInfo, ClauseInfoContext
+from ormlambda.types import AliasType
 
 if TYPE_CHECKING:
     from ormlambda import Table
@@ -22,25 +22,28 @@ class Select[T: Type[Table], *Ts](MySQLDecompositionQuery[T, *Ts]):
         *,
         joins: Optional[list[JoinSelector]] = None,
         by: JoinType = JoinType.INNER_JOIN,
-        context:Optional[ClauseInfoContext] = None
+        context: Optional[ClauseInfoContext] = None,
+        alias_table: AliasType[ClauseInfo] = "{table}",
     ) -> None:
+        context = context if context else ClauseInfoContext()
         super().__init__(
             tables,
             lambda_query,
             by=by,
             joins=joins,
-            context=context            
+            context=context,
         )
+        self._alias_table = alias_table
 
     # TODOL: see who to deal when we will have to add more mysql methods
     @override
     @property
     def query(self) -> str:
-            # # FIXME [ ]: refactor ClauseInfo class
-            # if isinstance(x, IAggregate) and x._row_column.has_foreign_keys:
-            #     self._joins.update(x._row_column.fk_relationship)
+        # # FIXME [ ]: refactor ClauseInfo class
+        # if isinstance(x, IAggregate) and x._row_column.has_foreign_keys:
+        #     self._joins.update(x._row_column.fk_relationship)
 
-        from_clause = "FROM " + ClauseInfo[T](self.table, None, context=self._context).query
+        from_clause = "FROM " + ClauseInfo[T](self.table, None, alias_table=self._alias_table, context=self._context).query
         all_cols: str = ClauseInfo.join_clauses(self._all_clauses, ",")
 
         select_clauses = [
