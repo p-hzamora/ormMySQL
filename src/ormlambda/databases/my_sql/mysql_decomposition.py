978efbccbbd3855482f375b6df82c01db4bb559f
phzamora <p.hzamora@icloud.com>
2024-12-17 06:30:17 +0100

    fix: passed context into MuSQLDecompositionQuery class

--- src/ormlambda/databases/my_sql/mysql_decomposition.py
+++ src/ormlambda/databases/my_sql/mysql_decomposition.py
@@ -13,24 +13,14 @@ if tp.TYPE_CHECKING:
 
 
 class MySQLDecompositionQuery[T: tp.Type[Table], *Ts](DecompositionQueryBase[T, *Ts]):
-    def __init__(
-        self,
-        tables: tuple[T, *Ts],
-        lambda_query: tp.Callable[[T], tuple[*Ts]],
-        *,
-        by: JoinType = JoinType.INNER_JOIN,
-        replace_asterisk_char: bool = True,
-        joins: tp.Optional[list[IJoinSelector]] = None,
-        context:tp.Optional[ClauseInfoContext] = None
-
-    ) -> None:
+    def __init__(self, tables: tuple[T, *Ts], lambda_query: tp.Callable[[T], tuple[*Ts]], *, by: JoinType = JoinType.INNER_JOIN, replace_asterisk_char: bool = True, joins: tp.Optional[list[IJoinSelector]] = None, context: tp.Optional[ClauseInfoContext] = None) -> None:
         super().__init__(
             tables=tables,
             lambda_query=lambda_query,
             by=by,
             replace_asterisk_char=replace_asterisk_char,
             joins=joins,
-            context=context
+            context=context,
         )
 
     def stringify_foreign_key(self, sep: str = "\n") -> str:
@@ -39,7 +29,7 @@ class MySQLDecompositionQuery[T: tp.Type[Table], *Ts](DecompositionQueryBase[T, 
 
     def _add_fk_relationship[RTable: tp.Type[Table]](self, foreign_key: ForeignKey[T, RTable]) -> None:
         comparer = foreign_key.resolved_function(self._context)
-        join_selector = JoinSelector[T, RTable](comparer, self._by,alias=comparer.right_condition.alias_table)
+        join_selector = JoinSelector[T, RTable](comparer, self._by, alias="{table}", context=self._context)
         join_selector.query
         self._joins.add(join_selector)
         t2 = comparer._right_condition.table
