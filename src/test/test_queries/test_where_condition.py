d098bdc59423343e03195e58d94b7413b1af81cb
phzamora <p.hzamora@icloud.com>
2024-12-22 19:38:05 +0100

    test: updated test_where_condition

--- src/test/test_queries/test_where_condition.py
+++ src/test/test_queries/test_where_condition.py
@@ -2,10 +2,11 @@ import unittest
 import sys
 from pathlib import Path
 
+
 sys.path = [str(Path(__file__).parent.parent), *sys.path]
 sys.path.append([str(x) for x in Path(__file__).parents if x.name == "src"].pop())
 
-from ormlambda.common.enums import ConditionType  # noqa: E402
+from ormlambda.common.abstract_classes.comparer import Comparer  # noqa: E402
 from ormlambda.databases.my_sql.clauses import Where  # noqa: E402
 from models import City, Country, Address, B, C  # noqa: E402
 
@@ -13,87 +14,82 @@ ADDRESS_1 = Address(200, "Calle Cristo de la victoria", None, None, 1, "28026", 
 
 
 class TestCondition(unittest.TestCase):
-    COND_CI_CO = Where[City, Country](instances=(City, Country), function=lambda x, y: x.last_update != y.country_id)
-    COND_A_CI = Where[Address, City](instances=(Address, City), function=lambda a, c: a.address2 <= c.city_id)
-    COND_B_C = Where[B, C](instances=(B, C), function=lambda b, c: b.A.value == c.B.data)
+    COND_CI_CO: Comparer = City.last_update != Country.country_id
+    COND_A_CI: Comparer = Address.address2 <= City.city_id
+    COND_B_C: Comparer = B.A.value == C.B.data
 
     def test_condition_constructor(self):
-        self.assertIsInstance(self.COND_CI_CO, Where)
-        self.assertIsInstance(self.COND_A_CI, Where)
-        self.assertIsInstance(self.COND_B_C, Where)
+        self.assertIsInstance(self.COND_CI_CO, Comparer)
+        self.assertIsInstance(self.COND_A_CI, Comparer)
+        self.assertIsInstance(self.COND_B_C, Comparer)
 
     def test_to_query_cond_1(self):
-        self.assertEqual(self.COND_CI_CO.query, "WHERE city.last_update != country.country_id")
+        self.assertEqual(Where(self.COND_CI_CO).query, "WHERE city.last_update != country.country_id")
 
     def test_to_query_cond_2(self):
-        self.assertEqual(self.COND_A_CI.query, "WHERE address.address2 <= city.city_id")
+        self.assertEqual(Where(self.COND_A_CI).query, "WHERE address.address2 <= city.city_id")
 
     def test_to_query_cond_3(self):
-        self.assertEqual(self.COND_B_C.query, "WHERE a.value = b.data")
+        self.assertEqual(Where(self.COND_B_C).query, "WHERE a.value = b.data")
 
     def test_join_condition_restrictive_false(self):
-        joins = Where.join_condition(self.COND_CI_CO, self.COND_A_CI, self.COND_B_C, restrictive=False)
-        self.assertEqual(joins, "WHERE (city.last_update != country.country_id) OR (address.address2 <= city.city_id) OR (a.value = b.data)")
+        joins = Where(self.COND_CI_CO, self.COND_A_CI, self.COND_B_C, restrictive=False)
+        self.assertEqual(joins.query, "WHERE city.last_update != country.country_id OR address.address2 <= city.city_id OR a.value = b.data")
 
     def test_join_condition_restrictive_true(self):
-        joins = Where.join_condition(self.COND_CI_CO, self.COND_A_CI, self.COND_B_C, restrictive=True)
-        self.assertEqual(joins, "WHERE (city.last_update != country.country_id) AND (address.address2 <= city.city_id) AND (a.value = b.data)")
+        joins = Where(self.COND_CI_CO, self.COND_A_CI, self.COND_B_C, restrictive=True)
+        self.assertEqual(joins.query, "WHERE city.last_update != country.country_id AND address.address2 <= city.city_id AND a.value = b.data")
 
     def test_value_replace(self):
-        cond = Where[Address, City](instances=(Address, City), function=lambda a, ci: a.city_id == ci.city_id)
+        cond = Where(Address.city_id == City.city_id)
         self.assertEqual(cond.query, "WHERE address.city_id = city.city_id")
 
         city = City(1, "Madrid", 50)
-        cond_2 = Where[Address, City](instances=(Address, City), function=lambda a, ci: a.city_id == ci.country_id, a=ADDRESS_1, ci=city)
-        self.assertEqual(cond_2.query, "WHERE 1 = 50")
+        cond_2 = city.city_id == city.country_id
+        self.assertEqual(cond_2, False)
 
     def test_value_replace_2(self):
         country = Country(50, "Espanna")
 
-        cond_2 = Where[Address, City, Country](
-            instances=(Address, City, Country),
-            function=lambda a, ci, co: a.city_id != ci.city_id <= co.country_id,
-            a=ADDRESS_1,
-            co=country,
-        )
-        self.assertEqual(cond_2.query, "WHERE (1 != city.city_id) AND (city.city_id <= 50)")
+        cond_2 = Where((ADDRESS_1.city_id != City.city_id) & (City.city_id <= country.country_id))
+        self.assertEqual(cond_2.query, "WHERE city.city_id != 1 AND city.city_id <= 50")
 
-    def test_like_condition(self):
-        country = Country(50, "Espanna")
+    # def test_like_condition(self):
+    #     country = Country(50, "Espanna")
 
-        cond_2 = Where[Address, Country](instances=(Address, Country), function=lambda a, c: (a.city_id, ConditionType.REGEXP, c.country), c=country)
-        self.assertEqual(cond_2.query, "WHERE address.city_id REGEXP 'Espanna'")
+    #     cond_2 = Where(Address.city_id, ConditionType.REGEXP, country.country)
+    #     self.assertEqual(cond_2.query, "WHERE address.city_id REGEXP 'Espanna'")
 
-    def test_raise_KeyError(self):
-        with self.assertRaises(KeyError):
-            Where[Address, City, Country](instances=(Address, City, Country), function=lambda a, ci, co: a.city_id != ci.city_id <= co.country_id == 3).query
+    # def test_raise_KeyError(self):
+    #     with self.assertRaises(KeyError):
+    #         Where((Address.city_id != City.city_id) & (City.city_id<= Country.country_id) & (Country.country_id == 3)).query
 
     def test_replace_address(self):
-        cond = Where[City, Address](instances=(City, Address), function=lambda c, a: c.city == a.address, a=ADDRESS_1)
+        cond = Where(City.city == ADDRESS_1.address)
         self.assertEqual(cond.query, "WHERE city.city = 'Calle Cristo de la victoria'")
 
     def test_wrapped_string_variable_content(self):
         variable_ = "var_string"
-        cond = Where[City, Address](instances=(City,), function=lambda c: c.city > variable_, variable_=variable_)
+        cond = Where(City.city > variable_)
         self.assertEqual(cond.query, "WHERE city.city > 'var_string'")
 
     def test_wrapped_string_const_content(self):
-        cond = Where[City, Address](instances=(City,), function=lambda c: c.city > "var_string")
+        cond = Where(City.city > "var_string")
         self.assertEqual(cond.query, "WHERE city.city > 'var_string'")
 
     def test_wrapped_distinct_from_string_variable_content(self):
         variable_ = 1000
-        cond = Where[City, Address](instances=(City,), function=lambda c: c.city > variable_, variable_=variable_)
+        cond = Where(City.city > variable_)
         self.assertEqual(cond.query, "WHERE city.city > 1000")
 
     def test_wrapped_distinct_from_string_const_content(self):
-        cond = Where[City, Address](instances=(City,), function=lambda c: c.city > 1000)
+        cond = Where(City.city > 1000)
         self.assertEqual(cond.query, "WHERE city.city > 1000")
 
-    def test_no_replace_variable(self):
+    def test_replace_variable(self):
         variable_ = "var_string"
-        cond = Where[City, Address](instances=(City,), function=lambda c: c.city > variable_)
-        self.assertEqual(cond.query, "WHERE city.city > 'variable_'")
+        cond = Where(City.city > variable_)
+        self.assertEqual(cond.query, "WHERE city.city > 'var_string'")
 
 
 if __name__ == "__main__":
