a81b10c7bc92a37fce397ecf199e783edae33152
p-hzamora <p.hzamora@icloud.com>
2024-12-24 08:44:21 +0100

    test: updated test_mapped_table

--- src/test/test_mapped_table.py
+++ src/test/test_mapped_table.py
@@ -9,14 +9,14 @@ from ormlambda import Table, Column  # noqa: E402
 class Person(Table):
     __table_name__ = "person"
 
-    id: int = Column(int, is_primary_key=True)
-    title: int = Column(int)
-    content: int = Column(int)
-    dni_list: list = Column(list)
+    id: Column[int] = Column(int, is_primary_key=True)
+    title: Column[str] = Column(str)
+    content: Column[str] = Column(str)
+    dni_list: Column[list] = Column(list)
 
 
 class TestSQLMapping(unittest.TestCase):
-    PERSON_INSTANCE: Person = Person(1, "titleCustom", "contentCustom", [1, 2, 3, 4, 5, 6])
+    PERSON_INSTANCE = Person(1, "titleCustom", "contentCustom", [1, 2, 3, 4, 5, 6])
 
     def test_create_orm(self):
         self.assertEqual(self.PERSON_INSTANCE.id, 1)
@@ -25,16 +25,22 @@ class TestSQLMapping(unittest.TestCase):
         self.assertEqual(self.PERSON_INSTANCE.dni_list, [1, 2, 3, 4, 5, 6])
 
     def test_column_name(self):
-        self.assertEqual(self.PERSON_INSTANCE._id.column_name, "id")
-        self.assertEqual(self.PERSON_INSTANCE._title.column_name, "title")
-        self.assertEqual(self.PERSON_INSTANCE._content.column_name, "content")
-        self.assertEqual(self.PERSON_INSTANCE._dni_list.column_name, "dni_list")
+        self.assertEqual(Person.id.column_name, "id")
+        self.assertEqual(Person.title.column_name, "title")
+        self.assertEqual(Person.content.column_name, "content")
+        self.assertEqual(Person.dni_list.column_name, "dni_list")
 
     def test_column_value(self):
-        self.assertEqual(self.PERSON_INSTANCE._id.column_value, 1)
-        self.assertEqual(self.PERSON_INSTANCE._title.column_value, "titleCustom")
-        self.assertEqual(self.PERSON_INSTANCE._content.column_value, "contentCustom")
-        self.assertEqual(self.PERSON_INSTANCE._dni_list.column_value, [1, 2, 3, 4, 5, 6])
+        self.assertEqual(self.PERSON_INSTANCE.id, 1)
+        self.assertEqual(self.PERSON_INSTANCE.title, "titleCustom")
+        self.assertEqual(self.PERSON_INSTANCE.content, "contentCustom")
+        self.assertEqual(self.PERSON_INSTANCE.dni_list, [1, 2, 3, 4, 5, 6])
+
+    def test_raise_ValueError(self):
+        with self.assertRaises(ValueError) as error:
+            Person(id="should be pass 'int'")
+        mssg: str = "The 'id' Column from 'person' table expected '<class 'int'>' type. You passed 'str' type"
+        self.assertEqual(error.exception.__str__(),mssg)
 
 
 if "__main__" == __name__:
