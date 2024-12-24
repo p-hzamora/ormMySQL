63ac7b079ead05e99ea76fc95951d7fa045ae2a5
phzamora <p.hzamora@icloud.com>
2024-12-24 15:10:35 +0100

    refactor: 'MySQLDecompositionQuery'

--- src/ormlambda/databases/my_sql/mysql_decomposition.py
+++ src/ormlambda/databases/my_sql/mysql_decomposition.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 import typing as tp
 
-from ormlambda import ForeignKey
 from ormlambda.common.abstract_classes import DecompositionQueryBase
 from ormlambda.common.enums import JoinType
 from .clauses import JoinSelector
@@ -13,16 +12,23 @@ if tp.TYPE_CHECKING:
 
 
 class MySQLDecompositionQuery[T: tp.Type[Table], *Ts](DecompositionQueryBase[T, *Ts]):
-    def __init__(self, tables: tuple[T, *Ts], columns: tp.Callable[[T], tuple[*Ts]], *, by: JoinType = JoinType.INNER_JOIN, replace_asterisk_char: bool = True, joins: tp.Optional[list[IJoinSelector]] = None, context: tp.Optional[ClauseInfoContext] = None) -> None:
+    def __init__(
+        self,
+        tables: tuple[T, *Ts],
+        columns: tp.Callable[[T], tuple[*Ts]],
+        *,
+        by: JoinType = JoinType.INNER_JOIN,
+        joins: tp.Optional[list[IJoinSelector]] = None,
+        context: tp.Optional[ClauseInfoContext] = None,
+    ) -> None:
         super().__init__(
             tables=tables,
             columns=columns,
             by=by,
-            replace_asterisk_char=replace_asterisk_char,
             joins=joins,
             context=context,
         )
 
-    def stringify_foreign_key(self, sep: str = "\n") -> str:
-        sorted_joins = JoinSelector.sort_join_selectors(self._joins)
+    def stringify_foreign_key(self, joins: set[JoinSelector], sep: str = "\n") -> str:
+        sorted_joins = JoinSelector.sort_join_selectors(joins)
         return f"{sep}".join([join.query for join in sorted_joins])
