8a44633e3ccce0689ab8851052711551fcd40993
phzamora <p.hzamora@icloud.com>
2024-12-24 15:06:56 +0100

    chore: deleted unnecessary method

--- src/ormlambda/databases/my_sql/mysql_decomposition.py
+++ src/ormlambda/databases/my_sql/mysql_decomposition.py
@@ -26,15 +26,3 @@ class MySQLDecompositionQuery[T: tp.Type[Table], *Ts](DecompositionQueryBase[T, 
     def stringify_foreign_key(self, sep: str = "\n") -> str:
         sorted_joins = JoinSelector.sort_join_selectors(self._joins)
         return f"{sep}".join([join.query for join in sorted_joins])
-
-    def _add_fk_relationship[RTable: tp.Type[Table]](self, foreign_key: ForeignKey[T, RTable]) -> None:
-        comparer = foreign_key.resolved_function(self._context)
-        join_selector = JoinSelector[T, RTable](comparer, self._by, alias="{table}", context=self._context)
-        join_selector.query
-        self._joins.add(join_selector)
-        t2 = comparer._right_condition.table
-        tables = list(self._tables)
-        if t2 not in tables:
-            tables.append(t2)
-        self._tables = tuple(tables)
-        return None
