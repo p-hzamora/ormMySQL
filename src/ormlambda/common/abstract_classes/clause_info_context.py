dfe0dbf8af7840706ae570cc702438a54811dd5b
phzamora <p.hzamora@icloud.com>
2024-12-26 18:24:23 +0100

    fix: ClauseInfoContext class

--- src/ormlambda/common/abstract_classes/clause_info_context.py
+++ src/ormlambda/common/abstract_classes/clause_info_context.py
@@ -33,22 +33,14 @@ class ClauseInfoContext(IClauseInfo):
     def __init__[T: Table, TProp](self, table_context: dict[TableAliasKey[T], str]) -> None: ...
     @overload
     def __init__[T: Table, TProp](self, clause_context: dict[AliasKey[T, TProp], str]) -> None: ...
-    @overload
-    def __init__[T: Table, TProp](self, alias_context: dict[str, AliasKey[T, TProp]]) -> None: ...
 
     def __init__[T: Table, TProp](
         self,
         clause_context: Optional[dict[ClauseAliasKey[T, TProp], str]] = None,
         table_context: Optional[dict[TableAliasKey[T], str]] = None,
-        alias_context: Optional[dict[str, AliasKey[T, TProp]]] = None,
     ) -> None:
-        self._alias_context: dict[str, AliasKey[T, TProp]] = alias_context if alias_context else {}
         self._clause_context: dict[AliasKey[T, TProp], str] = clause_context if clause_context else {}
         self._table_context: dict[AliasKey[T, TProp], str] = table_context if table_context else {}
-
-    @property
-    def is_empty(self) -> bool:
-        return len(self._alias_context) == 0
 
     def add_clause_to_context[T: Table, TProp](self, clause: ClauseInfo[T]) -> None:
         if not clause:
@@ -65,11 +57,7 @@ class ClauseInfoContext(IClauseInfo):
         if not key:
             return None
 
-        if alias not in self._alias_context:
-            self._alias_context[alias] = key
-
-        if key not in self._clause_context:
-            self._clause_context[key] = alias
+        self._clause_context[key] = alias
 
         return None
 
@@ -77,11 +65,7 @@ class ClauseInfoContext(IClauseInfo):
         if not key:
             return None
 
-        if alias not in self._alias_context:
-            self._alias_context[alias] = key
-
-        if key not in self._table_context:
-            self._table_context[key] = alias
+        self._table_context[key] = alias
 
         return None
 
