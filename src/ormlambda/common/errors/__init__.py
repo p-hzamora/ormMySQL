ff74bb81e419e1e9192a5d867e1cc7c86d19623f
phzamora <p.hzamora@icloud.com>
2024-12-26 18:25:18 +0100

    refactor: 'UnmatchedLambdaParameterError' Error

--- src/ormlambda/common/errors/__init__.py
+++ src/ormlambda/common/errors/__init__.py
@@ -1,8 +1,12 @@
+import inspect
+import typing as tp
+
+
 class UnmatchedLambdaParameterError(Exception):
-    def __init__(self, expected_params: int, found_param: tuple[str, ...], *args: object) -> None:
+    def __init__(self, expected_params: int, function: tp.Callable[..., tp.Any], *args: object) -> None:
         super().__init__(*args)
         self.expected_params = expected_params
-        self.found_param: tuple[str, ...] = found_param
+        self.found_param: tuple[str, ...] = tuple(inspect.signature(function).parameters)
 
     def __str__(self) -> str:
         return f"Unmatched number of parameters in lambda function with the number of tables: Expected {self.expected_params} parameters but found {str(self.found_param)}."
