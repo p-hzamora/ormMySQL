e766bc62e5ecbb96314925b1965edd472a944fbf
phzamora <p.hzamora@icloud.com>
2024-12-26 10:03:25 +0100

    fix: passed 'context' attribute in Where class

--- src/ormlambda/databases/my_sql/clauses/where.py
+++ src/ormlambda/databases/my_sql/clauses/where.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import typing as tp
 from ormlambda.common.abstract_classes.comparer import Comparer
 from ormlambda.common.abstract_classes.clause_info import AggregateFunctionBase
+from ormlambda.common.abstract_classes.clause_info_context import ClauseInfoContext
 
 
 class Where(AggregateFunctionBase):
@@ -9,9 +10,10 @@ class Where(AggregateFunctionBase):
     The purpose of this class is to create 'WHERE' condition queries properly.
     """
 
-    def __init__(self, *comparer: Comparer, restrictive: bool = True) -> None:
+    def __init__(self, *comparer: Comparer, restrictive: bool = True, context: tp.Optional[ClauseInfoContext] = None) -> None:
         self._comparer: tuple[Comparer] = comparer
         self._restrictive: bool = restrictive
+        self._context: tp.Optional[ClauseInfoContext] = context
 
     @staticmethod
     def FUNCTION_NAME() -> str:
@@ -20,7 +22,7 @@ class Where(AggregateFunctionBase):
     @property
     def query(self) -> str:
         if isinstance(self._comparer, tp.Iterable):
-            comparer = Comparer.join_comparers(self._comparer, restrictive=self._restrictive)
+            comparer = Comparer.join_comparers(self._comparer, restrictive=self._restrictive, context=lambda: self._context)
         else:
             comparer = self._comparer
         return f"{self.FUNCTION_NAME()} {comparer}"
@@ -30,8 +32,10 @@ class Where(AggregateFunctionBase):
         return None
 
     @staticmethod
-    def join_condition(wheres: tp.Iterable[Where], restrictive: bool) -> str:
-        comparers = []
-        for x in wheres:
-            comparers.extend(x._comparer)
-        return Where(*comparers, restrictive=restrictive).query
+    def join_condition(wheres: tp.Iterable[Where], restrictive: bool, context: ClauseInfoContext) -> str:
+        comparers: list[Comparer] = []
+        for where in wheres:
+            for c in where._comparer:
+                c.set_context(lambda: context)
+                comparers.append(c)
+        return Where(*comparers, restrictive=restrictive, context=context).query
