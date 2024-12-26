6ddbb1f4349e13b38f9f4f000fcceeafff3ba42f
phzamora <p.hzamora@icloud.com>
2024-12-26 11:13:12 +0100

    delete: lambda_disassembler/ folder

--- src/ormlambda/utils/lambda_disassembler/__init__.py
+++
@@ -1,4 +0,0 @@
-from .disassembler import Disassembler  # noqa: F401
-from .nested_element import NestedElement  # noqa: F401
-from .tree_instruction import TreeInstruction, TupleInstruction  # noqa: F401
-from .name_of import nameof  # noqa: F401
