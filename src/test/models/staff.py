b7f585a9f486c900f8bd16a22c1cbc358090fbcf
p-hzamora <p.hzamora@icloud.com>
2024-12-27 12:20:29 +0100

    chore: updated Staff model

--- src/test/models/staff.py
+++ src/test/models/staff.py
@@ -16,17 +16,17 @@ from .store import Store
 class Staff(Table):
     __table_name__ = "staff"
 
-    staff_id: int = Column(int, is_primary_key=True, is_auto_increment=True)
-    first_name: str
-    last_name: str
-    address_id: str
-    picture: bytes
-    email: str
-    store_id: int
-    active: int
-    username: str
-    password: str
-    last_update: datetime
+    staff_id: Column[int] = Column(int, is_primary_key=True, is_auto_increment=True)
+    first_name: Column[str] = Column(str)
+    last_name: Column[str] = Column(str)
+    address_id: Column[int] = Column(int)
+    picture: Column[bytes] = Column(bytes)
+    email: Column[str] = Column(str)
+    store_id: Column[int] = Column(int)
+    active: Column[int] = Column(int)
+    username: Column[str] = Column(str)
+    password: Column[str] = Column(str)
+    last_update: Column[datetime] = Column(datetime)
 
     Address = ForeignKey[Self, Address](Address, lambda s, a: s.staff_id == a.address_id)
     Store = ForeignKey[Self, Store](Store, lambda staff, store: staff.staff_id == store.store_id)
