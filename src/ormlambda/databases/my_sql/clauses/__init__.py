4f9cd2951db3580698c7380860ad6224eddcd117
phzamora <p.hzamora@icloud.com>
2025-01-09 11:38:33 +0100

    chore: renamed aliases

--- src/ormlambda/databases/my_sql/clauses/__init__.py
+++ src/ormlambda/databases/my_sql/clauses/__init__.py
@@ -6,8 +6,8 @@ from .drop_table import DropTable as DropTable
 from .insert import InsertQuery as InsertQuery
 from .joins import JoinSelector as JoinSelector
 from .joins import JoinType as JoinType
-from .limit import LimitQuery as LimitQuery
-from .offset import OffsetQuery as OffsetQuery
+from .limit import Limit as Limit
+from .offset import Offset as Offset
 from .order import Order as Order
 from .update import UpdateQuery as UpdateQuery
 from .upsert import UpsertQuery as UpsertQuery
@@ -16,3 +16,4 @@ from .count import Count as Count
 from .group_by import GroupBy as GroupBy
 from .alias import Alias as Alias
 from .ST_AsText import ST_AsText as ST_AsText
+from .select import Select as Select
