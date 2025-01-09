ea7f688c4ba8e229253b863c4e73feb6b5dff0c5
phzamora <p.hzamora@icloud.com>
2025-01-09 17:03:38 +0100

    wip: 'Alias' class

--- src/ormlambda/databases/my_sql/clauses/alias.py
+++ src/ormlambda/databases/my_sql/clauses/alias.py
@@ -1,9 +1,11 @@
 import typing as tp
 
 from ormlambda.types import AliasType, ColumnType
-from ormlambda.common.abstract_classes.clause_info import ClauseInfo
-from ormlambda import Table, Column
+from ormlambda.common.abstract_classes.clause_info import AggregateFunctionBase, ClauseInfo
 from ormlambda.utils.foreign_key import ForeignKey
+from ormlambda.utils.column import Column
+from ormlambda.utils.table_constructor import Table
+from ormlambda.common.abstract_classes.clause_info_context import ClauseInfoContext
 
 
 class Alias(ClauseInfo[None]):
@@ -12,18 +14,31 @@ class Alias(ClauseInfo[None]):
         column: tp.Optional[ColumnType[TProp]] = None,
         alias_table: tp.Optional[AliasType[ClauseInfo[None]]] = None,
         alias_clause: tp.Optional[AliasType[ClauseInfo[None]]] = None,
-        context=None,
+        context: ClauseInfoContext = None,
+        table: Table = None,
     ):
+        if isinstance(column, AggregateFunctionBase):
+            ci = column
+            ci._table = self._extract_table(ci.unresolved_column)
+        else:
+            ci = AggregateFunctionBase._convert_into_clauseInfo(column, context)[0]
+        ci.alias_clause = alias_clause
+        ci.alias_table = alias_table if alias_table else ci.alias_table
         super().__init__(
-            table=self._extract_table(column),
-            column=column,
-            alias_table=alias_table,
-            alias_clause=alias_clause,
-            context=context,
+            table=ci.table,
+            column=ci._column,
+            alias_table=ci.alias_table,
+            alias_clause=ci.alias_clause,
+            context=ci.context,
         )
 
+    @tp.override
+    @property
+    def query(self) -> str:
+        return super().query
+
     def _extract_table[TProp](self, obj: ColumnType[TProp]) -> tp.Optional[Table]:
-        if isinstance(obj, type) and issubclass(obj, Table):
+        if isinstance(obj, type) and issubclass(obj, Table):  # noqa: F821
             return obj
 
         if isinstance(obj, ForeignKey):
