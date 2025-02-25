05e8340bf4c96401791a499d8d72e882d9191fe1
phzamora <p.hzamora@icloud.com>
2025-02-26 00:26:55 +0100

    feat: added env.py

---
+++ src/test/env.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import os
+
+APP_DIR = Path(__file__).parent
+TEST_DIR = APP_DIR.parent
+SRC_DIR = TEST_DIR.parent
+BASE_DIR = SRC_DIR.parent
+
+try:
+    from dotenv import load_dotenv, find_dotenv
+
+    load_dotenv(find_dotenv(str(BASE_DIR/'.env')))
+
+except ImportError:
+    print("python-dotenv is not installed. Skipping...")
+
+
+#################################
+# Load DATABASE ENV
+#################################
+
+DB_USERNAME = os.getenv("USERNAME",None)
+
+DB_PASSWORD = os.getenv("PASSWORD",None)
+
+DB_HOST = os.getenv("HOST",'localhost')
+
+DB_DATABASE = os.getenv("DATABASE",'sakila')
+
