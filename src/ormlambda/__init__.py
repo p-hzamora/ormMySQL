186c580a63f069592dca26e4f9a4fcef41b88d23
phzamora <p.hzamora@icloud.com>
2025-03-03 21:17:04 +0100

    chore: added BaseRepository to ormlambda __init__

--- src/ormlambda/__init__.py
+++ src/ormlambda/__init__.py
@@ -13,6 +13,7 @@ from .sql import (
 )
 # endregion
 
+from .repository import BaseRepository as BaseRepository
 from .statements import BaseStatement as BaseStatement
 from .statements.types import OrderType as OrderType
 from .model.base_model import BaseModel as BaseModel  # COMMENT: to avoid relative import we need to import BaseModel after import Table,Column, ForeignKey, IRepositoryBase and Disassembler
