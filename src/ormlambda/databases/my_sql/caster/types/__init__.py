a40a0b4c10b605a54bbdd1eb35226046a3ffe53f
p-hzamora <p.hzamora@icloud.com>
2025-03-06 11:33:13 +0100

    chore: added new casters to __init__

--- src/ormlambda/databases/my_sql/caster/types/__init__.py
+++ src/ormlambda/databases/my_sql/caster/types/__init__.py
@@ -4,3 +4,5 @@ from .float import FloatCaster as FloatCaster
 from .point import PointCaster as PointCaster
 from .none import NoneTypeCaster as NoneTypeCaster
 from .datetime import DatetimeCaster as DatetimeCaster
+from .bytes import BytesCaster as BytesCaster
+from .iterable import IterableCaster as IterableCaster
