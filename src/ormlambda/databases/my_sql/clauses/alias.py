1e6697e0e628e21dbcd425d42ef2ff164c142b2b
phzamora <p.hzamora@icloud.com>
2025-03-10 19:26:00 +0100

    update: alias

--- src/ormlambda/databases/my_sql/clauses/alias.py
+++ src/ormlambda/databases/my_sql/clauses/alias.py
@@ -1,30 +1,32 @@
 from __future__ import annotations
-from ormlambda.sql.clause_info import ClauseInfo
-
-from ormlambda import Table
-
 import typing as tp
 
+from ormlambda import Table, Column
+from ormlambda.sql.clause_info import ClauseInfo
 from ormlambda.common.interfaces.IQueryCommand import IQuery
-from ormlambda.sql.clause_info.clause_info_context import ClauseInfoContext
-
+from ormlambda.sql.clause_info import ClauseInfoContext, IAggregate
 
 if tp.TYPE_CHECKING:
-    from ormlambda.sql.clause_info import IAggregate
+    from ormlambda.sql.types import ColumnType
     from ormlambda import Table
     from ormlambda.sql.types import AliasType
 
 
-class Alias[T: Table](IQuery):
+class Alias[T: Table, TProp](ClauseInfo, IQuery):
     def __init__(
         self,
-        element: IAggregate | ClauseInfo[T],
+        element: IAggregate | ClauseInfo[T] | ColumnType[TProp],
         alias_clause: tp.Optional[AliasType[ClauseInfo[T]]],
     ):
-        context = ClauseInfoContext(table_context=element.context._table_context, clause_context={})
-        element.context = context
+        if isinstance(element, Column):
+            context = ClauseInfoContext()
+            element = ClauseInfo(table=element.table, column=element, alias_clause=alias_clause)
+        else:
+            context = ClauseInfoContext(table_context=element.context._table_context, clause_context={})
+            element.context = context
+            element._alias_clause = alias_clause
+
         self._element = element
-        self._element._alias_clause = alias_clause
 
     @tp.override
     @property
