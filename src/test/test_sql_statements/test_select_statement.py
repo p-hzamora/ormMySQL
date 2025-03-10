f4e19d360168e4a19b718a52abdeaca2577b4c35
phzamora <p.hzamora@icloud.com>
2025-03-10 19:28:49 +0100

    test: wip in test_select_statement

--- src/test/test_sql_statements/test_select_statement.py
+++ src/test/test_sql_statements/test_select_statement.py
@@ -2,7 +2,6 @@ import unittest
 import typing as tp
 import sys
 from pathlib import Path
-from mysql.connector import MySQLConnection
 
 
 sys.path.append([str(x) for x in Path(__file__).parents if x.name == "src"].pop())
@@ -12,7 +11,6 @@ from ormlambda.repository import IRepositoryBase
 from ormlambda.databases.my_sql import MySQLRepository
 from config import config_dict  # noqa: E402
 from ormlambda import Table, Column, BaseModel, ForeignKey  # noqa: E402
-
 
 DDBBNAME = "__test_ddbb__"
 
@@ -45,21 +43,6 @@ class AWithMultipleReferencesToB(Table):
     B_fk_b3 = ForeignKey[tp.Self, BSimple](BSimple, lambda self, b: self.fk_b3 == b.pk_b)
 
 
-class AModel(BaseModel[AWithMultipleReferencesToB]):
-    def __new__[TRepo](cls, repository: IRepositoryBase):
-        return super().__new__(cls, AWithMultipleReferencesToB, repository)
-
-
-class CSimpleModel(BaseModel[CSimple]):
-    def __new__[TRepo](cls, repository: IRepositoryBase):
-        return super().__new__(cls, CSimple, repository)
-
-
-class BSimpleModel(BaseModel[BSimple]):
-    def __new__[TRepo](cls, repository: IRepositoryBase):
-        return super().__new__(cls, BSimple, repository)
-
-
 class TestJoinQueries(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
@@ -67,9 +50,9 @@ class TestJoinQueries(unittest.TestCase):
         cls.ddbb.create_database(DDBBNAME, "replace")
         cls.ddbb.database = DDBBNAME
 
-        cls.model = AModel(cls.ddbb)
-        CSimpleModel(cls.ddbb).create_table()
-        BSimpleModel(cls.ddbb).create_table()
+        cls.model = BaseModel(AWithMultipleReferencesToB, cls.ddbb)
+        BaseModel(CSimple, cls.ddbb).create_table()
+        BaseModel(BSimple, cls.ddbb).create_table()
         cls.model.create_table()
 
     @classmethod
@@ -86,15 +69,18 @@ class TestJoinQueries(unittest.TestCase):
         self.model.select(
             lambda a: (
                 a.data_a,
-                a.B_fk_b1.name,
-                a.B_fk_b2.name,
-                a.B_fk_b3.name,
-                a.B_fk_b1.CSimple.name,
-                a.B_fk_b2.CSimple.name,
+                self.model.alias(a.B_fk_b1.name, alias="b_b1_name"),
+                self.model.alias(a.B_fk_b2.name, alias="b_b2_name"),
+                self.model.alias(a.B_fk_b3.name, alias="b_b3_name"),
+                self.model.alias(a.B_fk_b1.CSimple.name, alias="b1_c_name"),
+                self.model.alias(a.B_fk_b2.CSimple.name, alias="b2_c_name"),
             ),
         )
         real_query: str = "SELECT `A`.data_a AS `A_data_a`, `A_fk_b3_pk_b`.name AS `B_name`, `A_fk_b3_pk_b`.name AS `B_name`, `A_fk_b3_pk_b`.name AS `B_name`, `B_fk_c_pk_c`.name AS `C_name`, `B_fk_c_pk_c`.name AS `C_name` FROM A AS `A` INNER JOIN B AS `A_fk_b1_pk_b` ON `A`.fk_b1 = `A_fk_b1_pk_b`.pk_b INNER JOIN B AS `A_fk_b2_pk_b` ON `A`.fk_b2 = `A_fk_b2_pk_b`.pk_b INNER JOIN B AS `A_fk_b3_pk_b` ON `A`.fk_b3 = `A_fk_b3_pk_b`.pk_b INNER JOIN C AS `B_fk_c_pk_c` ON `A_fk_b3_pk_b`.fk_c = `B_fk_c_pk_c`.pk_c"
         self.assertEqual(self.model.query, real_query)
+
+        select = self.model.select(lambda x: (x.data_a, x.B_fk_b1.name,), flavour=tuple)
+        print(select)
 
 
 if __name__ == "__main__":
