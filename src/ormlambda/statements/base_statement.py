3e715e2ac05e1949aa13b44e20d478cffefb4b50
p-hzamora <p.hzamora@icloud.com>
2025-03-21 16:30:32 +0100

    update: change 'IRepositoryBase' by 'BaseRepository' class

--- src/ormlambda/statements/base_statement.py
+++ src/ormlambda/statements/base_statement.py
@@ -3,11 +3,12 @@ from typing import Any, Type, override, Iterable, Literal, TYPE_CHECKING, Option
 from collections import defaultdict
 
 
-from ormlambda.repository import IRepositoryBase
+from ormlambda.repository import BaseRepository
 from ormlambda.statements.interfaces import IStatements_two_generic
 from ormlambda import Table
 
 from ormlambda.sql.clause_info import AggregateFunctionBase
+from ormlambda.caster.caster import Caster
 
 
 if TYPE_CHECKING:
@@ -19,13 +20,13 @@ ORDER_QUERIES = Literal["select", "join", "where", "order", "with", "group by", 
 
 
 class BaseStatement[T: Table, TRepo](IStatements_two_generic[T, TRepo]):
-    def __init__(self, model: tuple[T], repository: IRepositoryBase) -> None:
+    def __init__(self, model: tuple[T], repository: BaseRepository) -> None:
         self.__valid_repository(repository)
 
         self._query: Optional[str] = None
         self._model: T = model[0] if isinstance(model, Iterable) else model
         self._models: tuple[T] = self._model if isinstance(model, Iterable) else (model,)
-        self._repository: IRepositoryBase = repository
+        self._repository: BaseRepository = repository
 
         if not issubclass(self._model, Table):
             # Deben heredar de Table ya que es la forma que tenemos para identificar si estamos pasando una instancia del tipo que corresponde o no cuando llamamos a insert o upsert.
@@ -34,8 +35,8 @@ class BaseStatement[T: Table, TRepo](IStatements_two_generic[T, TRepo]):
 
     @staticmethod
     def __valid_repository(repository: Any) -> bool:
-        if not isinstance(repository, IRepositoryBase):
-            raise ValueError(f"'repository' attribute does not instance of '{IRepositoryBase.__name__}'")
+        if not isinstance(repository, BaseRepository):
+            raise ValueError(f"'repository' attribute does not instance of '{BaseRepository.__name__}'")
         return True
 
     def __repr__(self):
@@ -48,7 +49,7 @@ class BaseStatement[T: Table, TRepo](IStatements_two_generic[T, TRepo]):
         response_sql = self._repository.read_sql(query, flavour=dict, model=self._model, select=select)  # store all columns of the SQL query
 
         if response_sql and isinstance(response_sql, Iterable):
-            return ClusterQuery[T](select, response_sql).clean_response()
+            return ClusterQuery(self.repository, select, response_sql).clean_response()
 
         return response_sql
 
@@ -67,17 +68,18 @@ class BaseStatement[T: Table, TRepo](IStatements_two_generic[T, TRepo]):
 
     @property
     @override
-    def repository(self) -> IRepositoryBase: ...
+    def repository(self) -> BaseRepository: ...
 
 
 class ClusterQuery[T]:
-    def __init__(self, select: DecompositionQueryBase[T], response_sql: tuple[dict[str, Any]]) -> None:
+    def __init__(self, repository: BaseRepository, select: DecompositionQueryBase[T], response_sql: tuple[dict[str, Any]]) -> None:
+        self._repository: BaseRepository = repository
         self._select: DecompositionQueryBase[T] = select
         self._response_sql: tuple[dict[str, Any]] = response_sql
         self._caster = Caster(repository)
 
     def clean_response(self) -> tuple[dict[Type[Table], tuple[Table, ...]]]:
-        tbl_dicc: dict[Type[Table], list[Table]] = self.__loop_foo()
+        tbl_dicc: dict[Type[Table], list[dict[str, Any]]] = self.__loop_foo()
 
         response = {}
         tuple_response = []
