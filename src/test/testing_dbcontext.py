3459e1045d456479be37b62f520595fdc2d79c55
phzamora <p.hzamora@icloud.com>
2025-03-21 17:46:26 +0100

    test: updated testing_dbcontext.py

--- src/test/testing_dbcontext.py
+++ src/test/testing_dbcontext.py
@@ -11,37 +11,32 @@ import math
 sys.path.append([str(x) for x in Path(__file__).parents if x.name == "src"].pop())
 
 from ormlambda.databases.my_sql import MySQLRepository  # noqa: E402
+from ormlambda import ORM
 from ormlambda.databases.my_sql.clauses.joins import JoinType  # noqa: E402
-from models.staff import StaffModel, Staff  # noqa: E402
-from models.address import AddressModel, Address  # noqa: E402
-from models.actor import ActorModel, Actor  # noqa: E402
-from models.store import StoreModel  # noqa: E402
+from models.staff import Staff  # noqa: E402
+from models.address import Address  # noqa: E402
+from models.actor import Actor  # noqa: E402
+from models.store import Store  # noqa: E402
 
 
-a = Staff.find_dependent_tables()
+db = MySQLRepository(user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE, host=DB_HOST)
 
-database = MySQLRepository(user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE, host=DB_HOST)
-
-actor_model = ActorModel(database)
-store_model = StoreModel(database)
-a_model = AddressModel(database)
-s_model = StaffModel(database)
+StaffModel = ORM(Staff, db)
+AddressModel = ORM(Address, db)
+ActorModel = ORM(Actor, db)
+StoreModel = ORM(Store, db)
 
 
-result = (
-    AddressModel(database)
-    .where(Address.City.Country.country_id == 87)
-    .select(
-        lambda address: (
-            address.city_id,
-            address.City.city_id,
-            address.City.country_id,
-            address.City.Country.country_id,
-        ),
-        flavour=tuple,
-    )
+result = AddressModel.where(Address.City.Country.country_id == 87).select(
+    lambda address: (
+        address.city_id,
+        address.City.city_id,
+        address.City.country_id,
+        address.City.Country.country_id,
+    ),
+    flavour=tuple,
 )
-res = a_model.where(Address.City.Country.country.regex(r"^[aA]")).select(
+res = AddressModel.where(Address.City.Country.country.regex(r"^[aA]")).select(
     lambda a: (
         a,
         a.City,
@@ -55,23 +50,23 @@ res = a_model.where(Address.City.Country.country.regex(r"^[aA]")).select(
 query = Actor.create_table_query()
 
 asdf = "TEST_DB"
-s_model.repository.create_database(asdf, "replace")
-s_model.repository.drop_database(asdf)
+StaffModel.repository.create_database(asdf, "replace")
+StaffModel.repository.drop_database(asdf)
 
 
-staff = s_model.where(Staff.staff_id == 1).select_one()
+staff = StaffModel.where(Staff.staff_id == 1).select_one()
 staff.staff_id = 100
-s_model.upsert(staff)
-id = s_model.order(lambda x: x.staff_id, order_type="DESC").where(Staff.staff_id == staff.staff_id).select_one().staff_id
-new_staff = s_model.where(Staff.staff_id == id).select_one()
+StaffModel.upsert(staff)
+id = StaffModel.order(lambda x: x.staff_id, order_type="DESC").where(Staff.staff_id == staff.staff_id).select_one().staff_id
+new_staff = StaffModel.where(Staff.staff_id == id).select_one()
 new_staff.first_name = "PEPON"
-s_model.upsert(new_staff)
+StaffModel.upsert(new_staff)
 
-staffs = s_model.where(Staff.staff_id > 2).delete()
+staffs = StaffModel.where(Staff.staff_id > 2).delete()
 
 
 res_tuple_3 = (
-    a_model.order(lambda a: a.address_id, order_type="DESC")
+    AddressModel.order(lambda a: a.address_id, order_type="DESC")
     .where(Address.City.Country.country_id == 87)
     .select(
         lambda a: (
@@ -83,7 +78,7 @@ res_tuple_3 = (
 )
 
 result = (
-    a_model.order(lambda a: a.address_id, order_type="DESC")
+    AddressModel.order(lambda a: a.address_id, order_type="DESC")
     .where(Address.City.Country.country_id.regex(r"^[A]"))
     .limit(100)
     .select(
@@ -98,9 +93,9 @@ result = (
 )
 
 
-res_one_table_five_results = a_model.where(Address.address_id <= 5).limit(100).order(lambda a: a.address_id, order_type="DESC").select(lambda a: (a,))
-res_one_table_one_result = a_model.where(Address.address_id == 5).order(lambda a: a.address_id, order_type="DESC").select_one()
-res_one_table_one_result = a_model.where(Address.address2 == 100).order(lambda a: a.address_id, order_type="DESC").select_one(lambda x: x)
+res_one_table_five_results = AddressModel.where(Address.address_id <= 5).limit(100).order(lambda a: a.address_id, order_type="DESC").select(lambda a: (a,))
+res_one_table_one_result = AddressModel.where(Address.address_id == 5).order(lambda a: a.address_id, order_type="DESC").select_one()
+res_one_table_one_result = AddressModel.where(Address.address2 == 100).order(lambda a: a.address_id, order_type="DESC").select_one(lambda x: x)
 
 
 query = """
@@ -108,19 +103,19 @@ SELECT address.address_id as `kkk`, country.country as `country_country`, city.c
 WHERE country.country REGEXP '^[FHA]' AND (city.city REGEXP '[^W]') AND (177 >= address.address_id) AND (address.address_id <= 28)
 ORDER BY address.address_id DESC
 """
-address = a_model.repository.read_sql(query, flavour=dict)
+address = AddressModel.repository.read_sql(query, flavour=dict)
 pass
 
 
 def pagination(page: int):
     limit = 20
-    total_register: int = a_model.repository.read_sql(f"SELECT COUNT(*) FROM {a_model._model.__table_name__}")[0][0]
+    total_register: int = AddressModel.repository.read_sql(f"SELECT COUNT(*) FROM {AddressModel._model.__table_name__}")[0][0]
     total_pages = int(math.ceil(total_register / limit))
 
     if page > total_pages:
         page = total_pages
     offset = (page - 1) * limit
-    return {"page": page, "pages": total_pages, "limit": limit, "data": a_model.limit(limit).offset(offset).select(flavour=dict)}
+    return {"page": page, "pages": total_pages, "limit": limit, "data": AddressModel.limit(limit).offset(offset).select(flavour=dict)}
 
 
 a = [pagination(x) for x in range(1, 11)]
