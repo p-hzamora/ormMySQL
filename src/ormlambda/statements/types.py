f2fe5967c7f20d57b2ba746765e04102cbd867a8
p-hzamora <p.hzamora@icloud.com>
2025-03-24 09:28:45 +0100

    update: statements types

--- src/ormlambda/statements/types.py
+++ src/ormlambda/statements/types.py
@@ -25,8 +25,7 @@ class OrderType(enum.Enum):
 
 type Tuple[T] = tuple[T, ...]
 
-type Select1[T1] = tuple[Tuple[T1]]
-type Select2[T1, T2] = tuple[*Select1[T1], Tuple[T2]]
+type Select2[T1, T2] = tuple[Tuple[T1], Tuple[T2]]
 type Select3[T1, T2, T3] = tuple[*Select2[T1, T2], Tuple[T3]]
 type Select4[T1, T2, T3, T4] = tuple[*Select3[T1, T2, T3], Tuple[T4]]
 type Select5[T1, T2, T3, T4, T5] = tuple[*Select4[T1, T2, T3, T4], Tuple[T5]]
@@ -38,13 +37,6 @@ type Select10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10] = tuple[*Select9[T1, T2, 
 
 type WhereCondition[T, T1] = Callable[[T, T1], bool]
 type JoinCondition[T, T1] = tuple[T1, WhereCondition[T, T1], Optional[JoinType]]
-
-type TupleJoins1[T, T1] = tuple[JoinCondition[T, T1]]
-type TupleJoins2[T, T1, T2] = tuple[*TupleJoins1[T, T1], JoinCondition[T, T2]]
-type TupleJoins3[T, T1, T2, T3] = tuple[*TupleJoins2[T, T1, T2], JoinCondition[T, T3]]
-type TupleJoins4[T, T1, T2, T3, T4] = tuple[*TupleJoins3[T, T1, T2, T3], JoinCondition[T, T4]]
-type TupleJoins5[T, T1, T2, T3, T4, T5] = tuple[*TupleJoins4[T, T1, T2, T3, T4], JoinCondition[T, T5]]
-type TupleJoins6[T, T1, T2, T3, T4, T5, T6] = tuple[*TupleJoins5[T, T1, T2, T3, T4, T5], JoinCondition[T, T6]]
 
 
 # TODOH: This var is duplicated from 'src\ormlambda\databases\my_sql\clauses\create_database.py'
