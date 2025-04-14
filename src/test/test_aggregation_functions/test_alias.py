569f880c01de458a3a4421fdec0b50f8894a421e
p-hzamora <p.hzamora@icloud.com>
2025-04-14 14:44:51 +0200

    test: updated test_alias

--- src/test/test_aggregation_functions/test_alias.py
+++ src/test/test_aggregation_functions/test_alias.py
@@ -5,8 +5,6 @@ import unittest
 
 sys.path.insert(0, [str(x.parent) for x in Path(__file__).parents if x.name == "test"].pop())
 
-
-from ormlambda.sql.clause_info import ClauseInfo
 from ormlambda.sql.clause_info.clause_info_context import ClauseInfoContext
 from ormlambda.databases.my_sql.clauses import Alias
 
@@ -18,26 +16,26 @@ from test.models import D, Address, City
 class TestAlias(unittest.TestCase):
     def test_alias_without_aliases(self) -> None:
         with self.assertRaises(TypeError):
-            Alias(ClauseInfo(D.data_d))
+            Alias(D.data_d)
 
     def test_alias_with_alias_table(self) -> None:
         query = "`other_name`.data_d AS `custom-alias`"
         self.assertEqual(
-            Alias(ClauseInfo(D.data_d, alias_table="other_name"), alias_clause="custom-alias").query,
+            Alias(D.data_d, alias_table="other_name", alias_clause="custom-alias").query,
             query,
         )
 
     def test_alias_with_clause_table(self) -> None:
         query = "d.data_d AS `override-original-clause`"
         self.assertEqual(
-            Alias(ClauseInfo(D.data_d, alias_clause="other_name"), alias_clause="override-original-clause").query,
+            Alias(D.data_d, alias_clause="override-original-clause").query,
             query,
         )
 
     def test_alias_passing_only_table(self) -> None:
         query = "`other_name`.* AS `name_for_column`"
         self.assertEqual(
-            Alias(ClauseInfo(D, D, alias_table="other_name"), alias_clause="name_for_column").query,
+            Alias(D, D, alias_table="other_name", alias_clause="name_for_column").query,
             query,
         )
 
@@ -46,11 +44,8 @@ class TestAlias(unittest.TestCase):
         query = "`ctx_name_for_address`.city_id AS `alias-name`"
         self.assertEqual(
             Alias(
-                ClauseInfo(
-                    Address.City.city_id,
-                    alias_clause="{table}~{column}",
-                    context=ctx,
-                ),
+                Address.City.city_id,
+                context=ctx,
                 alias_clause="alias-name",
             ).query,
             query,
