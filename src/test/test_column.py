c7bab2c720415154966173729cd80f5c81a4a219
p-hzamora <p.hzamora@icloud.com>
2025-04-17 00:27:46 +0200

    test: updated test_column file

--- src/test/test_column.py
+++ src/test/test_column.py
@@ -6,12 +6,25 @@ import unittest
 sys.path.insert(0, [str(x.parent) for x in Path(__file__).parents if x.name == "test"].pop())
 
 
+from ormlambda.sql.column import Column
 from test.models import A
 
 
-class TestClauseInfo(unittest.TestCase):
+class TestColumn(unittest.TestCase):
     def test_passing_only_table(self):
         self.assertEqual(A.pk_a.table, A)
+
+    def test_comparing_column_hashes_and_failed(self):
+        EXPECTED = Column(str)
+        column = Column(column_name="x-name")
+
+        with self.assertRaises(AssertionError):
+            self.assertEqual(hash(EXPECTED), hash(column))
+
+    def test_comparing_column_hashes_and_success(self):
+        EXPECTED = Column(str, column_name="x-name")
+        column = Column(column_name="x-name")
+        self.assertEqual(hash(EXPECTED), hash(column))
 
 
 if __name__ == "__main__":
