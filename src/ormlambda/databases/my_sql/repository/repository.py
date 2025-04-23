bea61b023e2ad3624a12a67df51658d3aac9361f
p-hzamora <p.hzamora@icloud.com>
2025-04-23 08:39:00 +0200

    refactor: moved Response class

--- src/ormlambda/databases/my_sql/repository/repository.py
+++ src/ormlambda/databases/my_sql/repository/repository.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 import contextlib
 from pathlib import Path
-from typing import Any, Generator, Iterable, Optional, Type, override, TYPE_CHECKING, Unpack
+from typing import Generator, Iterable, Optional, Type, override, TYPE_CHECKING, Unpack
 import uuid
-import shapely as shp
 
 # from mysql.connector.pooling import MySQLConnectionPool
 from mysql.connector import MySQLConnection  # noqa: F401
@@ -11,145 +10,16 @@ from mysql.connector.pooling import MySQLConnectionPool  # noqa: F401
 from ormlambda.repository import BaseRepository
 
 # Custom libraries
-from ormlambda.repository import IRepositoryBase
-from ormlambda.caster import Caster
-
 from ..clauses import CreateDatabase, TypeExists
 from ..clauses import DropDatabase
 from ..clauses import DropTable
-from ..clauses import Alias
+from ormlambda.repository.response import Response
 
 
 if TYPE_CHECKING:
-    from ormlambda.common.abstract_classes.decomposition_query import ClauseInfo
-    from ormlambda import Table
     from ormlambda.databases.my_sql.clauses.select import Select
     from ..types import MySQLArgs
 
-type TResponse[TFlavour, *Ts] = TFlavour | tuple[dict[str, tuple[*Ts]]] | tuple[tuple[*Ts]] | tuple[TFlavour]
-
-
-class Response[TFlavour, *Ts]:
-    def __init__(self, repository: IRepositoryBase, response_values: list[tuple[*Ts]], columns: tuple[str], flavour: Type[TFlavour], model: Optional[Table] = None, select: Optional[Select] = None) -> None:
-        self._repository: IRepositoryBase = repository
-        self._response_values: list[tuple[*Ts]] = response_values
-        self._columns: tuple[str] = columns
-        self._flavour: Type[TFlavour] = flavour
-        self._model: Table = model
-        self._select: Select = select
-
-        self._response_values_index: int = len(self._response_values)
-        # self.select_values()
-        self._caster = Caster(repository)
-
-    @property
-    def is_one(self) -> bool:
-        return self._response_values_index == 1
-
-    @property
-    def is_there_response(self) -> bool:
-        return self._response_values_index != 0
-
-    @property
-    def is_many(self) -> bool:
-        return self._response_values_index > 1
-
-    def response(self, _tuple: bool, **kwargs) -> TResponse[TFlavour, *Ts]:
-        if not self.is_there_response:
-            return tuple([])
-        cleaned_response = self._response_values
-
-        if self._select is not None:
-            cleaned_response = self._parser_response()
-
-        cast_flavour = self._cast_to_flavour(cleaned_response, **kwargs)
-        if _tuple is not True:
-            return cast_flavour
-
-        return tuple(cast_flavour)
-
-    def _cast_to_flavour(self, data: list[tuple[*Ts]], **kwargs) -> list[dict[str, tuple[*Ts]]] | list[tuple[*Ts]] | list[TFlavour]:
-        def _dict(**kwargs) -> list[dict[str, tuple[*Ts]]]:
-            nonlocal data
-            return [dict(zip(self._columns, x)) for x in data]
-
-        def _tuple(**kwargs) -> list[tuple[*Ts]]:
-            nonlocal data
-            return data
-
-        def _set(**kwargs) -> list[set]:
-            nonlocal data
-            for d in data:
-                n = len(d)
-                for i in range(n):
-                    try:
-                        hash(d[i])
-                    except TypeError:
-                        raise TypeError(f"unhashable type '{type(d[i])}' found in '{type(d)}' when attempting to cast the result into a '{set.__name__}' object")
-            return [set(x) for x in data]
-
-        def _list(**kwargs) -> list[list]:
-            nonlocal data
-            return [list(x) for x in data]
-
-        def _default(**kwargs) -> list[TFlavour]:
-            nonlocal data
-            replacer_dicc: dict[str, str] = {}
-
-            for col in self._select.all_clauses:
-                if hasattr(col, "_alias_aggregate") or col.alias_clause is None or isinstance(col, Alias):
-                    continue
-                replacer_dicc[col.alias_clause] = col.column
-
-            cleaned_column_names = [replacer_dicc.get(col, col) for col in self._columns]
-
-            result = []
-            for attr in data:
-                dicc_attr = dict(zip(cleaned_column_names, attr))
-                result.append(self._flavour(**dicc_attr, **kwargs))
-
-            return result
-
-        selector: dict[Type[object], Any] = {
-            dict: _dict,
-            tuple: _tuple,
-            set: _set,
-            list: _list,
-        }
-        return selector.get(self._flavour, _default)(**kwargs)
-
-    def _parser_response(self) -> TFlavour:
-        new_response: list[tuple] = []
-        for row in self._response_values:
-            new_row: list = []
-            for i, data in enumerate(row):
-                alias = self._columns[i]
-                clause_info = self._select[alias]
-                if not self._is_parser_required(clause_info):
-                    new_row = row
-                    break
-                else:
-                    parse_data = self._caster.for_value(data, value_type=clause_info.dtype).from_database
-                    new_row.append(parse_data)
-            new_row = tuple(new_row)
-            if not isinstance(new_row, tuple):
-                new_row = tuple(new_row)
-
-            new_response.append(new_row)
-        return new_response
-
-    @staticmethod
-    def _is_parser_required[T: Table](clause_info: ClauseInfo[T]) -> bool:
-        if clause_info is None:
-            return False
-
-        return clause_info.dtype is shp.Point
-
-    @staticmethod
-    def parser_data[T: Table, TProp](clause_info: ClauseInfo[T], data: TProp):
-        if clause_info.dtype is shp.Point:
-            return shp.from_wkt(data)
-        return data
 
 
 class MySQLRepository(BaseRepository[MySQLConnectionPool]):
@@ -222,7 +92,6 @@ class MySQLRepository(BaseRepository[MySQLConnectionPool]):
             - flavour: Type[TFlavour]: Useful to return tuple of any Iterable type as dict,set,list...
         """
 
-        model: Table = kwargs.pop("model", None)
         select: Select = kwargs.pop("select", None)
         cast_to_tuple: bool = kwargs.pop("cast_to_tuple", True)
 
@@ -231,9 +100,8 @@ class MySQLRepository(BaseRepository[MySQLConnectionPool]):
                 cursor.execute(query)
                 values: list[tuple] = cursor.fetchall()
                 columns: tuple[str] = cursor.column_names
-                return Response[TFlavour](
+                return Response(
                     repository=self,
-                    model=model,
                     response_values=values,
                     columns=columns,
                     flavour=flavour,
---
+++ src/ormlambda/repository/response.py
@@ -0,0 +1,145 @@
+from __future__ import annotations
+from typing import Type, Any, TYPE_CHECKING, Optional
+import shapely as shp
+
+# Custom libraries
+from ormlambda.caster import Caster
+from ormlambda.sql.clauses.alias import Alias
+
+if TYPE_CHECKING:
+    from ormlambda import BaseRepository
+    from ormlambda.common.abstract_classes.decomposition_query import ClauseInfo
+    from ormlambda import Table
+    from ormlambda.databases.my_sql.clauses.select import Select
+
+
+type TResponse[TFlavour, *Ts] = TFlavour | tuple[dict[str, tuple[*Ts]]] | tuple[tuple[*Ts]] | tuple[TFlavour]
+
+
+class Response[TFlavour, *Ts]:
+    def __init__(
+        self,
+        repository: BaseRepository,
+        response_values: list[tuple[*Ts]],
+        columns: tuple[str],
+        flavour: Type[TFlavour],
+        select: Optional[Select] = None,
+    ) -> None:
+        self._repository: BaseRepository = repository
+        self._response_values: list[tuple[*Ts]] = response_values
+        self._columns: tuple[str] = columns
+        self._flavour: Type[TFlavour] = flavour
+        self._select: Select = select
+
+        self._response_values_index: int = len(self._response_values)
+        # self.select_values()
+        self._caster = Caster(repository)
+
+    @property
+    def is_one(self) -> bool:
+        return self._response_values_index == 1
+
+    @property
+    def is_there_response(self) -> bool:
+        return self._response_values_index != 0
+
+    @property
+    def is_many(self) -> bool:
+        return self._response_values_index > 1
+
+    def response(self, _tuple: bool, **kwargs) -> TResponse[TFlavour, *Ts]:
+        if not self.is_there_response:
+            return tuple([])
+        cleaned_response = self._response_values
+
+        if self._select is not None:
+            cleaned_response = self._parser_response()
+
+        cast_flavour = self._cast_to_flavour(cleaned_response, **kwargs)
+        if _tuple is not True:
+            return cast_flavour
+
+        return tuple(cast_flavour)
+
+    def _cast_to_flavour(self, data: list[tuple[*Ts]], **kwargs) -> list[dict[str, tuple[*Ts]]] | list[tuple[*Ts]] | list[TFlavour]:
+        def _dict(**kwargs) -> list[dict[str, tuple[*Ts]]]:
+            nonlocal data
+            return [dict(zip(self._columns, x)) for x in data]
+
+        def _tuple(**kwargs) -> list[tuple[*Ts]]:
+            nonlocal data
+            return data
+
+        def _set(**kwargs) -> list[set]:
+            nonlocal data
+            for d in data:
+                n = len(d)
+                for i in range(n):
+                    try:
+                        hash(d[i])
+                    except TypeError:
+                        raise TypeError(f"unhashable type '{type(d[i])}' found in '{type(d)}' when attempting to cast the result into a '{set.__name__}' object")
+            return [set(x) for x in data]
+
+        def _list(**kwargs) -> list[list]:
+            nonlocal data
+            return [list(x) for x in data]
+
+        def _default(**kwargs) -> list[TFlavour]:
+            nonlocal data
+            replacer_dicc: dict[str, str] = {}
+
+            for col in self._select.all_clauses:
+                if hasattr(col, "_alias_aggregate") or col.alias_clause is None or isinstance(col, Alias):
+                    continue
+                replacer_dicc[col.alias_clause] = col.column
+
+            cleaned_column_names = [replacer_dicc.get(col, col) for col in self._columns]
+
+            result = []
+            for attr in data:
+                dicc_attr = dict(zip(cleaned_column_names, attr))
+                result.append(self._flavour(**dicc_attr, **kwargs))
+
+            return result
+
+        selector: dict[Type[object], Any] = {
+            dict: _dict,
+            tuple: _tuple,
+            set: _set,
+            list: _list,
+        }
+        return selector.get(self._flavour, _default)(**kwargs)
+
+    def _parser_response(self) -> TFlavour:
+        new_response: list[tuple] = []
+        for row in self._response_values:
+            new_row: list = []
+            for i, data in enumerate(row):
+                alias = self._columns[i]
+                clause_info = self._select[alias]
+                if not self._is_parser_required(clause_info):
+                    new_row = row
+                    break
+                else:
+                    parse_data = self._caster.for_value(data, value_type=clause_info.dtype).from_database
+                    new_row.append(parse_data)
+            new_row = tuple(new_row)
+            if not isinstance(new_row, tuple):
+                new_row = tuple(new_row)
+
+            new_response.append(new_row)
+        return new_response
+
+    @staticmethod
+    def _is_parser_required[T: Table](clause_info: ClauseInfo[T]) -> bool:
+        if clause_info is None:
+            return False
+
+        return clause_info.dtype is shp.Point
+
+    @staticmethod
+    def parser_data[T: Table, TProp](clause_info: ClauseInfo[T], data: TProp):
+        if clause_info.dtype is shp.Point:
+            return shp.from_wkt(data)
+        return data
