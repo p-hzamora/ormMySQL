922f02db12261e3e6c21332515e596857b11ee86
phzamora <p.hzamora@icloud.com>
2025-04-30 14:49:41 +0200

    add: DatabaseType class

---
+++ src/ormlambda/types/base.py
@@ -0,0 +1,10 @@
+import enum
+
+
+class DatabaseType(str, enum.Enum):
+    MYSQL='mysql'
+    SQLITE='sqlite'
+    POSTGRESQL='postgressql'
+
+    def __str__(self):
+        return super().__str__()
