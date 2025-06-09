0046bc48ed02732b36748b3a042e6bf986bc2f34
p-hzamora <p.hzamora@icloud.com>
2025-06-09 20:02:23 +0200

    add: errors.py file

---
+++ src/ormlambda/errors.py
@@ -0,0 +1,18 @@
+class CompileError(Exception):
+    """Exception raised for errors in the compilation process."""
+
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"CompileError: {self.message}"
+    
+
+class NoSuchModuleError(Exception):
+        """Raised when a dynamically-loaded module (usually a database dialect)
+    of a particular name cannot be located."""
+
+
+        def __str__(self):
+            return f"NoSuchModuleError: {self.args[0]}"
