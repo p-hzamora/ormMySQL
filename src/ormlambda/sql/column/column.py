053c9a93f5d241d2d0e1d3c1b10c18972979ce65
p-hzamora <p.hzamora@icloud.com>
2025-06-11 11:11:02 +0200

    fix: __repr__ in Column class

--- src/ormlambda/sql/column/column.py
+++ src/ormlambda/sql/column/column.py
@@ -93,7 +93,7 @@ class Column[TProp]:
         self.is_not_null: bool = is_not_null
 
     def __repr__(self) -> str:
-        return f"{type(self).__name__}[{self.dtype.__name__}] => {self.column_name}"
+        return f"{type(self).__name__}[{self.dtype.__class__.__name__}] => {self.column_name}"
 
     def __str__(self) -> str:
         return self.table.__table_name__ + "." + self.column_name
