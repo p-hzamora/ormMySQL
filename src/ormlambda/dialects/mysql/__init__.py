eed151b17071206205d8bdf7855c28d4758bca34
p-hzamora <p.hzamora@icloud.com>
2025-06-11 13:01:29 +0200

    chore: avoid * when loading classes

--- src/ormlambda/dialects/mysql/__init__.py
+++ src/ormlambda/dialects/mysql/__init__.py
@@ -1,12 +1,38 @@
 from . import base
 from . import mysqlconnector
 
-# default dialect
-base.dialect = dialect = mysqlconnector.dialect
-
-
-from .types import *  # noqa: F403
+from .types import (  # noqa: F401
+    NUMERIC,
+    DECIMAL,
+    DOUBLE,
+    REAL,
+    FLOAT,
+    INTEGER,
+    BIGINT,
+    MEDIUMINT,
+    TINYINT,
+    SMALLINT,
+    BIT,
+    TIME,
+    TIMESTAMP,
+    DATETIME,
+    YEAR,
+    TEXT,
+    TINYTEXT,
+    MEDIUMTEXT,
+    LONGTEXT,
+    VARCHAR,
+    CHAR,
+    NVARCHAR,
+    NCHAR,
+    TINYBLOB,
+    MEDIUMBLOB,
+    LONGBLOB,
+)
 
 
 from .repository import MySQLRepository  # noqa: F401
 from .caster import MySQLCaster  # noqa: F401
+
+# default dialect
+base.dialect = dialect = mysqlconnector.dialect
