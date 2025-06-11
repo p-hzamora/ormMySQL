8c5c743283ae77405f2dd7c968dc47989f03f709
p-hzamora <p.hzamora@icloud.com>
2025-06-11 10:44:13 +0200

    chore: avoided query property in 'Offset' class

--- src/ormlambda/sql/clauses/offset.py
+++ src/ormlambda/sql/clauses/offset.py
@@ -1,14 +1,8 @@
 from __future__ import annotations
-from ormlambda.common.interfaces.IQueryCommand import IQuery
 from ormlambda.sql.elements import ClauseElement
 
-from typing import TYPE_CHECKING, override
 
-if TYPE_CHECKING:
-    from ormlambda.dialects import Dialect
-
-
-class Offset(IQuery, ClauseElement):
+class Offset(ClauseElement):
     __visit_name__ = "offset"
     OFFSET = "OFFSET"
 
@@ -16,10 +10,6 @@ class Offset(IQuery, ClauseElement):
         if not isinstance(number, int):
             raise ValueError
         self._number: int = number
-
-    @override
-    def query(self, dialect: Dialect, **kwargs) -> str:
-        return f"{self.OFFSET} {self._number}"
 
 
 __all__ = ["Offset"]
