324a55078737462c47ead1edd3ec63ec66a8ea68
p-hzamora <p.hzamora@icloud.com>
2025-06-30 15:40:26 +0200

    chore: added datatypes into ormlambda/__init__.py

--- src/ormlambda/__init__.py
+++ src/ormlambda/__init__.py
@@ -24,3 +24,36 @@ from .model.base_model import (
 )  # COMMENT: to avoid relative import we need to import BaseModel after import Table,Column, ForeignKey, IRepositoryBase and Disassembler
 
 from .engine import create_engine, URL, make_url  # noqa: F401
+
+from .sql.sqltypes import (  # noqa: F401
+    JSON as JSON,
+    UUID as UUID,
+    NullType as NullType,
+    INTEGER as INTEGER,
+    INT as INT,
+    SMALLINTEGER as SMALLINTEGER,
+    BIGINTEGER as BIGINTEGER,
+    NUMERIC as NUMERIC,
+    FLOAT as FLOAT,
+    REAL as REAL,
+    DOUBLE as DOUBLE,
+    DECIMAL as DECIMAL,
+    STRING as STRING,
+    TEXT as TEXT,
+    UNICODE as UNICODE,
+    UNICODETEXT as UNICODETEXT,
+    CHAR as CHAR,
+    NCHAR as NCHAR,
+    BLOB as BLOB,
+    VARCHAR as VARCHAR,
+    NVARCHAR as NVARCHAR,
+    DATE as DATE,
+    TIME as TIME,
+    DATETIME as DATETIME,
+    TIMESTAMP as TIMESTAMP,
+    BOOLEAN as BOOLEAN,
+    LARGEBINARY as LARGEBINARY,
+    VARBINARY as VARBINARY,
+    ENUM as ENUM,
+    POINT as POINT,
+)
