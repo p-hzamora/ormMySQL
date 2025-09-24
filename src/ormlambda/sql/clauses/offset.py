9ca325f6e83e2a85174e611f81585004b88aa9ef
p-hzamora <p.hzamora@icloud.com>
2025-09-24 09:12:13 +0200

    wip: Offset

--- src/ormlambda/sql/clauses/offset.py
+++ src/ormlambda/sql/clauses/offset.py
@@ -6,10 +6,11 @@ class Offset(ClauseElement):
     __visit_name__ = "offset"
     OFFSET = "OFFSET"
 
-    def __init__(self, number: int, **kwargs) -> None:
-        if not isinstance(number, int):
-            raise ValueError
-        self._number: int = number
+    def __init__(self, number: tuple[int], **kwargs) -> None:
+        # FIXME []: check this validation when I finished to refactor. Same process in Limit class
+        # if not isinstance(number, int):
+        #     raise ValueError
+        self._number: int = number[0]
 
 
 __all__ = ["Offset"]
