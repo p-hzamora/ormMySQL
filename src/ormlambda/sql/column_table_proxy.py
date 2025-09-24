a480e3e179e5fc7a91ffb9573bbcb792e8b3029f
p-hzamora <p.hzamora@icloud.com>
2025-09-24 08:50:21 +0200

    refactor: enhance 'ColumnTableProxy' class by adding a property for path and renaming method

--- src/ormlambda/sql/column_table_proxy.py
+++ src/ormlambda/sql/column_table_proxy.py
@@ -3,11 +3,16 @@ import abc
 
 from ormlambda.sql.context import FKChain
 
+
 class ColumnTableProxy(abc.ABC):
     _path: FKChain
 
     def __init__(self, path: FKChain):
         self._path = path
 
+    @property
+    def path(self) -> FKChain:
+        return self._path
+
     @abc.abstractmethod
-    def _get_full_reference(self): ...
+    def get_table_chain(self): ...
