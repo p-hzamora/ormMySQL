bc8cf45e702b8a1f4a99a103cf9dabdde7050eb0
p-hzamora <p.hzamora@icloud.com>
2025-09-24 09:12:22 +0200

    wip: Limit class

--- src/ormlambda/sql/clauses/limit.py
+++ src/ormlambda/sql/clauses/limit.py
@@ -6,10 +6,11 @@ class Limit(ClauseElement):
     __visit_name__ = "limit"
     LIMIT = "LIMIT"
 
-    def __init__(self, number: int, **kwargs) -> None:
-        if not isinstance(number, int):
-            raise ValueError
-        self._number: int = number
+    def __init__(self, number: tuple[int], **kwargs) -> None:
+
+        # if not isinstance(number, int):
+        #     raise ValueError
+        self._number: int = number[0]
 
 
 __all__ = ["Limit"]
