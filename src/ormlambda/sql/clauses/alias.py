75f655b01bbf37ff246dafe3720b4a4d5263ff0a
p-hzamora <p.hzamora@icloud.com>
2025-09-24 08:49:55 +0200

    refactor: simplify 'Alias' class by removing unnecessary inheritance and parameters

--- src/ormlambda/sql/clauses/alias.py
+++ src/ormlambda/sql/clauses/alias.py
@@ -1,42 +1,11 @@
 from __future__ import annotations
-import typing as tp
 
-from ormlambda.sql.clause_info import ClauseInfo
-from ormlambda.sql.elements import ClauseElement
+from ormlambda import ColumnProxy
 
 
-if tp.TYPE_CHECKING:
-    from ormlambda import Table
-    from ormlambda.sql.types import TableType
-    from ormlambda.sql.types import ColumnType
-    from ormlambda.sql.types import AliasType
-
-
-class Alias[T: Table](ClauseInfo[T], ClauseElement):
-    __visit_name__ = "alias"
-
-    def __init__[TProp](
-        self,
-        table: TableType[T],
-        column: tp.Optional[ColumnType[TProp]] = None,
-        alias_table: tp.Optional[AliasType[ClauseInfo[T]]] = None,
-        alias_clause: tp.Optional[AliasType[ClauseInfo[T]]] = None,
-        keep_asterisk: bool = False,
-        preserve_context: bool = False,
-        **kw,
-    ):
-        if not alias_clause:
-            raise TypeError
-        super().__init__(
-            table,
-            column,
-            alias_table=alias_table,
-            alias_clause=alias_clause,
-            keep_asterisk=keep_asterisk,
-            preserve_context=preserve_context,
-            dtype=None,
-            **kw,
-        )
+class Alias[T](ColumnProxy[T]):
+    def __init__(self, column: ColumnProxy[T], alias: str):
+        super().__init__(column._column, path=column.path, alias=alias)
 
 
 __all__ = ["Alias"]
