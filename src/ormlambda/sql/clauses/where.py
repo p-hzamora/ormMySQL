173cc46fe544f7e21549e60cede3b8295b7849a4
p-hzamora <p.hzamora@icloud.com>
2025-09-24 09:09:58 +0200

    update: streamlined Where class

--- src/ormlambda/sql/clauses/where.py
+++ src/ormlambda/sql/clauses/where.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import typing as tp
+from ormlambda import ColumnProxy
 from ormlambda.sql.comparer import Comparer
 from ormlambda.sql.clause_info import AggregateFunctionBase
 from ormlambda.sql.elements import ClauseElement
@@ -20,12 +21,26 @@ class Where(AggregateFunctionBase, ClauseElement):
         *comparer: Comparer,
         restrictive: bool = True,
     ) -> None:
+        if len(comparer) == 1 and isinstance(comparer[0], tp.Iterable):
+            comparer = comparer[0]
         self._comparer: tuple[Comparer] = comparer
         self._restrictive: bool = restrictive
 
     @staticmethod
     def FUNCTION_NAME() -> str:
         return "WHERE"
+
+    def used_columns(self) -> tp.Iterable[ColumnProxy]:
+        res = []
+
+        for comparer in self._comparer:
+            if isinstance(comparer._left_condition, ColumnProxy):
+                res.append(comparer._left_condition)
+
+            if isinstance(comparer._right_condition, ColumnProxy):
+                res.append(comparer._right_condition)
+
+        return res
 
     def compile(self, dialect: Dialect, **kwargs) -> str:
         if isinstance(self._comparer, tp.Iterable):
