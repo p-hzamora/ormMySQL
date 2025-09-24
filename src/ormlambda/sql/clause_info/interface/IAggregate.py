db5612a2eb94b87d124e3c6bcff81540299f7587
p-hzamora <p.hzamora@icloud.com>
2025-09-24 09:13:19 +0200

    add:  'used_columns' and 'dtype' abstract methods in IAggregate

--- src/ormlambda/sql/clause_info/interface/IAggregate.py
+++ src/ormlambda/sql/clause_info/interface/IAggregate.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 import abc
+from typing import Any, Iterable, TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from ormlambda import ColumnProxy
 from ormlambda.common.interfaces.IQueryCommand import IQuery
 
 
@@ -8,3 +11,13 @@ class IAggregate(IQuery):
     @classmethod
     @abc.abstractmethod
     def FUNCTION_NAME(cls) -> str: ...
+
+    def __repr__(self):
+        return f"{IAggregate.__name__}: {type(self).__name__}"
+
+    # @abc.abstractmethod
+    def used_columns(self) -> Iterable[ColumnProxy]: ...
+
+    @property
+    @abc.abstractmethod
+    def dtype(self) -> Any: ...
