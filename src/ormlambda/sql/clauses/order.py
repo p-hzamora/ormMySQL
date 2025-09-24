ae7283da9730243473e34428d5c54ad290bfdecc
p-hzamora <p.hzamora@icloud.com>
2025-09-24 09:11:06 +0200

    update: streamlined Order class

--- src/ormlambda/sql/clauses/order.py
+++ src/ormlambda/sql/clauses/order.py
@@ -2,40 +2,32 @@ from __future__ import annotations
 import typing as tp
 
 from ormlambda.sql.types import ColumnType
-from ormlambda.sql.clause_info import AggregateFunctionBase
 
 from ormlambda.statements import OrderType
 from ormlambda.sql.elements import ClauseElement
 
-if tp.TYPE_CHECKING:
-    from ormlambda.dialects import Dialect
 
-
-class Order(AggregateFunctionBase, ClauseElement):
+class Order[TProp](ClauseElement):
     __visit_name__ = "order"
+
+    columns: tuple[ColumnType[TProp]]
 
     @staticmethod
     def FUNCTION_NAME() -> str:
         return "ORDER BY"
 
-    def __init__[TProp](
+    def __init__(
         self,
-        column: tuple[ColumnType[TProp], ...] | ColumnType[TProp],
+        columns: tuple[ColumnType[TProp], ...] | ColumnType[TProp],
         order_type: tp.Iterable[OrderType],
-        *,
-        dialect: Dialect,
-        **kw,
     ):
-        super().__init__(
-            table=None,
-            column=column,
-            dialect=dialect,
-            **kw,
-        )
+        if isinstance(columns, str) or not isinstance(columns, tp.Iterable):
+            columns = (columns,)
 
         if isinstance(order_type, str) or not isinstance(order_type, tp.Iterable):
             order_type = (order_type,)
 
+        self.columns = columns
         self._order_type: list[OrderType] = [self.__cast_to_OrderType(x) for x in order_type]
 
     def __cast_to_OrderType(self, _value: tp.Any) -> tp.Iterable[OrderType]:
@@ -48,5 +40,9 @@ class Order(AggregateFunctionBase, ClauseElement):
             except Exception:
                 pass
         raise Exception(f"order_type param only can be 'ASC' or 'DESC' string or '{OrderType.__name__}' enum")
+
+    def used_columns(self):
+        return self.columns
+
 
 __all__ = ["Order"]
