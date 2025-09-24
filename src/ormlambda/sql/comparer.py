e7223300210da339d6be9024b0f45a6d6a4b6425
phzamora <p.hzamora@icloud.com>
2025-09-24 23:02:40 +0200

    fix: 'Comparer' class

--- src/ormlambda/sql/comparer.py
+++ src/ormlambda/sql/comparer.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 import abc
 import re
 import typing as tp
-from ormlambda.common.interfaces.IQueryCommand import IQuery
 
 
 from ormlambda.sql.types import ConditionType, ComparerTypes
-from ormlambda.sql.clause_info import ClauseInfo
+from ormlambda.sql.clause_info import ClauseInfo, IAggregate
 from ormlambda import ConditionType as ConditionEnum
 from ormlambda.sql.elements import ClauseElement
+
+from ormlambda import ColumnProxy
 
 if tp.TYPE_CHECKING:
     from ormlambda.dialects import Dialect
@@ -45,7 +46,7 @@ class CleanValue:
         return temp_name
 
 
-class Comparer(ClauseElement, IQuery):
+class Comparer(ClauseElement, IAggregate):
     __visit_name__ = "comparer"
 
     def __init__(
@@ -54,67 +55,31 @@ class Comparer(ClauseElement, IQuery):
         right_condition: ConditionType,
         compare: ComparerTypes,
         flags: tp.Optional[tp.Iterable[re.RegexFlag]] = None,
-        dialect: tp.Optional[Dialect] = None,
-        left_alias_table: tp.Optional[str] = "{table}",
-        right_alias_table: tp.Optional[str] = "{table}",
     ) -> None:
         self._compare: ComparerTypes = compare
-        self._left_condition: Comparer | ClauseInfo = left_condition
-        self._right_condition: Comparer | ClauseInfo = right_condition
+        self._left_condition: ConditionType = left_condition
+        self._right_condition: ConditionType = right_condition
         self._flags = flags
-        self._dialect = dialect
 
-        self.left_alias_table = left_alias_table
-        self.right_alias_table = right_alias_table
-
-    def set_dialect(self, dialect: Dialect) -> None:
-        self._dialect = dialect
-        return None
+    @property
+    def dtype(self) -> tp.Any: ...
 
     def __repr__(self) -> str:
-        return f"{Comparer.__name__}: {self.query}"
-
-    def _create_clause_info(self, cond: ConditionType, dialect: Dialect, **kw) -> Comparer | ClauseInfo:
-        from ormlambda import Column, ColumnProxy
-
-        if isinstance(cond, Comparer):
-            return cond
-
-        table = None if not isinstance(cond, Column | ColumnProxy) else cond.table
-
-        if isinstance(cond, ColumnProxy):
-            kw = {
-                **kw,
-                "alias_table": cond.get_table_chain(),
-            }
-
-        # it a value that's not depend of any Table
-        return ClauseInfo(
-            table,
-            cond,
-            alias_clause=None,
-            dialect=dialect,
-            **kw,
-        )
+        return f"{Comparer.__name__}: {self._left_condition} {self._compare} {self._right_condition}"
 
     def left_condition(self, dialect: Dialect, **kwargs) -> Comparer | ClauseInfo:
-        return self._create_clause_info(self._left_condition, dialect=dialect, alias_table=self.left_alias_table, **kwargs)
+        if isinstance(self._left_condition, ColumnProxy | Comparer):
+            return self._left_condition.compile(dialect=dialect, **kwargs).string
+        return self._left_condition
 
     def right_condition(self, dialect: Dialect, **kwargs) -> Comparer | ClauseInfo:
-        return self._create_clause_info(self._right_condition, dialect=dialect, alias_table=self.right_alias_table, **kwargs)
+        if isinstance(self._right_condition, ColumnProxy | Comparer):
+            return self._right_condition.compile(dialect=dialect, **kwargs).string
+        return f"'{self._right_condition}'"
 
     @property
     def compare(self) -> ComparerTypes:
         return self._compare
-
-    def query(self, dialect: Dialect, **kwargs) -> str:
-        lcond = self.left_condition(dialect).query(dialect, **kwargs)
-        rcond = self.right_condition(dialect).query(dialect, **kwargs)
-
-        if self._flags:
-            rcond = CleanValue(rcond, self._flags).clean()
-
-        return f"{lcond} {self._compare} {rcond}"
 
     def __and__(self, other: Comparer, **kwargs) -> Comparer:
         # Customize the behavior of '&'
@@ -125,12 +90,13 @@ class Comparer(ClauseElement, IQuery):
         return Comparer(self, other, "OR", **kwargs)
 
     @classmethod
-    def join_comparers(cls, comparers: list[Comparer], restrictive: bool = True, *, dialect) -> str:
+    def join_comparers(cls, comparers: list[Comparer], restrictive: bool = True, *, dialect, **kwargs) -> str:
         if not isinstance(comparers, tp.Iterable):
             raise ValueError(f"Excepted '{Comparer.__name__}' iterable not {type(comparers).__name__}")
+        
         if len(comparers) == 1:
             comparer = comparers[0]
-            return comparer.query(dialect)
+            return comparer.compile(dialect, alias_clause=None, **kwargs).string
 
         join_method = cls.__or__ if not restrictive else cls.__and__
 
@@ -139,9 +105,22 @@ class Comparer(ClauseElement, IQuery):
             if ini_comparer is None:
                 ini_comparer = comparers[i]
             right_comparer = comparers[i + 1]
-            new_comparer = join_method(ini_comparer, right_comparer, dialect=dialect)
+            new_comparer = join_method(ini_comparer, right_comparer)
             ini_comparer = new_comparer
-        return new_comparer.query(dialect)
+
+        res = new_comparer.compile(dialect, alias_clause=None, **kwargs)
+        return res.string
+
+    def used_columns(self) -> tp.Iterable[ColumnProxy]:
+        res = []
+
+        if isinstance(self._left_condition, ColumnProxy):
+            res.append(self._left_condition)
+
+        if isinstance(self._right_condition, ColumnProxy):
+            res.append(self._right_condition)
+
+        return res
 
 
 class Regex(Comparer):
