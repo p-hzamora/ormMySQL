0a863c14d172037ae775521ca4582067d7a05720
p-hzamora <p.hzamora@icloud.com>
2025-09-24 10:21:05 +0200

    update: streamline GroupBy class

--- src/ormlambda/sql/clauses/group_by.py
+++ src/ormlambda/sql/clauses/group_by.py
@@ -3,21 +3,17 @@ from typing import TYPE_CHECKING, Iterable
 
 if TYPE_CHECKING:
     from ormlambda.dialects import Dialect
-from ormlambda.sql.clause_info import AggregateFunctionBase
 from ormlambda.sql.types import ColumnType
 from ormlambda.sql.elements import ClauseElement
 
 
-class GroupBy(AggregateFunctionBase, ClauseElement):
+class GroupBy(ClauseElement):
     __visit_name__ = "group_by"
-
-    @classmethod
-    def FUNCTION_NAME(self) -> str:
-        return "GROUP BY"
 
     def __init__(self, column: ColumnType, dialect: Dialect, **kwargs):
         if isinstance(column, Iterable):
             column = column[0]
+            
         super().__init__(
             table=column.table,
             column=column,
