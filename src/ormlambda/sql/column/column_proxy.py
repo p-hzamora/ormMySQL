7b13848a35d8971ae4c7e8c2b4d63180faea709c
phzamora <p.hzamora@icloud.com>
2025-09-24 23:06:16 +0200

    wip: updating 'get_relations' method in ColumnProxy class

--- src/ormlambda/sql/column/column_proxy.py
+++ src/ormlambda/sql/column/column_proxy.py
@@ -5,9 +5,8 @@ from typing import TYPE_CHECKING, Optional, overload
 from ormlambda.sql.column_table_proxy import ColumnTableProxy
 from ormlambda.sql.context import PATH_CONTEXT
 from .column import Column
-from ormlambda import ConditionType
+from ormlambda import ConditionType, JoinType
 from ormlambda.sql.elements import ClauseElement
-
 
 
 if TYPE_CHECKING:
@@ -19,8 +18,7 @@ if TYPE_CHECKING:
 
 
 class ColumnProxy[TProp](ColumnTableProxy, Column[TProp], ClauseElement):
-
-    __visit_name__ = 'column_proxy'
+    __visit_name__ = "column_proxy"
     _column: Column
     _path: FKChain
     alias: Optional[str]
@@ -28,7 +26,7 @@ class ColumnProxy[TProp](ColumnTableProxy, Column[TProp], ClauseElement):
     @overload
     def __init__(self, column: Column[TProp], path: FKChain): ...
     @overload
-    def __init__(self, column: Column[TProp], path: FKChain, alias=str): ...
+    def __init__(self, column: Column[TProp], path: FKChain, alias: str): ...
 
     def __init__(self, column, path, alias=None):
         self._column = column
@@ -39,7 +37,11 @@ class ColumnProxy[TProp](ColumnTableProxy, Column[TProp], ClauseElement):
         return self.get_full_chain()
 
     def __repr__(self) -> str:
-        return f"{ColumnProxy.__name__}({self._column.table.__table_name__}.{self._column.column_name}) Path={self._path.get_path_key()}"
+        table = self._column.table
+        table = table.__table_name__ if table else None
+
+        path = f"{table}.{self._column.column_name}"
+        return f"{ColumnProxy.__name__}({path if table else self._column.column_name}) Path={self._path.get_path_key()}"
 
     def __getattr__(self, name: str):
         # it does not work when comparing methods
@@ -89,34 +91,33 @@ class ColumnProxy[TProp](ColumnTableProxy, Column[TProp], ClauseElement):
     def number_table_in_chain(self) -> int:
         return len(self._path.steps)
 
-    def get_relations(self, by, dialect: Dialect) -> tuple[JoinSelector]:
+    def get_relations(self, by: JoinType, dialect: Dialect) -> tuple[JoinSelector]:
         from ormlambda.sql.clauses import JoinSelector
+        from ormlambda.sql.comparer import Comparer
 
         result: list[JoinSelector] = []
         alias = self._path.base.__table_name__
 
-        for i in range(n := self.number_table_in_chain()):
+        for i in range(self.number_table_in_chain()):
             tbl = self._path.steps[i]
             relation = tbl.resolved_function(dialect)
-            relation.left_alias_table = alias
-            alias += f"_{tbl.clause_name}"
-            relation.right_alias_table = alias
 
-            js = JoinSelector(relation, by, alias, dialect=dialect)
+            lcond = ColumnProxy(column=relation._left_condition, path=self._path.parent, alias=alias)
+
+            alias += f"_{tbl.clause_name}"
+            rcond = ColumnProxy(column=relation._right_condition, path=self._path, alias=alias)
+
+            comparer = Comparer(left_condition=lcond, right_condition=rcond, compare=relation.compare)
+            js = JoinSelector(comparer, by, alias, dialect=dialect)
             PATH_CONTEXT.add_join(js, alias)
             result.append(js)
-            js.query(dialect)
+            js.compile(dialect)
         return result
 
-    def query(self, dialect: Dialect) -> str:
+    def query(self, dialect: Dialect, **kwargs) -> str:
         from ormlambda.sql.clause_info import ClauseInfo
 
         alias_table = self.get_table_chain()
-        return ClauseInfo(
-            table=self._path.base,
-            column=self._column.column_name,
-            alias_table=alias_table if alias_table else "{table}",
-            alias_clause=self.alias or "{column}",
-            dtype=self._column.dtype,
-            dialect=dialect,
-        ).query(dialect)
+
+        params = {"table": self._path.base, "column": self._column.column_name, "alias_table": alias_table if alias_table else "{table}", "alias_clause": self.alias or "{column}", "dtype": self._column.dtype, "dialect": dialect, **kwargs}
+        return ClauseInfo(**params).query(dialect)
