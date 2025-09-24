5fe3af65adfda01acfebc84073a7570f8bb45928
p-hzamora <p.hzamora@icloud.com>
2025-09-24 08:35:01 +0200

    refactor: 'resolved_callback_object' method

--- src/ormlambda/common/global_checker.py
+++ src/ormlambda/common/global_checker.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 import re
-from typing import Any, TYPE_CHECKING, Type
+from typing import Any, TYPE_CHECKING, Iterable, Type, Callable
 
 from ormlambda.common.errors import UnmatchedLambdaParameterError
 
 if TYPE_CHECKING:
+    from ormlambda.sql.context import PathContext
     from ormlambda.sql import Table
-    from ormlambda.sql.column import ColumnProxy
     from ormlambda.sql.table import TableProxy
+    from ormlambda.sql.column import ColumnProxy
 
 
 class GlobalChecker:
@@ -16,24 +17,37 @@ class GlobalChecker:
         return callable(obj) and not isinstance(obj, type)
 
     @classmethod
-    def resolved_callback_object(cls, obj: Any, table: Type[Table | TableProxy]) -> tuple[ColumnProxy | TableProxy, ...]:
-        from ormlambda.sql.context import PATH_CONTEXT
+    def resolved_callback_object(cls, lambda_func: Callable[[Type[Table]], Any], table: Type[Table], context: PathContext) -> tuple[ColumnProxy | TableProxy, ...]:
         from ormlambda.sql.table import TableProxy
-
-        if isinstance(table, TableProxy):
-            table = table._table_class
-
-        if not cls.is_lambda_function(obj):
-            return obj
-            raise ValueError
+        from ormlambda import Column
+        from ormlambda.sql.column_table_proxy import FKChain
+        from ormlambda.sql.column import ColumnProxy
 
         try:
-            with PATH_CONTEXT.query_context(table) as context:
-                table_proxy = TableProxy(table, context.get_current_path())
-                return obj(table_proxy)
+            table_proxy = TableProxy(table, context.get_current_path())
+            response = lambda_func(table_proxy)
+            result = []
+
+
+            if isinstance(response, str) or not isinstance(response, Iterable):
+                response = [response]
+
+            for item in response:
+                if isinstance(item, TableProxy):
+                    result.extend(item.get_columns())
+
+                if isinstance(item, str):
+                    new_col = Column(dtype=str, column_name=item)
+                    new_col.table = table
+                    result = ColumnProxy(new_col, path=FKChain(table, None))
+                else:
+                    result.append(item)
+
+            return result
+
         except TypeError as err:
             cond1 = r"takes \d+ positional argument but \d+ were given"
             cond2 = r"missing \d+ required positional arguments:"
             if re.search(r"(" + f"{cond1}|{cond2}" + r")", err.args[0]):
-                raise UnmatchedLambdaParameterError(len(table), obj)
+                raise UnmatchedLambdaParameterError(len(table), lambda_func)
             raise err
