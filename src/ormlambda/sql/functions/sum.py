bdaf5aad9ba0a29d438abc9a2360095ab8f8ec37
p-hzamora <p.hzamora@icloud.com>
2025-09-29 12:58:39 +0200

    refactor: 'Sum' class

--- src/ormlambda/sql/functions/sum.py
+++ src/ormlambda/sql/functions/sum.py
@@ -1,38 +1,25 @@
 from __future__ import annotations
-import typing as tp
 
-from ormlambda.sql.clause_info import ClauseInfo
+from ormlambda.sql.elements import ClauseElement
+
 from ormlambda.sql.types import ColumnType, AliasType
-from ormlambda.sql.clause_info import AggregateFunctionBase
-
-if tp.TYPE_CHECKING:
-    from ormlambda.dialects import Dialect
+from ormlambda.sql.clause_info import IAggregate
 
 
-class Sum(AggregateFunctionBase):
-    @staticmethod
-    def FUNCTION_NAME() -> str:
-        return "SUM"
+class Sum(ClauseElement, IAggregate):
+    __visit_name__ = "sum"
 
-    def __init__[TProp](self, elements: tuple[ColumnType[TProp], ...] | ColumnType[TProp], alias_clause: AliasType[ColumnType[TProp]] = "sum", *, dialect: Dialect):
-        super().__init__(
-            table=None,
-            column=elements,
-            alias_table=None,
-            alias_clause=alias_clause,
-            keep_asterisk=False,
-            preserve_context=False,
-            dialect=dialect,
-        )
+    def __init__[TProp](
+        self,
+        elements: ColumnType[TProp],
+        alias: AliasType[ColumnType[TProp]] = "sum",
+    ):
+        self.column = elements
+        self.alias = alias
 
-    @tp.override
-    def query(self, dialect: Dialect, **kwargs) -> str:
-        columns: list[str] = []
+    def used_columns(self):
+        return [self.column]
 
-        for clause in self._convert_into_clauseInfo(self.unresolved_column, self._dialect):
-            new_clause = clause
-            new_clause.alias_clause = None
-            columns.append(new_clause)
-
-        method_string = f"{self.FUNCTION_NAME()}({ClauseInfo.join_clauses(columns,dialect=self._dialect)})"
-        return self._concat_alias_and_column(method_string, self._alias_aggregate)
+    @property
+    def dtype(self) -> int:
+        return int
