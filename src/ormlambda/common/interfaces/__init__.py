a385213f7c1a9ce834021d0cfc2ba7614edb55ca
p-hzamora <p.hzamora@icloud.com>
2025-09-29 23:56:15 +0200

    delet: 'IDecompositionQuery' class

--- src/ormlambda/common/interfaces/__init__.py
+++ src/ormlambda/common/interfaces/__init__.py
@@ -1,5 +1,3 @@
-from .ICustomAlias import ICustomAlias as ICustomAlias
-from .IDecompositionQuery import IDecompositionQuery as IDecompositionQuery
 from .IJoinSelector import IJoinSelector as IJoinSelector
 from .INonQueryCommand import INonQueryCommand as INonQueryCommand
 from .IQueryCommand import IQuery as IQuery
