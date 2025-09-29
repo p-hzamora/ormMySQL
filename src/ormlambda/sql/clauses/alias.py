813c2c7a85df026b6b7b0c21809a6b26fa7f52cf
p-hzamora <p.hzamora@icloud.com>
2025-09-29 12:57:48 +0200

    chore: added __repr__ into 'Alias' class

--- src/ormlambda/sql/clauses/alias.py
+++ src/ormlambda/sql/clauses/alias.py
@@ -7,5 +7,8 @@ class Alias[T](ColumnProxy[T]):
     def __init__(self, column: ColumnProxy[T], alias: str):
         super().__init__(column._column, path=column.path, alias=alias)
 
+    def __repr__(self):
+        return f"{Alias.__name__}: {super().__repr__()}"
+
 
 __all__ = ["Alias"]
