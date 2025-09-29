3ac52e31af19c4fdcc6c7257b04605612632a836
p-hzamora <p.hzamora@icloud.com>
2025-09-29 13:05:45 +0200

    refactor: 'Concat' class

--- src/ormlambda/sql/functions/concat.py
+++ src/ormlambda/sql/functions/concat.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
 
-from ormlambda.sql import ColumnProxy
-from ormlambda.sql import TableProxy
-from ormlambda.sql.clause_info import AggregateFunctionBase
+from ormlambda.sql.clause_info import IAggregate
 from ormlambda.sql.elements import ClauseElement
 from ormlambda.sql.types import ColumnType, AliasType
 
@@ -11,16 +9,20 @@ from ormlambda.sql.types import ColumnType, AliasType
 type ConcatResponse[TProp] = tuple[str | ColumnType[TProp], ...]
 
 
-class Concat(ClauseElement):
+class Concat(ClauseElement, IAggregate):
     __visit_name__ = "concat"
-
 
     def __init__[TProp](
         self,
         values: ConcatResponse[TProp],
         alias: AliasType[ColumnType[TProp]] = "concat",
     ) -> None:
-    
         self.values = values
         self.alias = alias
-        
+
+    def used_columns(self):
+        return [x for x in self.values if not isinstance(x, str)]
+
+    @property
+    def dtype(self) -> str:
+        return str
