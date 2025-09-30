03b7e4f539b47723e18b60046657e67e1e6de358
p-hzamora <p.hzamora@icloud.com>
2025-09-30 10:33:23 +0200

    update: 'Offset' constructor

--- src/ormlambda/sql/clauses/offset.py
+++ src/ormlambda/sql/clauses/offset.py
@@ -5,10 +5,9 @@ from ormlambda.sql.elements import ClauseElement
 class Offset(ClauseElement):
     __visit_name__ = "offset"
 
-    def __init__(self, number: tuple[int]) -> None:
-        # FIXME []: check this validation when I finished to refactor. Same process in Limit class
-        # if not isinstance(number, int):
-        #     raise ValueError
+    def __init__(self, number: int) -> None:
+        if not isinstance(number, int):
+            raise ValueError
         self._number: int = number
 
 
