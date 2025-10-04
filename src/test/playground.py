98fd352a6b6203e3717d49bfb52e18c006cd46c6
p-hzamora <p.hzamora@icloud.com>
2025-10-04 20:34:48 +0200

    test: creating pagination to test the tool

--- src/test/playground.py
+++ src/test/playground.py
@@ -1,48 +1,138 @@
 from __future__ import annotations
+from datetime import datetime
 import sys
 from pathlib import Path
+from typing import Any, Iterable, Optional, Type, cast
+import json
+import base64
 
 sys.path.insert(0, [str(x.parent) for x in Path(__file__).parents if x.name == "test"].pop())
 
+from pydantic import BaseModel
 from test.config import create_sakila_engine
-from ormlambda import ORM
+from ormlambda import ORM, Column
 from test.models import Address
 
 
 engine = create_sakila_engine()
 
+type DataType = dict[str, Any]
 
-def pagination(limit: int = 0, skip: int = 0) -> tuple:
+
+class PageInfo(BaseModel):
+    size: int
+    prev: Optional[int] = None
+    next: Optional[int] = None
+
+
+class Meta(BaseModel):
+    page: PageInfo
+
+
+class BaseCursor(BaseModel):
+    meta: Meta
+
+
+class CursorResponse(BaseCursor): ...
+
+
+class AddressResponse(BaseModel):
+    address_id: int
+    address: str
+    address2: Optional[str]
+    district: str
+    city_id: int
+    postal_code: str
+    phone: str
+    last_update: datetime
+
+
+class DBCursor[T]:
+    def __init__(self, data: CursorResponse):
+        self.result = data
+
+    def encode(self) -> str:
+        json_string = self.result.model_dump_json()
+
+        json_bytes = json_string.encode("utf-8")
+        base64_bytes = base64.b64encode(json_bytes)
+
+        return base64_bytes.decode("utf-8")
+
+    def __repr__(self):
+        return f"{DBCursor.__name__}: {self.result}"
+
+    @classmethod
+    def decode(cls: Type[DBCursor], cursor_string: str) -> DBCursor[T]:
+        if not cursor_string:
+            return None
+        try:
+            json_bytes = base64.b64decode(cursor_string)
+
+            json_string = json_bytes.decode("utf-8")
+
+            model = CursorResponse(**json.loads(json_string))
+            return cls(model)
+        except Exception as e:
+            raise ValueError(f"Invalid cursor: {e}")
+
+
+type FilteredCols = dict[str, Iterable[str]]
+
+
+def paginated_response(
+    cursor: Optional[str] = None,
+    size: int = 10,
+    columns: Optional[FilteredCols] = None,
+) -> tuple[tuple[AddressResponse, ...], DBCursor]:
+    def selected_columns(address: Address):
+        return (
+            address.address_id,
+            address.address,
+            address.address2,
+            address.district,
+            address.city_id,
+            address.postal_code,
+            address.phone,
+            address.last_update,
+        )
+
+    db_cursor = DBCursor.decode(cursor)
+
     model = ORM(Address, engine)
+    model.order(lambda x: x.address_id, "ASC")
+    model.limit(size)
 
-    if limit:
-        model.limit(limit)
+    if db_cursor and (next := db_cursor.result.meta.page.next):
+        model.where(lambda x: x.address_id > next)
 
-    if skip:
-        model.offset(skip)
+    if columns:
+        for col, values in columns.items():
+            val = "|".join(map(str, values))
+            model.where(lambda x: cast(Column, getattr(x, col)).regex(f"{val}"), True)
 
-    return model.select()
+    data = model.select(selected_columns, flavour=AddressResponse)
+
+    cursor_response = CursorResponse(
+        meta=Meta(
+            page=PageInfo(
+                size=size,
+                prev=None if not db_cursor else db_cursor.result.meta.page.prev,
+                next=None if not data else data[-1].address_id,
+            )
+        ),
+    )
+
+    return data, DBCursor(cursor_response).encode()
 
 
-# for x in range(0, 100, 20):
-#     data = pagination(x, x)
-
-# fmt: off
-res = (
-    ORM(Address, engine)
-    .where(lambda x: x.address_id >= 10)
-    .where(lambda x: x.City.city_id > 4)
-    .select()
+data, cursor = paginated_response(
+    columns={
+        "address_id": (1, 10, 20, 21, 22, 23, 24, 25, 100, 111),
+        "address": ("1795", "360", "270"),
+    }
 )
+data, cursor = paginated_response(cursor)
 
-res2 = (
-    ORM(Address, engine)
-    .where(lambda x: [
-        x.address_id >= 10,
-        x.City.city_id > 4,
-        ]
-    ) 
-    .select()
-)
-# fmt: on
-assert res == res2
+
+pass
