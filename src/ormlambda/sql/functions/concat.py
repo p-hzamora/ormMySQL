94e2b0f4cab8e19007d877e41fc774802c54ee54
p-hzamora <p.hzamora@icloud.com>
2025-10-04 20:26:03 +0200

    update: 'Concat' class

--- src/ormlambda/sql/functions/concat.py
+++ src/ormlambda/sql/functions/concat.py
@@ -1,22 +1,29 @@
 from __future__ import annotations
+from typing import Any, Iterable
 
 
 from ormlambda.sql.clause_info import IAggregate
 from ormlambda.sql.elements import ClauseElement
 from ormlambda.sql.types import ColumnType, AliasType
+from ormlambda import ColumnProxy, TableProxy
+from ormlambda.common import GlobalChecker
+
+VALID_CONCAT_TYPES = ColumnProxy | str | TableProxy
 
 
-type ConcatResponse[TProp] = tuple[str | ColumnType[TProp], ...]
-
-
-class Concat(ClauseElement, IAggregate):
+class Concat[T](ClauseElement, IAggregate):
     __visit_name__ = "concat"
 
     def __init__[TProp](
         self,
-        values: ConcatResponse[TProp],
+        values: tuple[str | ColumnType[TProp] | TableProxy[T], ...],
         alias: AliasType[ColumnType[TProp]] = "concat",
     ) -> None:
+        if isinstance(values, TableProxy):
+            values = GlobalChecker.parser_object(values, values._table_class)
+        if not self.is_valid(values):
+            raise ValueError(values)
+
         self.values = values
         self.alias = alias
 
@@ -26,3 +33,13 @@ class Concat(ClauseElement, IAggregate):
     @property
     def dtype(self) -> str:
         return str
+
+    @staticmethod
+    def is_valid(values: Any) -> bool:
+        if not isinstance(values, Iterable):
+            return False
+        for val in values:
+            if not isinstance(val, VALID_CONCAT_TYPES):
+                return False
+
+        return True
