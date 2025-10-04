e8a638c10647df91ab1d99992f5502fdd46d8243
p-hzamora <p.hzamora@icloud.com>
2025-10-04 18:34:01 +0200

    refactor: ormlambda/util folder

--- src/ormlambda/util/typing.py
+++ src/ormlambda/util/typing.py
@@ -1,6 +1,15 @@
-import typing as tp
 import typing_extensions as tpe
+from typing import Any, Literal, Type, NewType, get_origin, TypeGuard, TypeAliasType
 
-LITERAL_TYPES = frozenset([tp.Literal, tpe.Literal])
 
-type _AnnotationScanType = tp.Type[tp.Any] | str | tp.NewType
+LITERAL_TYPES = frozenset([Literal, tpe.Literal])
+
+type _AnnotationScanType = Type[Any] | str | NewType
+
+
+def is_literal(type_: Any) -> bool:
+    return get_origin(type) in LITERAL_TYPES
+
+
+def is_pep695(type_: _AnnotationScanType) -> TypeGuard[TypeAliasType]:
+    return isinstance(type_, TypeAliasType)
